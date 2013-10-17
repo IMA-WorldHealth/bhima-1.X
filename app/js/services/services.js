d89b7a599502453d329fa9075c97983a5ca283d5
dedrick <kitamuka@gmail.com>
2013-10-17 09:12:43 +0100

    moins importatnts

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -9,22 +9,22 @@
   services.service('bikaConnect', function($http) { 
     //TODO: Define API for getting data from the server - providing query data, simple table names, etc.
     this.fetch = function(table, columns, where, value) {     
-    var query = { 
-      e: [{t : table, c : columns}]
-    };
-    
-    if(where) { 
-      query.c = [{t : table, cl : where, v : value, z : '='}];
-    }
-    
-    console.log("query", query);
-    
-    
-    var promise = $http.get('/data/?' + JSON.stringify(query)).then(function(result) { 
-        // I can now manipulate the data before returning it if needed
-        return result.data;
-      });
-      return promise;
+      var query = { 
+        e: [{t : table, c : columns}]
+      };
+      
+      if(where) { 
+        query.c = [{t : table, cl : where, v : value, z : '='}];
+      }
+      
+      console.log("query", query);
+      
+      
+      var promise = $http.get('/data/?' + JSON.stringify(query)).then(function(result) { 
+          // I can now manipulate the data before returning it if needed
+          return result.data;
+        });
+        return promise;
     };
     
     //Because TODO
@@ -37,11 +37,16 @@
 
     
     this.get = function(target, requestObject){
-    var promise = $http.get(target + JSON.stringify(requestObject)).then(function(result) { 
-      return result.data;
-    });
-    return promise;
+      var promise = $http.get(target + JSON.stringify(requestObject)).then(function(result) { 
+        return result.data;
+      });
+      return promise;
   };
+
+  this.send = function(table, data){
+    
+
+  }
   });
 
 
