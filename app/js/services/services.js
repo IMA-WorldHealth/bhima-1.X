e2fd31adf023bfd9290ed0113d4e65a311c93854
Jonathan Niles <jonathanwniles@gmail.com>
2013-10-19 08:17:29 +0200

    socket demo .get() working; put/delete next; FIXME: $q requires $scope.$apply();

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -81,6 +81,169 @@
 
   });
 
+  services.factory('data', function($q) {
+
+    function serialize (input) { return JSON.stringify(input); }
+    function deserialize (input) { return JSON.parse(input); }
+
+    // Socket Store
+    function SocketStore(options) {
+      var port       = options.port || 8000, // default to port 8000
+          identifier = options.identifier,
+          table      = options.table,
+          columns    = options.columns,
+          autosync   = options.autosync || false, // autosync defaults to false
+          connection = new WebSocket('ws://127.0.0.1:' + port),
+          ready = $q.defer(),
+          socketid   = null, // this is set in a server-response to init()
+          changes    = [];
+      
+      this.identifier = identifier;
+      this.data = null;
+      this.index = null;
+      this.ready = ready; // refers to ready above
+
+      this.setData = function (data) {
+        this.data = data;
+        this.index = {};
+        for (var i = 0, l = data.length; i < l; i++) {
+          this.index[data[i][this.identifier]] = i;
+        }
+      };
+
+      var open = connection.onopen = function () { init(); };
+      var receive = connection.onmessage = function (rawpacket) { return router(deserialize(rawpacket.data));};
+      function send (rawpacket) { connection.send(serialize(rawpacket)); }
+  
+      function init () {
+        var parameters = {
+          method     : 'INIT',
+          table      : table,
+          columns    : columns,
+          identifier : identifier
+        };
+        send(parameters);
+      }
+    
+      function router (packet) {
+        var methods = {
+          'PUT'    : route_put,
+          'REMOVE' : route_remove,
+          'INIT'   : route_init
+        };
+        if (!socketid) { socketid = packet.socketid; }
+        methods[packet.method](packet.data);
+      }
+ 
+      var self = this;
+      function route_put (data) { self.put(data); }
+      function route_remove(id) { self.remove(id); }
+      function route_init (data) { 
+        self.setData(data);
+        console.log("Received data. Resolving...");
+        self.ready.resolve();
+      }
+  
+      this.get = function (id) {
+        var q = $q.defer(),
+            promise = this.ready.promise,
+            self = this;
+        promise.then(function() { console.log("Resolved."); });
+        promise.then(function(res) {
+          q.resolve(self.data[self.index[id]]);
+        }); 
+        return q.promise;
+      };
+
+      this.put = function (object, opts) {
+        // store put
+        var data = this.data,
+          index = this.index,
+          identifier = this.identifier;
+        var id = object[identifier] = (opts && "id" in opts) ? opts.id : identifier in object ? object[identifier] : Math.random();
+        
+        if (id in index) {
+          var ref = data[index[id]];
+          if (opts && opts.replace === false) {
+            data[index[id]] = object;
+          } else {
+            for (var key in object) {
+              ref[key] = object[key];
+            }
+          }
+        } else {
+          index[id] = data.push(object) - 1;
+        }
+
+        //FIXME: update this
+        var exists = store.get(object[identifier]);
+        var method = (exists) ? 'UPDATE' : 'INSERT';
+  
+        parameters = {
+          socketid : socketid,
+          method   : method,
+          data     : object
+        };
+
+        if (autosync) { send(parameters); } 
+        else { changes.push(parameters); }
+        return id;
+      };
+    
+      this.remove = function (id) {
+        var index = this.index,
+          data =  this.data;
+        if (id in index) {
+          data.splice(index[id], 1);
+          this.setData(data);
+
+          parameters = {
+            socketid : socketid,
+            method: 'REMOVE',
+            data: id
+          };
+  
+          if (autosync) { send(parameters); }
+          else { changes.push(parameters); }
+          return true;
+        }
+      };
+  
+      this.sync = function () {
+        for (var i = 0, l = changes.length; i < l; i++) {
+          send(changes[i]);
+        }
+      };
+    
+      self = this;
+      (function constructor() {
+        self.setData(options.data || []); 
+      })();
+
+    }
+
+    var registry = {};
+
+    function register(options) {
+      var table = options.table,
+          store;
+
+      if (registry[table]) {
+        // this is a store currently in use 
+        return registry[table];
+      } else {
+        store = new SocketStore(options);
+        registry[table] = store;
+        return store;
+      }
+    }
+
+    return {
+      register: register,
+    };
+
+  });
+
   services.factory('connect', function($http, $q) { 
     //return object with get/put/delete/update functions 
     //index ids for quick get
