43b7dd2d14f7684c617a80f3cb9e5dfa97e6f7fc
Steven Fountain <StevenFountain@live.co.uk>
2013-11-01 10:32:46 +0100

    Fixed issue with invoice date and debtor id, user is taken to invoice records on successful generation

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -786,7 +786,7 @@ controllers.controller('fiscalController', function($scope, $q, connect, appstat
     init();
   });
 
-  controllers.controller('salesController', function($scope, $q, connect, appstate) { 
+  controllers.controller('salesController', function($scope, $q, $location, connect, appstate) { 
     // TODO
     //  - selecting a debitor should either be done through id or name search (Typeahead select)
     //  - An Invoice should not be able to include the same item (removed from options for future line items)
@@ -846,7 +846,16 @@ controllers.controller('fiscalController', function($scope, $q, connect, appstat
     function createId(list) { 
       var default_id = 100000;
       if(list.length < 1) return default_id; //No invoices have been created
-      return list.reduce(function(a, b) { a = a.id || a; b = b.id || b; return Math.max(a, b)}).id + 1;
+      console.log("Sales list", list);
+      var search_max = list.reduce(function(a, b) { a = a.id || a; b = b.id || b; return Math.max(a, b)});
+      //reduce returns an object if only one element is in the array for some reason
+      //TODOSET
+      if(search_max.id) {
+        return search_max.id +1;
+      } else { 
+        return search_max +1;
+      }
+      //return list.reduce(function(a, b) { a = a.id || a; b = b.id || b; return Math.max(a, b)}).id + 1;
     }
 
     function getDate() { 
@@ -864,6 +873,8 @@ controllers.controller('fiscalController', function($scope, $q, connect, appstat
         t += $scope.inventory[i].quantity * $scope.inventory[i].price;
       }
 
+
+
       
       //create invoice record
       var format_invoice = {
@@ -871,7 +882,8 @@ controllers.controller('fiscalController', function($scope, $q, connect, appstat
         id : $scope.invoice_id,
         cost : t,
         currency : 'USD', //ohgd
-        debitor_id : $scope.debtor.id,
+        debitor_id : $scope.debtor.debitor_id,
+        invoice_date: $scope.sale_date,
         seller_id : '1', //TODO placeholder - this should be derived from appstate (session) or equivelant
         discount: '0', //placeholder
         note : '',
@@ -883,6 +895,7 @@ controllers.controller('fiscalController', function($scope, $q, connect, appstat
           var promise = generateInvoiceItems();
           promise.then(function(res) { 
             console.log("Invoice successfully generated", res);
+            $location.path('/sale_records/' + $scope.invoice_id);
           })
         }
       })
@@ -980,7 +993,7 @@ controllers.controller('fiscalController', function($scope, $q, connect, appstat
         columnDefs : [{field:'id', display:'id'},
                       {field:'cost', display:'total', cellFilter: 'currency'},
                       {field:'debitor_id', display:'debtor'},
-                      {field:'invoice_date', display:'date'},
+                      {field:'invoice_date', display:'date', cellFilter: 'date: "dd/MM/yyyy"'},
                       {field:'posted', display:'posted'}],
       data : 'invoice_model.data',
       //FIXME Search seems unpredictable - check filter settings
