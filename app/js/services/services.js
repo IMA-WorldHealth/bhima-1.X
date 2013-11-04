3ad401c05eed666ab1a924d560b61c1821869aeb
Steven Fountain <StevenFountain@live.co.uk>
2013-11-04 10:16:52 +0100

    Update invoice database with posted flag, basicPost method for connect and correct response for server (200)

--- app/js/services/services.js
+++ app/js/services/services.js
@@ -643,9 +643,15 @@
     }
 
 
+//    TODO reverse these two methods? I have no idea
     function basicPut(table, object) { 
       var format_object = {t: table, data: object};
       return $http.post('data/', format_object);
+    }
+
+    function basicPost(table, object, primary) {
+      var format_object = {t: table, data: object, pk: primary};
+      return $http.put('data/', format_object);
     }
 
     function packageModel(model, data) { 
@@ -714,6 +720,7 @@
       req : req,
       basicReq : basicReq,
       basicPut : basicPut,
+      basicPost : basicPost,
       journal : journal
     };
   });
