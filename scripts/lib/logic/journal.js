011d087359232846f9cd9517d128851d723130e9
Steven Fountain <StevenFountain@live.co.uk>
2013-11-05 09:52:43 +0100

    Journal replies to client with success message and ID, this is done by passing a reference, should be implemented with promises

--- scripts/lib/logic/journal.js
+++ scripts/lib/logic/journal.js
@@ -6,35 +6,38 @@ exports.poster = function(req, res) {
   insert(req.body, res);
 }
 
+// FIXME Temporary fix, pass a reference to res through to process - this should be done with a callback/ promise
 var insert = function(obj,res){  
   for(var i = 0; i<obj.length; i++){
     getData(obj[i], function(err, data){
       if(err) throw err;
-      process(data[0]); //verification et insertion eventuelle    
+      process(data[0], res); //verification et insertion eventuelle
     });
   } 
 }
 
-var process = function(data){
+var process = function(data, res){
   var date = util.convertToMysqlDate(new Date());
-  var callback = function(err, record){
-    if(record.length<1){
+  var callback = function (err, record) {
+    if (record.length < 1) {
       //insertion
-      var sql = db.insert('posting_journal', [{id:'',
-                                       enterprise_id: data.enterprise_id,
-                                       user_id: data.seller_id,
-                                       sale_id:data.id,
-                                       date:date,
-                                       description:data.note,
-                                       posted: 0
-                                      }
-                                     ]
-                          );
+      var sql = db.insert('posting_journal', [
+        {id: '',
+          enterprise_id: data.enterprise_id,
+          user_id: data.seller_id,
+          sale_id: data.id,
+          date: date,
+          description: data.note,
+          posted: 0
+        }
+      ]
+      );
       db.execute(sql, function (err, ans) {
-                       if (err) throw err;
-                       console.log("Post success", ans);
-                      });
-    }else{
+        if (err) throw err;
+        console.log("Post success", ans);
+        res.send({status: 200, insertId: ans.insertId});
+      });
+    } else {
       console.log('on insert pas');
     }
   }
