ce469dc3fde23728bbfc4f2bdc3f8d25a445d4c4
Steven Fountain <StevenFountain@live.co.uk>
2013-11-25 11:52:30 +0100

    Download accounts, settup for account creation form, added test i18n library

--- app/js/controllers/controllers.js
+++ app/js/controllers/controllers.js
@@ -3287,8 +3287,49 @@ controllers.controller('exchangeRateController', function ($scope, connect) {
 
 });
 
-controllers.controller('createAccountController', function($scope, $q) { 
+controllers.controller('createAccountController', function($scope, $q, connect) { 
   console.log("createAccountController initialised");
+
+  $scope.model = {};
+  $scope.model['accounts'] = {'data' : []};
+
+//  Request
+  var account_request = {
+    'tables' : {
+      'account' : {
+        'columns' : ["id", "account_txt", "account_type_id", "fixed"]
+      }
+    }
+  }
+
+  //  grid options
+  var grid;
+  var dataview;
+  var sort_column = "id";
+  var columns = [
+    {id: 'id', name: 'No.', field: 'id', sortable: true, maxWidth: 80},
+    {id: 'account_txt', name: 'Text', field: 'account_txt'},
+    {id: 'account_type_id', name: 'Type', field: 'account_type_id', maxWidth: 60},
+    {id: 'fixed', name: 'Fixed', field: 'fixed', maxWidth: 60}
+  ];
+  var options = {
+    enableCellNavigation: true,
+    enableColumnReorder: true,
+    forceFitColumns: true,
+    /*Bootstrap style row height*/
+    rowHeight: 35
+  };
+
+  function init() { 
+
+    connect.req(account_request).then(function(res) { 
+      $scope.model['accounts'] = res;
+      console.log($scope.model['accounts'].data);
+      grid = new Slick.Grid('#account_grid', $scope.model['accounts'].data, columns, options);
+    })
+  }
+
+  init();
 });
 
 })(angular);
