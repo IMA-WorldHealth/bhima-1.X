9812deb85224d65f0922765f6c8c23d548cfa74a
Steven Fountain <StevenFountain@live.co.uk>
2013-11-29 15:22:13 +0100

    Report to produce account realisation per fiscal year - client skeleton

--- scripts/lib/logic/reports.js
+++ scripts/lib/logic/reports.js
@@ -16,9 +16,37 @@ module.exports = (function(db) {
   }
 
   function finance() { 
+    //TODO discuss how wasteful requesting a full new table / calculation is when one could just get the new columns (new fiscal year etc.)
+    //could we calculate for all fiscal years every time, and then just pull from that - send all data at once, check version on client, if it matches current version on server don't re-send, just filter accordingly
     var deferred = q.defer();
+
+    //Condition is currently in the column select (very useful for selecting a variable number of columns - based on different querries)
+    //Condition could be in the ON of the join appended with an AND
     
-    var initial_query;
+    //this works for realisation 
+    //add budget 
+    //-LEFT JOIN budget 
+    //SUM(case when thing.fiscal_id = 2 then SUM(budget.budget) else 0 end)
+    var initial_query = "SELECT account.id," +
+                               "account.account_number," + 
+                               "SUM(case when posting_journal.fiscal_year_id = 2 then posting_journal.credit else 0 end)," +
+                               "SUM(case when posting_journal.fiscal_year_id = 2 then posting_journal.debit else 0 end)," +
+                               "posting_journal.period_id " +
+                        "FROM account " +
+                        "LEFT JOIN posting_journal " +
+                        "ON account.id = posting_journal.account_id " + 
+                        // "AND posting_journal.period_id = 3 " +
+                        "GROUP BY account.account_number;";
+
+    db.execute(initial_query, function(err, ans) {
+      if(err) {
+        console.log("finance report, initial query failed");
+        // deferred.reject(err);
+        return;
+      }
+      deferred.resolve(ans);
+    });
+
     return deferred.promise;
   }
 
