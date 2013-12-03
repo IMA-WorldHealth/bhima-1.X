4b7737e0d897877338a04717f93a6273054a1a52
Steven Fountain <StevenFountain@live.co.uk>
2013-12-03 15:45:22 +0100

    Removed legacy inserts in kpk, updated data with tshikaji info - this should be seperated, report sends request for fiscal columns

--- scripts/lib/logic/report.js
+++ scripts/lib/logic/report.js
@@ -2,8 +2,8 @@ var q = require('Q');
 
 module.exports = (function(db) { 
   'use strict'
-  
-  function generate(request, callback) { 
+
+  function generate(request, params, callback) { 
     /*summary 
     *   Route request for reports, if no report matches given request, return null  
     *   --This might be overly complex, it was much simpler but was decided one file was messy
@@ -12,17 +12,37 @@ module.exports = (function(db) {
       'finance' : finance
     }
 
-    route[request]().then(function(report) { 
+    route[request](params).then(function(report) { 
       callback(report);
     });
   }
 
-  function finance() { 
+  function finance(reportParameters) { 
+    var deferred = q.defer();
+    console.log('finance got', reportParameters, typeof(reportParameters));
+    
+    //finance expects a JSON object - invalid JSON will throw error
+    var financeParams = JSON.parse(reportParameters);
+
+    //expecting fiscal
+    var requiredFiscalYears;
+    //Need to get this from somwhere - 
+    var currentFiscalYear = 1;
+
+    //This is strange
+    if(financeParams) { 
+      requiredFiscalYears = financeParams.fiscal || [currentFiscalYear];
+    }
+
+    console.log('working with', requiredFiscalYears);
+
+    //will require information about current fiscal year - will need to reply with YTD
+
+    
     //TODO realisation should be a combination of credits - debits (or the other way around) - NOT two columns
 
     //TODO discuss how wasteful requesting a full new table / calculation is when one could just get the new columns (new fiscal year etc.)
     //could we calculate for all fiscal years every time, and then just pull from that - send all data at once, check version on client, if it matches current version on server don't re-send, just filter accordingly
-    var deferred = q.defer();
 
     //Condition is currently in the column select (very useful for selecting a variable number of columns - based on different querries)
     //Condition could be in the ON of the join appended with an AND
@@ -31,16 +51,7 @@ module.exports = (function(db) {
     //add budget 
     //-LEFT JOIN budget 
     //SUM(case when thing.fiscal_id = 2 then SUM(budget.budget) else 0 end)
-    var initial_query = "SELECT account.id," +
-                               "account.account_number," + 
-                               "SUM(case when posting_journal.fiscal_year_id = 1 then posting_journal.credit else 0 end) AS account_credit," +
-                               "SUM(case when posting_journal.fiscal_year_id = 1 then posting_journal.debit else 0 end) AS account_debit," +
-                               "posting_journal.period_id " +
-                        "FROM account " +
-                        "LEFT JOIN posting_journal " +
-                        "ON account.id = posting_journal.account_id " + 
-                        // "AND posting_journal.period_id = 3 " +
-                        "GROUP BY account.account_number;";
+    var initial_query = buildFinanceQuery(requiredFiscalYears);
 
     db.execute(initial_query, function(err, ans) {
       if(err) {
@@ -56,5 +67,30 @@ module.exports = (function(db) {
 
   return { 
     generate: generate
+  }
+
+  function buildFinanceQuery(requiredFiscalYears) { 
+
+    var query;
+    var fiscalColumns = "";
+
+    //add budget columns
+    requiredFiscalYears.forEach(function(year) { 
+      fiscalColumns += "(SUM(case when posting_journal.fiscal_year_id = "+year+" then posting_journal.credit else 0 end) - SUM(case when posting_journal.fiscal_year_id = "+year+" then posting_journal.debit else 0 end)) AS 'realisation "+year+"',";
+    });
+
+    query = "SELECT account.id," +
+                   "account.account_number," +
+                   fiscalColumns + 
+                   "posting_journal.period_id " +
+            "FROM account " +
+            "LEFT JOIN posting_journal " +
+            "ON account.id = posting_journal.account_id " + 
+            // "AND posting_journal.period_id = 3 " +
+            "GROUP BY account.account_number;";
+
+    return query;
+
+
   }
 });
