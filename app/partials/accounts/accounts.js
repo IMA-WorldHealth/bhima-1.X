e69a5f1a63998e75f705ba292c1f8e3d48d53aa4
Jonathan Niles <jonathanwniles@gmail.com>
2013-12-10 19:12:54 +0100

    lots of small style changes; chart of accounts formatters added; begun work on tree to reduce HTTP requests

--- app/partials/accounts/accounts.js
+++ app/partials/accounts/accounts.js
@@ -1,129 +1,139 @@
-angular.module('kpk.controllers').controller('accountController', function($scope, $q, $modal, connect, appstate) {
+angular.module('kpk.controllers')
+.controller('accountController', function($scope, $q, $modal, connect, appstate) {
+  // This module displays the chart of accounts
+  // It should have facility for editing, displaying
+  // and locking accounts
 
-    function getData () {
+  'use strict';
 
-      var account_defn, account_type_defn, 
-          account_store, type_store,
-          enterpriseid = appstate.get("enterprise").id;
+  function loadData () {
+    var imports = {};
 
-      account_defn = {
-        tables: {
-          'account': {
-            columns: ['enterprise_id', 'id', 'account_number', 'account_txt', 'account_category_id', 'account_type_id', 'fixed'],
-          },
-          'account_type': {
-            columns: ['type'] 
-          }
-        },
-        join: ['account.account_type_id=account_type.id'],
-        where: ["account.enterprise_id=" + enterpriseid],
-      };
+    imports.enterpriseid = appstate.get("enterprise").id;
 
-      account_type_defn = {
-        tables: {
-          'account_type' : {
-            columns: ['id', 'type']
-          }
-        },
-      };
-      
-      return $q.all([connect.req(account_defn), connect.req(account_type_defn)]);
-    }
-
-    function initGrid () {
-      var grid, columns, options, dataview;
-     
-      // dataview config 
-      dataview = new Slick.Data.DataView();
-
-      dataview.onRowCountChanged.subscribe(function (e, args) {
-        grid.updateRowCount();
-        grid.render(); 
-      });
-
-      dataview.onRowsChanged.subscribe(function (e, args) {
-        grid.invalidate(args.rows);
-        grid.render();
-      });
-
-      columns = [
-        {id: "account_number"       , name: "Account Number"   , field: "account_number", sortable: true},
-        {id: "txt"      , name: "Account Text"     , field: "account_txt", sortable: true},
-        {id: "type"     , name: "Account Type"     , field: "type"},
-        {id: "category" , name: "Account Category" , field: "account_category_id"},
-        {id: "fixed"    , name: "Fixed/Variable"   , field: "fixed"}
-        // {id: "locked"   , name: "Locked"           , field: "locked", sortable: true}
-      ];
-
-      options = {
-        editable             : true,
-        autoEdit             : false,
-        asyncEditorLoading   : false,
-        enableCellNavigation : true,
-        forceFitColumns      : true
-      };
-
-      grid = new Slick.Grid("#kpk-accounts-grid", dataview, columns, options);
-
-      function sorter (e, args) {
-        var field = args.sortCol.field;
-        function sort (a, b) { return (a[field] > b[field]) ? 1 : -1; }
-        dataview.sort(sort, args.sortAsc);
-        grid.invalidate();
-      }
-
-      grid.onSort.subscribe(sorter);
-
-      return {grid: grid, dataview: dataview};
-
-    }
-
-    var promise = getData();
- 
-    $scope.models = {};
-    $scope.stores = {};
-    var dataview, grid;
-
-    promise.then(function (a) {
-      $scope.models.accounts = a[0].data;
-      $scope.stores.accounts = a[0];
-      $scope.models.types = a[1].data;
-      var setup = initGrid(); 
-      grid = setup.grid;
-      $scope.dataview = setup.dataview;
-      
-      $scope.$watch('models.accounts', function () {
-        console.log("model changed!");
-        $scope.dataview.setItems($scope.models.accounts);
-      });
-    });
-
-    $scope.create = function () {
-      var instance = $modal.open({
-        templateUrl: "/partials/accounts/templates/chart-modal.html",
-        backdrop: true,
-        controller: function ($scope, $modalInstance, types) {
-          // work around because of issue #969
-          $scope.$modalInstance = $modalInstance;
-          $scope.types = types;
-        },
-        resolve: {
-          types: function() {
-            return $scope.models.types;
-          },
-        }
-      });
-
-      instance.result.then(function(account) {
-        account.id = $scope.stores.accounts.generateid();
-        account.enterprise_id = appstate.get('enterprise').id;
-        $scope.stores.accounts.post(account);
-        // TODO: This works, but have to refresh grid to make it work.
-        $scope.dataview.setItems($scope.models.accounts);
-      }, function() {});
+    imports.account = {
+      tables: {
+        'account': {columns: ['enterprise_id', 'id', 'account_number', 'account_txt', 'account_category_id', 'account_type_id', 'fixed']},
+        'account_type': {columns: ['type']}
+      },
+      join: ['account.account_type_id=account_type.id'],
+      where: ["account.enterprise_id=" + imports.enterpriseid],
     };
 
+    imports.account_type = {tables: {'account_type' : {columns: ['id', 'type']}}};
+    imports.account_category = {tables: {'account_category' : {columns : ['id', 'title']}}};
+    
+    return $q.all([connect.req(imports.account), connect.req(imports.account_type), connect.req(imports.account_category)]);
+  }
+
+  function initGrid () {
+    var grid, columns, options, dataview;
+   
+    // dataview config 
+    dataview = new Slick.Data.DataView();
+
+    dataview.onRowCountChanged.subscribe(function (e, args) {
+      grid.updateRowCount();
+      grid.render(); 
+    });
+
+    dataview.onRowsChanged.subscribe(function (e, args) {
+      grid.invalidate(args.rows);
+      grid.render();
+    });
+
+    columns = [
+      {id: "account_number" , name: "Account Number", field: "account_number", sortable: true},
+      {id: "txt"      , name: "Account Text"     , field: "account_txt", sortable: true},
+      {id: "type"     , name: "Account Type"     , field: "type"},
+      {id: "category" , name: "Account Category" , field: "account_category_id", sortable: true, formatter: formatAccountCategory},
+      {id: "fixed"    , name: "Fixed/Variable"   , field: "fixed", formatter: formatFixed},
+      {id: "locked"   , name: "Locked"           , field: "locked", sortable: true, formatter: formatLocked}
+    ];
+
+    options = {
+      editable             : true,
+      autoEdit             : false,
+      asyncEditorLoading   : false,
+      enableCellNavigation : true,
+      forceFitColumns      : true
+    };
+
+    grid = new Slick.Grid("#kpk-accounts-grid", dataview, columns, options);
+
+    function sorter (e, args) {
+      var field = args.sortCol.field;
+      function sort (a, b) { return (a[field] > b[field]) ? 1 : -1; }
+      dataview.sort(sort, args.sortAsc);
+      grid.invalidate();
+    }
+
+    grid.onSort.subscribe(sorter);
+
+    function formatFixed (row, col, item) {
+      return item === 1 ? "Fixed" : "Variable";
+    }
+
+    function formatLocked (row, col, item) {
+      return item === 1 ? "True" : "False";
+    }
+
+    function formatAccountCategory (row, col, item) {
+      return $scope.stores.account_category ? $scope.stores.account_category.get(item).title : item;
+    }
+
+    return {grid: grid, dataview: dataview};
+
+  }
+
+  var promise = loadData();
+ 
+  $scope.models = {};
+  $scope.stores = {};
+  var dataview, grid;
+
+  promise.then(function (a) {
+    $scope.stores.accounts = a[0];
+    $scope.models.accounts = a[0].data;
+    $scope.stores.types = a[1];
+    $scope.models.types = a[1].data;
+    $scope.stores.account_category = a[2];
+    var setup = initGrid(); 
+    grid = setup.grid;
+    $scope.dataview = setup.dataview;
+    
+    $scope.$watch('models.accounts', function () {
+      console.log("model changed!");
+      $scope.dataview.setItems($scope.models.accounts);
+    });
   });
+
+  $scope.create = function () {
+    var instance = $modal.open({
+      templateUrl: "/partials/accounts/templates/chart-modal.html",
+      backdrop: true,
+      controller: function ($scope, $modalInstance, types) {
+        // work around because of issue #969
+        $scope.$modalInstance = $modalInstance;
+        $scope.types = types;
+      },
+      resolve: {
+        types: function() {
+          return $scope.models.types;
+        },
+      }
+    });
+
+    instance.result.then(function(account) {
+      account.id = $scope.stores.accounts.generateid();
+      account.enterprise_id = appstate.get('enterprise').id;
+      $scope.stores.accounts.post(account);
+      // TODO: This works, but have to refresh grid to make it work.
+      $scope.dataview.setItems($scope.models.accounts);
+    }, function() {});
+  };
+
+});
 
 angular.module('kpk.controllers').controller('accountFormController', function ($scope) {
       $scope.account = {};
--- app/partials/kapok/tree.js
+++ app/partials/kapok/tree.js
@@ -1,56 +1,62 @@
-angular.module('kpk.controllers').controller('treeController', function($scope, $q, $location, appcache, kpkConnect) {    
-    var deferred = $q.defer();
-    var result = getRoles();
-    $scope.treeData = [];
-    var cb = function(role, units){
-      var element = {};
-      element.label = role.name;
-      element.id = role.id;
-      element.children = [];
+angular.module('kpk.controllers')
+.controller('treeController', function($scope, $q, $location, appcache, kpkConnect) {
+  // This module loads the tree.
+  // Rewrite Dec 12th so that tree only sends one XHR request,
+  // rather than several recursively for optimisation purposes.
+  'use strict';
 
-//      Set default element state
-      element.collapsed = true;
-//      console.log(appcache.checkDB());
+  var deferred = $q.defer();
+  var result = getRoles();
+  $scope.treeData = [];
+  function cb (role, units) {
+    var element = {};
+    element.label = role.name;
+    element.id = role.id;
+    element.children = [];
 
-      for(var i = 0; i<units.length; i++){
-        element.children.push({"label":units[i].name, "id":units[i].id, "p_url":units[i].p_url, "children":[]});
-      }
-      $scope.treeData.push(element);
+//    Set default element state
+    element.collapsed = true;
+//    console.log(appcache.checkDB());
 
-    };
-
-    result.then(function(values){
-      for(var i = 0; i<values.length; i++){
-        getChildren(values[i], cb);
-      }
-    });
- 
-    
-    $scope.$watch('navtree.currentNode', function( newObj, oldObj ) {
-        if( $scope.navtree && angular.isObject($scope.navtree.currentNode) ) {
-            $location.path($scope.navtree.currentNode.p_url);
-        }
-    }, true);
-
-    function getRoles(){
-      var request = {}; 
-      request.e = [{t : 'unit', c : ['id', 'name']}];
-      request.c = [{t:'unit', cl:'parent', v:0, z:'='}];
-      kpkConnect.get('/tree?',request).then(function(data) { 
-        deferred.resolve(data);
-      });
-      return deferred.promise;
+    for(var i = 0; i<units.length; i++){
+      element.children.push({"label":units[i].name, "id":units[i].id, "p_url":units[i].p_url, "children":[]});
     }
+    $scope.treeData.push(element);
 
-    function getChildren(role, callback){
-      var request = {}; 
-      request.e = [{t : 'unit', c : ['id', 'name', 'url']}];
-      request.c = [{t:'unit', cl:'parent', v:role.id, z:'='}];
-      kpkConnect.get('/tree?',request).then(function(data) {
-          callback(role, data); 
-        
-      });
+  }
 
-    };
+  result.then(function(values){
+    for(var i = 0; i<values.length; i++){
+      getChildren(values[i], cb);
+    }
+  });
+ 
+  
+  $scope.$watch('navtree.currentNode', function(newObj, oldObj) {
+    if ($scope.navtree && angular.isObject($scope.navtree.currentNode)) {
+      $location.path($scope.navtree.currentNode.p_url);
+    }
+  }, true);
+
+  function getRoles () {
+    var request = {}; 
+    request.e = [{t : 'unit', c : ['id', 'name']}];
+    request.c = [{t:'unit', cl:'parent', v:0, z:'='}];
+    kpkConnect.get('/tree?',request).then(function(data) { 
+      deferred.resolve(data);
+    });
+    return deferred.promise;
+  }
+
+  function getChildren(role, callback){
+    var request = {}; 
+    request.e = [{t : 'unit', c : ['id', 'name', 'url']}];
+    request.c = [{t:'unit', cl:'parent', v:role.id, z:'='}];
+    kpkConnect.get('/tree?',request).then(function(data) {
+        callback(role, data); 
+      
+    });
+
+  }
 
 });
