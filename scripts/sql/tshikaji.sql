67cdb56c181be819319b01991ce44e79c6635062
Steven Fountain <StevenFountain@live.co.uk>
2013-12-10 09:44:25 +0100

    adding priviledge

--- scripts/sql/tshikaji.sql
+++ scripts/sql/tshikaji.sql
@@ -92,7 +92,8 @@ INSERT INTO `unit` VALUES
   (44,'Balance vs. Budget', '',10, 0, 'partials/reports/balance_budget', 'reports/balance_budget'),
   (45,'Price List', 'Configure price lists!', 11, 0, 'partials/price_list', 'inventory/price_list'),
   (46,'Exchange Rate', 'Set todays exchange rate!', 1, 0, 'partials/exchange_rate', 'exchange_rate'),
-  (47, 'Transaction Report', '', 10, 0, 'partials/reports/transaction_report', 'reports/transaction_report');
+  (47, 'Transaction Report', '', 10, 0, 'partials/reports/transaction_report', 'reports/transaction_report'),
+  (48, 'View inventories items', '', 11, 0, 'partials/iventory/view', 'view');
 
 
 insert into `permission` values
@@ -145,7 +146,8 @@ insert into `permission` values
   (47, 44, 1),
   (48, 45, 1),
   (49, 46, 1),
-  (50, 47, 2);
+  (50, 47, 2),
+  (51, 48, 2);
 
 
 
