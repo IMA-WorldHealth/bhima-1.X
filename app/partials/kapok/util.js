0a18290227f88e0f0ed26b5ede3380b74bd5c160
Steven Fountain <StevenFountain@live.co.uk>
2013-12-11 13:48:43 +0100

    merging

--- app/partials/kapok/util.js
+++ app/partials/kapok/util.js
@@ -1,12 +1,15 @@
-angular.module('kpk.controllers').controller('utilController', function($scope, $translate) { 
+angular.module('kpk.controllers')
+.controller('utilController', function($scope, $translate, message) { 
   ////
   // summary: 
   //  Responsible for all utilities (buttons/ selects etc.) on the application side bar
   /////
+  'use strict';
+
 
   $scope.toggleTranslate = function toggleTranslate(lang_key) { 
     $translate.uses(lang_key);
-  }
-  
+  };
+
   //removed select code - downloaded Enterprises/Fiscal Years and populated selects
 });
