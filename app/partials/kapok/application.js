e684fd58fea864a1eb7b4815dcb6aadd6007289d
Steven Fountain <StevenFountain@live.co.uk>
2013-12-11 19:55:00 +0100

    remove calls to removed appcache

--- app/partials/kapok/application.js
+++ app/partials/kapok/application.js
@@ -3,17 +3,17 @@ angular.module('kpk.controllers').controller('appController', function($scope, $
     //lookup users language preference 
     
     //Cache URL's to maintain user session
-    var url = $location.url();
+    // var url = $location.url();
 
-    //Assuming initial page load
-    if (url === '') {
+    // //Assuming initial page load
+    /*if (url === '') {
       //only navigate to cached page if no page was requested
       appcache.getNav().then(function(res) {
         if(res) {
           $location.path(res);
         }
       });
-    }
+    }*/
 
     var default_enterprise, default_fiscal_year;
     //donwload and set enterprise and fiscal year - this should not be done here
@@ -28,9 +28,9 @@ angular.module('kpk.controllers').controller('appController', function($scope, $
       if(default_fiscal_year) appstate.set('fiscal', default_fiscal_year);
     })
     
-    $scope.$on('$locationChangeStart', function(e, n_url) { 
+    /*$scope.$on('$locationChangeStart', function(e, n_url) { 
       //Split url target - needs to be more general to allow for multiple routes?
       var target = n_url.split('/#')[1];
       if(target) appcache.cacheNav(target);
-    });
+    });*/
 });
