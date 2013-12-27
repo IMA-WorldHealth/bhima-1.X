a43c3af3aae3e1fbfb813a618d52dde12c56ee7e
Jonathan Niles <jonathanwniles@gmail.com>
2013-12-27 08:04:35 +0100

    work begun on location configuration

--- app/partials/location/location.js
+++ app/partials/location/location.js
@@ -1,5 +1,23 @@
 angular.module('kpk.controllers')
-.controller('locationCtrl', function ($scope, connect) {
+.controller('locationCtrl', function ($scope, $q, connect) {
   'use strict';
+
+  var imports = {};
+
+  imports.province = {tables: { 'province' : { columns : ['id', 'name']}}};
+  imports.sector = {tables: { 'sector' : { columns : ['id', 'name']}}};
+  imports.village = {tables: { 'village' : { columns : ['id', 'name']}}};
+  imports.country = {tables: { 'country' : { columns : ['id', 'country_en']}}};
+
+  function init () {
+    $q.all([
+      connect.req(imports.province),
+      connect.req(imports.sector),
+      connect.req(imports.village),
+      connect.req(imports.country)
+    ]).then(function (array) {
+      // ..
+    });
+  }
     
 });
