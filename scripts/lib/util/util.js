a67d9fadf6112a764b4952a92e3ce0a02b7206c8
Jonathan Niles <jonathanwniles@gmail.com>
2014-01-02 11:35:26 +0100

    added util.js for utility functions, cleaning database code
    Former-commit-id: 5a139087605911b1a687205940a810676ca7cace [formerly 51a0e34337ae1bbdb6836e201da140ba14f9dd42]
    Former-commit-id: 2d548b6e8484ff0c3d608121ff4aa3c4296542fa

--- scripts/lib/util/util.js
+++ scripts/lib/util/util.js
@@ -1,5 +1,28 @@
+// Module: lib/util/util.js
+
+// This modules adds utilities available throughout the
+// server.
 
 
+module.exports = {
+  
+  isInt : function (i) { return Math.floor(i) === Number(i); },
+
+  // this also works for hexadecimal ('0x12')
+  isNumber: function (n) { return !Number.isNaN(Number(i)); },
+
+  isArray: function (arr) { return Object.prototype.toString.call(arr) == '[object Array]'; },
+
+  isString: function (str) { return typeof str == 'string'; },
+
+  isObject: function (obj) { return Object.prototype.toString.call(obj) == '[object Object]'; }
+
+
+};
+
+
+
+/*
 exports.convertToMysqlDate = function(dateString){
 	return new Date().toMySqlDate(dateString);
 }
@@ -14,7 +37,8 @@ Date.prototype.toMySqlDate = function (dateParam) {
 
 	jour = String(date.getDate());
 	if (jour.length === 1) {
-	  jour = "0" + jour;
+    jour = "0" + jour;
 	}      
 	return annee + "-" + mois + "-" + jour;
 };
+*/
