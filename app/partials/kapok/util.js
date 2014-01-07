1e94197fe71db66a8eacb037de9e5b01d4a2abb0
Steven Fountain <StevenFountain@live.co.uk>
2014-01-07 06:45:13 +0000

    Load users langauge selection by default(experimental), cleaned up application code
    Former-commit-id: 8bd8076205f96c3135cfb0197288ce95251ea929 [formerly 760e2714862b0b973faa48e552311ebf4445c3ab]
    Former-commit-id: 74722b9498496229c13d03eb4695229f3dcec491

--- app/partials/kapok/util.js
+++ app/partials/kapok/util.js
@@ -1,18 +1,17 @@
 angular.module('kpk.controllers')
-.controller('utilController', function($scope, $translate, messenger) { 
+.controller('utilController', function($scope, $translate, appcache, messenger) { 
   ////
   // summary: 
   //  Responsible for all utilities (buttons/ selects etc.) on the application side bar
   /////
-  'use strict';
-
-
+  var MODULE_NAMESPACE = 'util';
+  var cache = new appcache(MODULE_NAMESPACE); 
+  
   $scope.toggleTranslate = function toggleTranslate(lang_key) { 
-    messenger.push({type: 'success', msg: 'clicked translate!'});
-
+    messenger.push({type: 'success', msg: 'clicked translate!'}); 
     $translate.uses(lang_key);
+    
+    //store the current language key to be used in the next session 
+    cache.put('language', {current: lang_key});
   };
-
-
-  //removed select code - downloaded Enterprises/Fiscal Years and populated selects
 });
