950ecad9f8364209a27dbec6b6e5fb3d073d57d9
Steven Fountain <StevenFountain@live.co.uk>
2014-01-09 12:50:59 +0000

    Removed translate buttons, controller logic for settings.js, removed translate function from util.js

--- app/partials/kapok/util.js
+++ app/partials/kapok/util.js
@@ -4,14 +4,5 @@ angular.module('kpk.controllers')
   // summary: 
   //  Responsible for all utilities (buttons/ selects etc.) on the application side bar
   /////
-  var MODULE_NAMESPACE = 'util';
-  var cache = new appcache(MODULE_NAMESPACE); 
-  
-  $scope.toggleTranslate = function toggleTranslate(lang_key) { 
-    messenger.push({type: 'success', msg: 'clicked translate!'}); 
-    $translate.uses(lang_key);
-    
-    //store the current language key to be used in the next session 
-    cache.put('language', {current: lang_key});
-  };
+ 
 });
