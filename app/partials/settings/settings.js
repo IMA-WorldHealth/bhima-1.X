ce0a024e2a280f791458b9b74c8c156aa7b39d55
Steven Fountain <StevenFountain@live.co.uk>
2014-01-09 12:50:59 +0000

    Removed translate buttons, controller logic for settings.js, removed translate function from util.js
    Former-commit-id: 5b96b735ba50991c05a678376ae7a99617e3c179 [formerly 950ecad9f8364209a27dbec6b6e5fb3d073d57d9]
    Former-commit-id: 15cb34ddb590ca9705f965eb6c2534a455ccd079

--- app/partials/settings/settings.js
+++ app/partials/settings/settings.js
@@ -1,3 +1,17 @@
-angular.module('kpk.controllers').controller('settingsController', function($scope, connect) { 
+angular.module('kpk.controllers').controller('settingsController', function($scope, connect, $translate, appcache, messenger) { 
   console.log('settings controller initialised');
+
+  var MODULE_NAMESPACE = 'util';
+  var cache = new appcache(MODULE_NAMESPACE);
+
+  cache.fetch('language')
+  .then(function(res) { 
+    if(res) $scope.settings = {language: res.current};
+  });
+
+  $scope.updateLanguage = function updateLanuage(key) { 
+    $translate.uses(key);
+    cache.put('language', {current: key});
+    messenger.push({type: 'success', msg: 'Language preference updated: ' + key});
+  };
 });
