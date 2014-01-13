5496ea1736ada4b5256149dc473a6fd96a825efb
Dedrick Kitamuka <kitamuka@gmail.com>
2014-01-13 10:13:22 +0100

    account statement by period continues
    Former-commit-id: 5d5345ad8f84e75722ff841923f4aecf3aa3ae24 [formerly f4066710bfe8cac9b9d4ea60e968960864bddcbb]
    Former-commit-id: afbff9879f922f636d7d8f594eed5a0cf1bec9bf

---
+++ app/partials/reports/account_statement/account_statement.js
@@ -0,0 +1,15 @@
+angular.module('kpk.controllers').controller('reportAccountStatementCtrl', function($scope, $q, $filter, connect, kpkUtilitaire){
+	console.log('on est la !');
+
+	//variables
+	
+	
+
+
+	//fonctions
+
+	
+
+	//invocations
+
+});
