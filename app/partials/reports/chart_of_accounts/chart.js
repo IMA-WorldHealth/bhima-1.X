2765c29f313231bd9c7ba5c55735a6c3bd1e3329
Steven Fountain <StevenFountain@live.co.uk>
2014-01-13 06:46:21 +0000

    Chart of accounts styling, parse for depth
    Former-commit-id: d3ded2d5ddb496a0be1fb5375b259f1d279cbfc7 [formerly 2b828d058e5e9a44e61e0ab2eb03adfcf47710a8]
    Former-commit-id: f135b8b88acbff83e8eea3b818988971a60e48e9

--- app/partials/reports/chart_of_accounts/chart.js
+++ app/partials/reports/chart_of_accounts/chart.js
@@ -1,11 +1,13 @@
 angular.module('kpk.controllers').controller('accountsReport', function($scope, $q, connect, messenger) { 
   console.log('accountReport controller initialised');
   
+  //Parse all accounts - add depth level etc. apply styling based on this
   $scope.model = {};
   var accountsQuery = { 
-    tables: { 
+    identifier: 'account_number',
+    tables: {
       'account': { 
-        columns: ["id", "account_txt", "account_number"]
+        columns: ["id", "account_txt", "account_number", "parent", "account_type_id"]
       }
     }
   }
@@ -20,7 +22,8 @@ angular.module('kpk.controllers').controller('accountsReport', function($scope, 
   function accountsReport() { 
     fetchRequests()
     .then(function(res) { 
-      messenger.push({type: 'success', msg: 'Fetched all accounts'});    
+      messenger.push({type: 'success', msg: 'Fetched all accounts'}); 
+      parseAccountDepth($scope.model['account'].data);
     });
   }
 
@@ -42,5 +45,33 @@ angular.module('kpk.controllers').controller('accountsReport', function($scope, 
     return deferred.promise; 
   } 
 
+  function parseAccountDepth(accounts) { 
+    var ROOT_NODE = 0; 
+    console.log('parsing account depth', accounts);
+  
+    accounts.forEach(function(account) { 
+      var parent, depth;
+
+      if(account.parent === ROOT_NODE) {  
+        account.depth = 0;
+        return;
+      }
+  
+      //TODO if parent.depth exists, increment and kill the loop
+      parent = $scope.model['account'].get(account.parent);
+      depth = 0;
+      while(parent) { 
+        depth++;
+        parent = $scope.model['account'].get(parent.parent);
+      }
+      account.depth = depth;
+    });
+
+    console.log('parsed', accounts);
+  }
+
+  $scope.buildDepthStyle = function buildDepthStyle(account) { 
+    return { "padding-left" : (account.depth * 30) + "px" }; 
+  };
   accountsReport();
 });
