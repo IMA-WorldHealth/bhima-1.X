ff50923a9e655703f63c0e472002eb3d568a295a
Steven Fountain <StevenFountain@live.co.uk>
2014-01-14 14:52:33 +0000

    Finance report table column selection
    Former-commit-id: 74508cc7eedcb2b9e30ca5a850b27f77873e967d [formerly 1642d6387f787024404c010ce625c8d73ee4bf7b]
    Former-commit-id: 5642e4d8d27a86a4a72bb05645ed4ccb7cc13894

--- app/partials/reports/finance/finance_report.js
+++ app/partials/reports/finance/finance_report.js
@@ -1,12 +1,11 @@
 //TODO Two step, download fiscal years, and then populate finance query, validate service will need to be updated
-angular.module('kpk.controllers').controller('reportFinance', function($scope, $q, connect, appstate, validate) {
-  var dependencies = {}, reportYears = [];
+angular.module('kpk.controllers').controller('reportFinance', function($scope, $q, connect, appstate, validate, messenger) {
+  var dependencies = {}, tableDefinition = {columns: [], options: []};
 
   dependencies.finance = { 
     required : true,
     identifier: "account_number",
-    query : '/reports/finance/?' + JSON.stringify({fiscal: [1, 2]}),
-    KPKAPIHISRequest : false
+    query : '/reports/finance/',
   };
  
   dependencies.fiscal = { 
@@ -24,19 +23,10 @@ angular.module('kpk.controllers').controller('reportFinance', function($scope, $
 
   function reportFinance(model) { 
     $scope.model = model;
-    parseAccountDepth($scope.model['finance']);
-    settupTable(reportYears);
+    parseAccountDepth($scope.model.finance);
+    settupTable($scope.model.fiscal.data);
   }
-
-  function settupTable(columnData) { 
-    var columnDefinition = [];
-    columnData.forEach(function(year) { 
-      if(year.toggle) columnDefinition.push({id: year.id, name: "Year " + year.id + " Budget", key: "budget_" + year.id});
-      if(year.toggle) columnDefinition.push({id: year.id, name: "Year " + year.id + " Realisation", key: "realisation_" + year.id});
-    });
-    $scope.columnDefinition = columnDefinition;
-  }
-
+ 
   function parseAccountDepth(accountModel) { 
     var accounts = accountModel.data; 
     
@@ -50,20 +40,40 @@ angular.module('kpk.controllers').controller('reportFinance', function($scope, $
       account.depth = depth;
     }); 
   }
-
-  function ReportYears() { 
-   
-    //Toggle must remove clumns from table column definition, ordering must be accounted for 
-    function toggle(year) { 
-      year.toggle = !year.toggle;
-    }
-  } 
-
-  function buildFinanceQuery() { 
-
+ 
+  //TODO settupTable, toggleColumn, pushColumn and popColumn all improved and encapsulated within Table object, numColumns = 2, col 1 - budget, col 2 - realisation
+  function settupTable(columnData) { 
+    columnData.forEach(function(year, index) { 
+      var tableOption = {active: false, id: year.id, index: index};
+      tableDefinition.options.push(tableOption);
+      toggleColumn(tableOption); 
+    });
   }
   
-  $scope.basicPrint = function basicPrint() { 
-    print();
-  };
+  function toggleColumn(yearOption) { 
+    var active = yearOption.active = !yearOption.active;
+    if(active) return pushColumn(yearOption);     
+    popColumn(yearOption);
+  }
+
+  function pushColumn(year) { 
+    console.log(year, 'insert at', year.index);
+    tableDefinition.columns.splice(year.index * 2, 0, {id: year.id, name: "Year " + year.id + " Budget", key: "budget_" + year.id});
+    tableDefinition.columns.splice(year.index * 2, 0, {id: year.id, name: "Year " + year.id + " Realisation", key: "realisation_" + year.id});
+  }
+
+  function popColumn(year) { 
+
+    //Avoid replacing the entire array, redrawing the DOM
+    tableDefinition.columns.forEach(function(column, index) {
+      if(column.id === year.id) tableDefinition.columns.splice(index, 2);
+    });
+  }
+
+ 
+  function basicPrint() { print(); } 
+ 
+  $scope.toggleYear = toggleColumn;
+  $scope.basicPrint = basicPrint;
+  $scope.tableDefinition = tableDefinition;
 });
