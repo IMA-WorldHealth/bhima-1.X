eac8bb5dcd77652b0e908cb353c6d032184692e6
Steven Fountain <StevenFountain@live.co.uk>
2014-01-17 20:13:24 +0000

    Cleaned up invoice structure, updated gruntfile to watch partial CSS
    Former-commit-id: 05d3306ec364cff91cd2b3658dec04c4f89ae8cd [formerly ec76e1f8a870db4a2f798deaa0224b4dc8e96e76]
    Former-commit-id: 866747b6a9e564fef767fc4161f10657ef53763e

--- app/partials/invoice/incoice.js
+++
@@ -1,6 +0,0 @@
-angular.module('kpk.controllers').controller('invoice', function($scope, $q, validate) { 
-  console.log('invoice creation controller');
-
-
-
-});
---
+++ app/partials/invoice/invoice.js
@@ -0,0 +1,10 @@
+angular.module('kpk.controllers').controller('invoice', function($scope, $q, validate) { 
+  console.log('invoice');
+
+  var dependencies = {};
+
+  dependencies.sale = { 
+    required: true
+    //...
+  }
+});
