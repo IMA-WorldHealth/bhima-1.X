60c59e8881d6eacece9b3df78221ed0236c50ac6
Steven Fountain <StevenFountain@live.co.uk>
2014-01-18 10:15:00 +0000

    Invoice controller, fetching dependencies requires validation multiple pass process()
    Former-commit-id: 70dbd8b5da57146c54f05a56c6725ba02c94dae9 [formerly 9b7147ba284a5f4189107f0cb96a7e1384f04249]
    Former-commit-id: c8a237d23301611771a570c4508965353252af27

--- app/partials/invoice/invoice.js
+++ app/partials/invoice/invoice.js
@@ -1,25 +1,57 @@
+//TODO Debtor table currently has no personal information - this strictly ties debtors to patients (or some existing table) - a reverse lookup from debtor/ creditor ID to recipient is needed
 angular.module('kpk.controllers').controller('invoice', function($scope, $routeParams, $q, validate) { 
-  console.log('invoice');
-  
-  var dependencies = {}, origin = $routeParams.origin, invoiceId = $routeParams.invoiceId;
-  
-  dependencies.patient = { 
-    required: true
-  };
+  var dependencies = {}, origin = $routeParams.originId, invoiceId = $routeParams.invoiceId;
+  if(!(origin && invoiceId)) throw new Error('Invalid parameters');
 
-  dependencies.sale = { 
-    required: true
-    //...
-  };
-
-  dependencies.saleItem = {
-    required: true
+  dependencies.recipient = { 
+    required: true 
   };
   
-  function invoice(model) { 
+  dependencies.invoice = { 
+    required: true,
+    query: { 
+      tables: {},
+      where: [origin + '.id=' + invoiceId]
+    }
+  };
+  dependencies.invoice.query.tables[origin] = {
+    columns: ['id', 'cost', 'currency_id', 'debitor_id', 'seller_id', 'invoice_date', 'note']
+  };
+  
+  //TODO sale_item hardcoded - have a map form originId to table name, item table name, recipient table name
+  dependencies.invoiceItem = {
+    required: true,
+    query: {
+      tables: {},
+      where: ['sale_item.sale_id=' + invoiceId]
+    }
+  };
+  dependencies.invoiceItem.query.tables['sale_item'] = { 
+    columns: ['id', 'quantity', 'unit_price', 'total']
+  }
+ 
+  validate.process(dependencies, ['invoice', 'invoiceItem']).then(updateDependencies);
+   
+  function updateDependencies(model) { 
     
+    //If all has gone well (this should be asserted by validate) there is only one sale
+    $scope.invoice = model.invoice.data[0];
+
+    dependencies.recipient.query = { 
+      tables: {},
+      where: ['patient.debtor_id=' + $scope.invoice.debitor_id]
+    }
+    dependencies.recipient.query.tables['patient'] = { 
+      columns: ['first_name', 'last_name', 'dob', 'location_id']
+    };
+    
+    console.log(model);
+    // return validate.process(dependencies).then(invoice);
+  }
+
+  function invoice(model) { 
+ 
     //Expose to template
     $scope.model = model; 
   }
-
 });
