af70b0b030ca2e34c4c53f4017dab7cef6552aa9
Steven Fountain <StevenFountain@live.co.uk>
2014-01-18 09:08:40 +0000

    Application routes using new (angular 1.8.2) optional routes, updated related records

--- app/partials/invoice/invoice.js
+++ app/partials/invoice/invoice.js
@@ -1,10 +1,25 @@
-angular.module('kpk.controllers').controller('invoice', function($scope, $q, validate) { 
+angular.module('kpk.controllers').controller('invoice', function($scope, $routeParams, $q, validate) { 
   console.log('invoice');
-
-  var dependencies = {};
+  
+  var dependencies = {}, origin = $routeParams.origin, invoiceId = $routeParams.invoiceId;
+  
+  dependencies.patient = { 
+    required: true
+  };
 
   dependencies.sale = { 
     required: true
     //...
+  };
+
+  dependencies.saleItem = {
+    required: true
+  };
+  
+  function invoice(model) { 
+    
+    //Expose to template
+    $scope.model = model; 
   }
+
 });
