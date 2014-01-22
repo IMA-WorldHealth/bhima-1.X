544af80adfb3886a92673b16ecaf64480b657bf5
jniles <jonathanwniles@gmail.com>
2014-01-22 16:39:48 +0100

    added sanitization scripts; tests for sanitation
    Former-commit-id: 399e70b190910235b3936b023e4e26521c9ce94d [formerly 3be9a898f9daf24e1464f6e61c002263f8fcfd5e]
    Former-commit-id: f52dc75b3f0c82192431f507a8978a85ce3127df

--- scripts/lib/database/parser.js
+++ scripts/lib/database/parser.js
@@ -1,13 +1,6 @@
 // builds the sql queries that a store will use
 
-// sanitization utils
-var sanitize = {
-  escapeid : function (id) { return ['`', id, '`'].join(''); },
-  escape: function (str) { return (!Number.isNaN(Number(str)) || ~str.indexOf('"')) ? str : '"' + str + '"'; },
-  isInt : function (i) { return (Math.floor(n) === Number(n)); },
-  isIn : function (s) {  return String(s).indexOf('(') > -1; },
-  isArray : function (a) { return a.length && Object.prototype.toString.call(a) === '[object Array]'; }
-};
+var sanitize = require('../util/sanitize.js');
 
 //module: Parser 
 module.exports = (function (options) {
