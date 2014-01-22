194cf40126c7f0dc9a77b7b014d7baeb7975d5f3
Steven Fountain <StevenFountain@live.co.uk>
2014-01-22 10:11:26 +0100

    Create credit note object, populate with sale/ debtor data. Unit layout/ styling
    Former-commit-id: f9912c9770e3cf6c14f21b761458b97cdda62f40 [formerly b028b3e99c54c224bf58c64649cc5bebffd4151e]
    Former-commit-id: 8cc8415008e4588a199258525fa0b431d5642d09

--- app/partials/credit_note/credit_note.js
+++ app/partials/credit_note/credit_note.js
@@ -1,14 +1,33 @@
-angular.module('kpk.controllers').controller('creditNote', function($scope, $routeParams, validate) { 
-  var invoiceId = $routeParams.invoiceId; 
-  var dependencies = {};
+angular.module('kpk.controllers').controller('creditNote', function($scope, $routeParams, $filter, validate) { 
+  var invoiceId = $routeParams.invoiceId, dependencies = {};
+
   dependencies.sale = { 
     required: true, 
     query: { 
       tables: { 
         sale: { 
           columns: ['id', 'cost', 'debitor_id', 'invoice_date', 'note'] 
+        },
+        patient: { 
+          columns: ['first_name', 'last_name']
         }
-      }
+      },
+      join: ['sale.debitor_id=patient.debitor_id']
+    }
+  }
+
+  dependencies.saleItem = { 
+    required: true, 
+    query: { 
+      tables: { 
+        sale_item: { 
+          columns: ['id', 'inventory_id', 'quantity', 'unit_price', 'total']
+        },
+        inventory: { 
+          columns: ['code', 'text']
+        }
+      },
+      join: ['sale_item.inventory_id=inventory.id']
     }
   }
   
@@ -16,13 +35,30 @@ angular.module('kpk.controllers').controller('creditNote', function($scope, $rou
 
   function buildSaleQuery() { 
     dependencies.sale.query.where = ['sale.id=' + invoiceId]; 
+    dependencies.saleItem.query.where = ['sale_item.sale_id=' + invoiceId];
     return validate.process(dependencies).then(creditNote);
   }
 
-  function creditNote(model) { 
+  function creditNote(model) {
+    console.log(model);
     $scope.model = model;
-  
-    
-    console.log('fetched', model);
+    $scope.sale = $scope.model.sale.data[0];
+    $scope.creditNote = packageCreditNote();
+  }
+
+  function packageCreditNote() { 
+    var defaultDescription = "Credit matching transaction #" + $scope.sale.id + " from " + $filter('date')($scope.sale.invoice_date);
+    var noteObject = { 
+      cost: $scope.sale.cost,
+      debitor_id: $scope.sale.debitor_id,
+      sale_id: $scope.sale.id,
+      note_date: new Date(),
+      description: defaultDescription
+    }
+    return noteObject;
+  }
+
+  function submitCreditNote(noteObject) { 
+
   }
 });
