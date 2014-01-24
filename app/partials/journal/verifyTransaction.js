b51a2a2b2535c9ab0278fcfd1c75792019efd650
Steven Fountain <StevenFountain@live.co.uk>
2014-01-24 10:40:03 +0100

    Resolved journal posting trans_id bug, updated max to support multiple tables, new transaction data fetched
    Former-commit-id: 1577ba3ab4efa1eed7d11a9a0ee917776b744489 [formerly 55fa53086846dd66bdebbd45aa813d371620f41b]
    Former-commit-id: 82012730162d27734bb5d74a5dce2fef62e5e6e0

--- app/partials/journal/verifyTransaction.js
+++ app/partials/journal/verifyTransaction.js
@@ -1,11 +1,23 @@
-angular.module('kpk.controllers').controller('verifyTransaction', function($scope, $modalInstance) { 
+angular.module('kpk.controllers').controller('verifyTransaction', function($scope, $modalInstance,connect) { 
   var transaction = $scope.transaction = {};
+  
   transaction.date = inputDate(new Date());
-  console.log($scope.transaction);
+ 
+  connect.req('/max/trans_id/general_ledger/posting_journal/').then(function(res) { 
+    transaction.id = res.data.max;
+    console.log('tr', transaction);
+  }, function(err) { console.log(err) });
+
+  function submitTransaction() { 
+    $modalInstance.close(transaction);
+  }
 
   function inputDate(date) {
     //Format the current date according to RFC3339 (for HTML input[type=="date"])
     console.log('date', date);
     return date.getFullYear() + "-" + ('0' + (date.getMonth() + 1)).slice(-2) + "-" + ('0' + date.getDate()).slice(-2);
   }
+
+  $scope.cancel = function() { $modalInstance.dismiss() };
+  $scope.submit = submitTransaction;
 });
