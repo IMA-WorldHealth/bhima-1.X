9557f0ffdf57250c97de7321d00d70d4d4b18507
Steven Fountain <StevenFountain@live.co.uk>
2014-01-27 09:52:37 +0100

    Journal log schema, fetch default log ID, exception handling, select deb cred type

--- app/partials/journal/verifyTransaction.js
+++ app/partials/journal/verifyTransaction.js
@@ -1,16 +1,25 @@
 angular.module('kpk.controllers').controller('verifyTransaction', function($scope, $modalInstance,connect) { 
   var transaction = $scope.transaction = {};
-  
+  var formComplete = false;
+  var defaultLogId = 10000, defaultTransactionId = 1;
+
   transaction.date = inputDate(new Date());
  
   connect.req('/max/trans_id/general_ledger/posting_journal/').then(function(res) { 
-    transaction.id = ++res.data.max;
+    transaction.id = ++res.data.max || defaultTransactionId;
     console.log('tr', transaction);
+    
+    connect.req('/max/id/journal_log/').then(loadNote);
   }, function(err) { console.log(err) });
+    
+  function loadNote(res) { 
+    var maxid = res.data.max || defaultLogId; 
+    transaction.logId = maxid;
+    formComplete = true;
+  }
 
   function submitTransaction() { 
-
-    $modalInstance.close(transaction);
+    if(formComplete) $modalInstance.close(transaction);
   }
 
   function inputDate(date) {
