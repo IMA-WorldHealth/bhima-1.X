4d57b767059bdfeb1fc3e9c36fea902133573772
Jonathan Niles <jonathanwniles@gmail.com>
2014-01-27 15:16:43 +0100

    added permissions

--- scripts/sql/tshikaji.sql
+++ scripts/sql/tshikaji.sql
@@ -107,7 +107,7 @@ INSERT INTO `unit` VALUES
   (57,'Income Expensive Balance', '', 10, 0, 'partials/reports/income_expensive/', 'reports/income_expensive/'),
   (58,'Credit Note', '', 5, 0, 'partials/credit_note/', 'credit_note/'),
   (59,'Convention', '', 1, 0, 'partials/convention/', 'convention/'),
-  (60,'Patient Group Assingning', '', 21, 0, 'partials/patient_group_assign/', 'patient_group_assign/'), 
+  (60,'Patient Group Assigning', '', 21, 0, 'partials/patient_group_assign/', 'patient_group_assign/'), 
   (61,'Patient Group', '', 1, 0, 'partials/patient_group/', 'patient_group/'); 
 
 insert into `permission` (`id_unit`, `id_user`) values
@@ -208,6 +208,8 @@ insert into `permission` (`id_unit`, `id_user`) values
   (57,1),
   (58,1),
   (59,1),
+  (60,1),
+  (61,1),
   (1,13),
   (2,13),
 	(4,13),
