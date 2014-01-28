5914a85e60912271145ca1f769b515132b4b06c8
Jonathan Niles <jonathanwniles@gmail.com>
2014-01-28 11:02:57 +0100

    enterprise module re-written with validate; separation of concerns between editing and adding
    Former-commit-id: d36f5dc325a78faa15ae36fe60ecf5a3dddaad88 [formerly b8a4179b680e48dfbbeaedff41320b22cdcb3938]
    Former-commit-id: 1464f1648e5d37d81ef2f31081787a1a076e18c4

--- app/partials/enterprise/enterprise.js
+++ app/partials/enterprise/enterprise.js
@@ -1,109 +1,97 @@
 angular.module('kpk.controllers')
-.controller('enterpriseController', function($scope, $q, connect, appstate) {
-  var imports = {},
-      models = $scope.models = {},
-      flags = $scope.flags = {},
-      swap = $scope.swap = {},
-      stores = {},
-      dependencies = ['enterprise', 'account', 'currency'];
+.controller('enterpriseController', [
+  '$scope',
+  '$q',
+  'connect',
+  'validate',
+  'messenger',
+function ($scope, $q, connect, validate, messenger) {
+  var flags = $scope.flags = {};
 
-  imports.enterprise = {
-    tables : { 'enterprise' : { columns : ['id', 'name', 'abbr', 'email', 'phone', 'location_id', 'logo', 'currency_id']}}
+  var dependencies = {};
+
+  dependencies.enterprise = {
+    required : true,
+    query : {
+      tables : {
+        'enterprise' : {
+          columns : ['id', 'name', 'abbr', 'email', 'phone', 'location_id', 'logo', 'currency_id']
+        }
+      }
+    } 
   };
-  imports.accounts = {
-    tables : {'account' : { columns : ['id', 'account_number', 'account_txt', 'locked']}}
-    // where : ['account.enterprise_id=' + imports.enterprise_id]
+
+  dependencies.currency = {
+    required : true,
+    query : {
+      tables : {
+        'currency' : {
+          columns : ['id', 'symbol', 'name']
+        }
+      }
+    }
   };
-  imports.currency = {
-    tables : { 'currency' : { columns : ['id', 'symbol', 'name'] }}
+
+  dependencies.location = {
+    required : true,
+    query : '/location/'
+  };
+
+  validate.process(dependencies).then(initialize, handleError);
+  
+  function initialize (models) {
+    for (var k in models) $scope[k] = models[k];
+  }
+
+  function handleError (error) {
+    messenger.danger('An error occured in fetching requests: ' + JSON.stringify(error)); 
+  }
+
+  $scope.formatLocation = function formatLocation (l) {
+    return [l.village, l.sector, l.province, l.country].join(' -- ');
+  };
+
+  $scope.newEnterprise = function () {
+    $scope.add = {};
+    $scope.action = 'new';
+  };
+
+  $scope.editEnterprise = function (enterprise) {
+    $scope.edit = angular.copy(enterprise);
+    $scope.editing_enterprise = enterprise;
+    $scope.action = 'edit'; 
+  };
+
+  $scope.saveEdit = function () {
+    var data = connect.clean($scope.edit);
+
+    connect.basicPost('enterprise', [data], ['id'])
+    .then(function (res) {
+      $scope.enterprise.put(edit);
+    }, function (err) {
+      messenger.danger('Error updating enterprise ' + data.id + ':' + JSON.stringify(err));
+    });
+
+  };
+
+  $scope.resetEdit = function () {
+    $scope.edit = angular.copy($scope.editing_enterprise);
+  };
+
+  $scope.saveNew = function () {
+    var data = connect.clean($scope.add);
+
+    connect.basicPut('enterprise', [data])
+    .then(function (res) {
+      data.id = res.data.insertId;
+      $scope.enterprise.post(data);
+    }, function (err) {
+      messenger.danger('Error in saving new enterprise:' + JSON.stringify(err));
+    });
   };
   
-  //appstate doesn't gaurantee page has already loaded for 'get', a registered callback will be executed when the value is ready // imports.enterprise_id = appstate.get('enterprise').id;
-  appstate.register('enterprise', function(res) { 
-    imports.enterprise_id = res.id;
-    
-    //assign to query
-    imports.accounts.where = ['account.enterprise_id=' + imports.enterprise_id];
-    initialize();
-  });
+  $scope.resetNew = function () {
+    $scope.add = {}; 
+  };
 
-  function initialize () {
-    $q.all([
-      connect.req(imports.enterprise),
-      connect.req(imports.accounts),
-      connect.req(imports.currency)
-    ])
-    .then(function (array) {
-      array.forEach(function (depend, idx) {
-        console.log('set', depend, idx);
-        stores[dependencies[idx]] = depend;
-        models[dependencies[idx]] = depend.data;
-      });
-      
-      connect.fetch('/location/')
-      .then(function (result) {
-        console.log("Got location data:", result.data);
-        models.location = result.data;
-      }, function (error) {
-        console.error("found an error:", error);
-      });
-
-    });
-  }
-
-  function formatLocation (l) {
-    return [l.village, l.sector, l.province, l.country].join(' -- ');
-  }
-
-  function formatAccount (account) {
-    // FIXME/TODO: Different label for accounts that are locked.
-    // Is it possible to disable them using angular's select?
-    return [account.account_txt, account.account_number].join(' :: ');
-  }
-
-  function formatAccountNumber (account_id) {
-    var account = stores.account.get(account_id);
-    if(account) return (stores.account) ? account.account_number : '';
-  }
-
-  function load (enterprise) {
-    flags.new = false;
-    $scope.swap = enterprise;
-  }
-
-  function save () {
-    var data = connect.clean($scope.swap);
-    if (flags.new) {
-      connect.basicPut('enterprise', [data])
-      .then(function (success) {
-        console.log('data posted successfully');
-        // TODO: make this truly async so that no reload is necessary
-        console.log('success:', success.data.insertId);
-        initialize();
-      }, function (error) {
-        console.log('There was an error!');
-      });
-    } else {
-      connect.basicPost('enterprise', [data], ['id'])
-      .then(function (success) { 
-        console.log('data put successfully');
-        initialize();
-      }, function (error) {
-        console.log('there was an error!');
-      });
-    }
-  }
-
-  function clear () {
-    $scope.swap = {};
-    flags.new = true;
-  }
-
-  $scope.formatAccount = formatAccount;
-  $scope.formatAccountNumber = formatAccountNumber;
-  $scope.formatLocation = formatLocation;
-  $scope.load = load;
-  $scope.save = save;
-  $scope.clear = clear;
-
-});
+}]);
