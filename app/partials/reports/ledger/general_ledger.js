84953ee920096931391c8c6d86fbda1e876befe4
Steven Fountain <StevenFountain@live.co.uk>
2014-02-05 11:50:49 +0100

    Refactored general ledger code, seperated into methods and use standard validate

--- app/partials/reports/ledger/general_ledger.js
+++ app/partials/reports/ledger/general_ledger.js
@@ -1,39 +1,33 @@
-angular.module('kpk.controllers')
-.controller('reportGeneralLedgerCtrl', function ($scope, $q, $filter, connect, appstate) {
-  'use strict';
-
-  // This simply provides a view into the general ledger.
-
-  var models = $scope.models = {},
-      flags = $scope.flags = {},
-      imports = {},
-      stores = {};
-
-  imports.enterprise_id = appstate.get('enterprise').id;
-
-  imports.general_ledger = {
-    tables : { 
-      'general_ledger' : { columns : ['id', 'fiscal_year_id', 'period_id', 'trans_id', 'trans_date', 'doc_num', 'description', 'account_id', 'debit', 'credit', 'debit_equiv', 'credit_equiv', 'currency_id', 'deb_cred_id', 'deb_cred_type', 'inv_po_id', 'comment', 'cost_ctrl_id', 'origin_id', 'user_id']},
-      'account' : { columns : ['account_number']}
-    },
-    join : ['general_ledger.account_id=account.id'],
-    where: ['general_ledger.enterprise_id='+imports.enterprise_id]
-  };
-
-  var dependencies = ['general_ledger'];
-
+angular.module('kpk.controllers').controller('reportGeneralLedgerCtrl', function ($scope, $q, $filter, connect, appstate, validate) {
+  'use strict'; 
+  var dependencies = {}, flags = $scope.flags = {};
   var columns, dataview, options, grid;
 
-  $q.all([
-    connect.req(imports.general_ledger)
-  ])
-  .then(initialize);
-
-  function initialize (array) { 
-    for (var i = 0, l = dependencies.length; i < l; i++) {
-      stores[dependencies[i]] = array[i];
-      models[dependencies[i]] = array[i].data;
+  dependencies.ledger = { 
+    query : {
+      tables : { 
+        'general_ledger' : { columns : ['id', 'fiscal_year_id', 'period_id', 'trans_id', 'trans_date', 'doc_num', 'description', 'account_id', 'debit', 'credit', 'debit_equiv', 'credit_equiv', 'currency_id', 'deb_cred_id', 'deb_cred_type', 'inv_po_id', 'comment', 'cost_ctrl_id', 'origin_id', 'user_id']},
+        'account' : { columns : ['account_number']}
+      },
+      join : ['general_ledger.account_id=account.id'],
     }
+  };
+  
+  appstate.register('enterprise', buildLedgerQuery);
+
+  function buildLedgerQuery(enterprise) { 
+    dependencies.ledger.where = ['general_ledger.enterprise_id='+enterprise.id]
+    validate.process(dependencies).then(reportGeneralLedger);
+  }
+
+  function reportGeneralLedger(model) { 
+    $scope.model = model;
+    
+    settupGridOptions();
+    initialiseGrid();
+  }
+
+  function settupGridOptions() { 
     columns = [
       {id: 'id'             , name: 'ID'                  , field:'id'             , visible : false} ,
       {id: 'fiscal_year_id' , name: 'Fiscal Year'         , field:'fiscal_year_id' , visible : true } ,
@@ -55,7 +49,6 @@ angular.module('kpk.controllers')
       {id: 'origin_id'      , name: 'Origin ID'           , field:'origin_id'      , visible : false} ,
       {id: 'user_id'        , name: 'User ID'             , field:'user_id'        , visible : false}
     ];
-
     $scope.columns = angular.copy(columns);
 
     options = {
@@ -64,7 +57,9 @@ angular.module('kpk.controllers')
       forceFitColumns: true,
       rowHeight: 35,
     };
-    
+  }
+
+  function initialiseGrid() { 
     dataview = new Slick.Data.DataView();
 
     dataview.onRowCountChanged.subscribe(function (e, args) {
@@ -80,35 +75,14 @@ angular.module('kpk.controllers')
     grid = new Slick.Grid('#kpk-ledger-general_ledger-grid', dataview, columns, options);
     grid.setSelectionModel(new Slick.RowSelectionModel());
 
-    // enable sorting
-
-    function compareSort(a, b) {
-      var x = a[sort_column], y = b[sort_column];
-      return (x == y) ? 0 : (x > y ? 1 : -1);
-    }
-
+    //Grid sorting 
     grid.onSort.subscribe(function(e, args) {
       sort_column = args.sortCol.field;
       dataview.sort(compareSort, args.sortAsc);
     });
 
-    // enable searching
-
-    function search (item, args) {
-      if (item.searchStr !== "" &&
-          String(item.account_number).indexOf(args.searchStr) === -1 &&
-          String(item.trans_id).indexOf(args.searchStr) === -1 &&
-          String(item.deb_cred_id).indexOf(args.searchStr) === -1
-         ) {
-        return false;
-      }
-      return true;
-    }
-
-    // initialize everything
-
     dataview.beginUpdate();
-    dataview.setItems(models.general_ledger);
+    dataview.setItems($scope.model.ledger.data);
     dataview.setFilterArgs({
       searchStr: flags.searchStr
     });
@@ -116,29 +90,29 @@ angular.module('kpk.controllers')
     dataview.endUpdate();
 
     dataview.syncGridSelection(grid, true);
-
-    $scope.$watch('flags.searchStr', function () {
-      if (!flags.searchStr) flags.searchStr = ""; //prevent default
-      dataview.setFilterArgs({
-        searchStr: flags.searchStr 
-      });
-      dataview.refresh();
-    });
-
-    $scope.$watch('columns', function () {
-      if (!$scope.columns) return;
-      var columns = $scope.columns.filter(function (column) {
-        return column.visible;
-      });
-      grid.setColumns(columns);
-    }, true);
-
   }
 
+  //Grid sort methods
+  function compareSort(a, b) {
+    var x = a[sort_column], y = b[sort_column];
+    return (x == y) ? 0 : (x > y ? 1 : -1);
+  }
+
+  function search (item, args) {
+    if (item.searchStr !== "" &&
+        String(item.account_number).indexOf(args.searchStr) === -1 &&
+        String(item.trans_id).indexOf(args.searchStr) === -1 &&
+        String(item.deb_cred_id).indexOf(args.searchStr) === -1
+        ) {
+      return false;
+    }
+    return true;
+  }
+
+  //Grid formats
   function formatAmount(row, cell, value) {
     return $filter('currency')(value);
   }
-  
 
   function formatEquiv(row, cell, value) {
     return $filter('currency')(value);
@@ -147,5 +121,22 @@ angular.module('kpk.controllers')
   function formatDate (row, cell, value) {
     return $filter('date')(value); 
   }
+
+  //Update grid on filter/settings changes
+  $scope.$watch('flags.searchStr', function () {
+    if (!flags.searchStr) flags.searchStr = ""; //prevent default
+    dataview.setFilterArgs({
+      searchStr: flags.searchStr 
+    });
+    dataview.refresh();
+  });
+
+  $scope.$watch('columns', function () {
+    if (!$scope.columns) return;
+    var columns = $scope.columns.filter(function (column) {
+      return column.visible;
+    });
+    grid.setColumns(columns);
+  }, true);
 
 });
