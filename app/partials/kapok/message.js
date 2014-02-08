3e8bf1f3ee9e012fcdabab24457405216695c4e0
Jonathan Niles <jonathanwniles@gmail.com>
2014-02-08 09:34:41 +0100

    added experimental css styles; removed double view container

--- app/partials/kapok/message.js
+++ app/partials/kapok/message.js
@@ -1,7 +1,9 @@
 angular.module('kpk.controllers')
-.controller('messageCtrl', function ($scope, $timeout, messenger) {
+.controller('message', function ($scope, $timeout, messenger) {
   // This binds the message service to the DOM
   'use strict';
+
+  messenger.warning('This is a test', 500000);
 
   $scope.messages = messenger.messages;
   $scope.close = messenger.close;
