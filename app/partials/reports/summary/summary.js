b9cca74c04728c94f64cf8de530a12567b4c2fc9
Jonathan Niles <jonathanwniles@gmail.com>
2014-02-12 15:50:21 +0100

    added translations for users and permissions

--- app/partials/reports/summary/summary.js
+++ app/partials/reports/summary/summary.js
@@ -1,49 +1,53 @@
-angular.module('kpk.controllers').controller('summaryController', function($scope, $q, $compile) { 
-  console.log('summary initialised');
+angular.module('kpk.controllers')
+.controller('summaryController', [
+  '$scope',
+  '$q',
+  'compile',
+  function($scope, $q, $compile) {
 
-  const ROW_LENGTH = 2;
+    var ROW_LENGTH = 2;
 
-  $scope.reports = [[]];
-  console.log($scope.reports);
+    $scope.reports = [[]];
 
-  function Report(title, view) { 
-    return {
-      'title': title,
-      'view': view
+    function Report(title, view) {
+      return {
+        'title': title,
+        'view': view
+      };
     }
-  }
-  
-  function init() { 
-    //check if the user has a settings configuration in appcache - load from this if it exists
-    var configuration = [
-      {title: "Cash Box", content: "content", view: "view"},
-      {title: "Finance", content: "content", view: "view2"},
-      {title: "Finance", content: "content", view: "view2"},
-      {title: "Finance", content: "content", view: "view2"},
-      {title: "Finance", content: "content", view: "view2"},
-      {title: "Finance", content: "content", view: "view2"},
-      {title: "Finance", content: "content", view: "view2"}
+   
+    function init() {
+      //check if the user has a settings configuration in appcache - load from this if it exists
+      var configuration = [
+        {title: "Cash Box", content: "content", view: "view"},
+        {title: "Finance", content: "content", view: "view2"},
+        {title: "Finance", content: "content", view: "view2"},
+        {title: "Finance", content: "content", view: "view2"},
+        {title: "Finance", content: "content", view: "view2"},
+        {title: "Finance", content: "content", view: "view2"},
+        {title: "Finance", content: "content", view: "view2"}
 
-    ]
+      ];
 
-    configuration.forEach(function(report) { 
-      addReport(report);
-    })
-  }
-
-  function addReport(report) { 
-    var currentGroup = $scope.reports[$scope.reports.length - 1];
-
-    if(currentGroup.length >= ROW_LENGTH) { 
-      currentGroup = [];
-      $scope.reports.push(currentGroup);
+      configuration.forEach(function(report) {
+        addReport(report);
+      });
     }
-    currentGroup.push(report);
+
+    function addReport(report) {
+      var currentGroup = $scope.reports[$scope.reports.length - 1];
+
+      if(currentGroup.length >= ROW_LENGTH) {
+        currentGroup = [];
+        $scope.reports.push(currentGroup);
+      }
+      currentGroup.push(report);
+    }
+
+    function summariseCash() {
+
+    }
+
+    init();
   }
-
-  function summariseCash() { 
-
-  }
-
-  init();
-});
+]);
