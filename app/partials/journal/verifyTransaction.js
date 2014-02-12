dac00225f73f9e9907088492ca9ad5508cb52c45
Jonathan Niles <jonathanwniles@gmail.com>
2014-02-12 15:50:21 +0100

    added translations for users and permissions
    Former-commit-id: 45b1d34de12b8f5c126c80123111f53ee665c1d1 [formerly b9cca74c04728c94f64cf8de530a12567b4c2fc9]
    Former-commit-id: 19c827f030b30a952d7307a3f9cc4b65c029b9fc

--- app/partials/journal/verifyTransaction.js
+++ app/partials/journal/verifyTransaction.js
@@ -1,51 +1,62 @@
-angular.module('kpk.controllers').controller('verifyTransaction', function($scope, $modalInstance, connect, updateType, transaction) { 
-  var transaction = $scope.transaction = {}, defaultLogId = 0, defaultTransactionId = 1;
-  var typeMap = { 
-    'split' : split,
-    'add' : add
-  }
+angular.module('kpk.controllers')
+.controller('verifyTransaction', [
+  '$scope',
+  '$modalInstance',
+  'connect',
+  'updateType',
+  'transaction',
+  function($scope, $modalInstance, connect, updateType, transaction) {
+    var transaction = $scope.transaction = {},
+        defaultLogId = 0,
+        defaultTransactionId = 1;
 
-  function split()  {
+    var typeMap = {
+      'split' : split,
+      'add' : add
+    };
 
-  }
+    function split()  {
 
-  function add() { 
+    }
+
+    function add() {
+   
+    }
+
+    $scope.formComplete = false;
+    $scope.invalidSubmit = "";
+
+    transaction.date = inputDate(new Date());
   
+    connect.req('/max/trans_id/general_ledger/posting_journal/').then(function(res) {
+      transaction.id = ++res.data.max || defaultTransactionId;
+     
+      connect.req('/max/id/journal_log/').then(loadNote);
+    }, function(err) { console.log(err); });
+     
+    function loadNote(res) {
+      var maxid = res.data.max || defaultLogId;
+      transaction.logId = ++maxid;
+
+      connect.req('/user_session/').then(loadUser);
+    }
+
+    function loadUser(res) {
+      transaction.userId = res.data.id;
+      $scope.formComplete = true;
+    }
+
+    function submitTransaction() {
+      if(!$scope.formComplete) return $scope.invalidSubmit = "Unable to verify transaction, required data is missing.";
+      $modalInstance.close(transaction);
+    }
+
+    function inputDate(date) {
+      //Format the current date according to RFC3339 (for HTML input[type=="date"])
+      return date.getFullYear() + "-" + ('0' + (date.getMonth() + 1)).slice(-2) + "-" + ('0' + date.getDate()).slice(-2);
+    }
+
+    $scope.cancel = function() { $modalInstance.dismiss(); };
+    $scope.submit = submitTransaction;
   }
-
-  $scope.formComplete = false;
-  $scope.invalidSubmit = "";
-
-  transaction.date = inputDate(new Date());
- 
-  connect.req('/max/trans_id/general_ledger/posting_journal/').then(function(res) { 
-    transaction.id = ++res.data.max || defaultTransactionId;
-    
-    connect.req('/max/id/journal_log/').then(loadNote);
-  }, function(err) { console.log(err) });
-    
-  function loadNote(res) { 
-    var maxid = res.data.max || defaultLogId; 
-    transaction.logId = ++maxid;
-
-    connect.req('/user_session/').then(loadUser);
-  }
-
-  function loadUser(res) { 
-    transaction.userId = res.data.id;
-    $scope.formComplete = true;
-  }
-
-  function submitTransaction() { 
-    if(!$scope.formComplete) return $scope.invalidSubmit = "Unable to verify transaction, required data is missing.";
-    $modalInstance.close(transaction);
-  }
-
-  function inputDate(date) {
-    //Format the current date according to RFC3339 (for HTML input[type=="date"])
-    return date.getFullYear() + "-" + ('0' + (date.getMonth() + 1)).slice(-2) + "-" + ('0' + date.getDate()).slice(-2);
-  }
-
-  $scope.cancel = function() { $modalInstance.dismiss() };
-  $scope.submit = submitTransaction;
-});
+]);
