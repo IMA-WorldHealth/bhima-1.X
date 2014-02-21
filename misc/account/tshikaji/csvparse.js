f3169e1c5070c7ae7ef9859649a7b7e5a2866d88
Steven Fountain <StevenFountain@live.co.uk>
2014-02-21 20:57:07 +0100

    First attempt at parsing CSV and creating relationships, redundant titles should be removed

--- misc/account/tshikaji/csvparse.js
+++ misc/account/tshikaji/csvparse.js
@@ -1,11 +1,13 @@
 var fs = require('fs'), q = require('q');
 
 var accountFilePath = "./ET09 PLAN COMPTABLE ALPHA.csv";
-var maxAccountDetails = 8, schema = {};
+var maxAccountDetails = 8, schema = {}, schemaIndex;
 
 // Account detail indexs
 var accountLabel = 4, accountNumber = 5, differentiateEnterprise = 4;
+var accountNumberLength = 7;
 
+var sqlOutput = [];
 readFile(accountFilePath).then(parseCSV);
 
 function parseCSV(fileData) { 
@@ -13,7 +15,6 @@ function parseCSV(fileData) {
 
   rawSchema = fileData.split(',');
     
-  console.log('numlines', rawSchema.length / maxAccountDetails);
   
   try { 
     do { 
@@ -44,15 +45,135 @@ function parseCSV(fileData) {
 
     var accountIndex = JSON.parse(JSON.stringify(Object.keys(schema)));
   
-    console.log(schema);
     // console.log(accountIndex); 
-    console.log(accountIndex.length);
+    
+    var accountGroupIndex = 1;
+    var groups = {}, identify; 
 
+  
+    // for(var i=0; i < accountNumberLength; i++) { 
+
+      //Visit every account - base case
+      accountIndex.forEach(function (accountNumber) {
+        var account = schema[accountNumber];
+        var identify = accountNumber.slice(0, accountGroupIndex);
+        
+        if(groups[identify]) { 
+          groups[identify][accountNumber] = accountNumber;
+        } else { 
+          groups[identify] = {}
+
+          groups[identify][accountNumber] = accountNumber;
+        }
+      });
+    // }    
+    
+    accountGroupIndex++;
+
+    parseRelation(groups, accountGroupIndex);
+    removeRedundant(groups, 1); 
+    // groupDebugPrint(groups, 1);
+    //
+    // console.log(groups);
+    // console.log(groups[7][71][710]);
+
+    sqlPrint(groups, 0);
+    
+    console.log("insert into `account` (`fixed`, `locked`, `enterprise_id`, `account_number`, `account_txt`, `account_type_id`, `parent`) values"); 
+    console.log(sqlOutput.join(',\n') + ';');
+    // console.log('1 10', groups[1].children[10]);
   } catch (error) { 
     console.log(error);
   }
+}
 
-  // console.log(schema);
+function groupDebugPrint(group, indentLevel) { 
+  for(account in group) { 
+    var spacer = new Array(indentLevel);
+    console.log(spacer, account);
+
+    if(account.length < accountNumberLength) { 
+      groupDebugPrint(group[account], indentLevel + 1);
+    }
+  }
+}
+
+function sqlPrint(group, parentAccountNumber) { 
+  for(account in group) { 
+    
+    if(account.length < accountNumberLength) { 
+      var accountNumber = account;
+      sqlOutput.push('(0, 0, 200,' + accountNumber + ',"Title Account for ' + account + '", 3,' + parentAccountNumber + ')'); 
+      sqlPrint(group[account], accountNumber);
+    } else { 
+      var accountType = (account[0] === '6' || account[0] === '7') ? '1' : '2';
+      sqlOutput.push('(0, 0, 200,' + account + ',"' + schema[account].label + '",' + accountType + ',' + parentAccountNumber + ')'); 
+    }
+  }
+}
+
+function padNumber(accountNumber) { 
+  var difference = accountNumberLength - accountNumber.length;
+  for(var i = 0; i <= difference; i++) { 
+    accountNumber += '0';
+  }
+  return accountNumber;
+}
+
+function removeRedundant(group, indexLevel) { 
+  for(accountIndex in group) { 
+    var accountGroup = group[accountIndex], keys = Object.keys(accountGroup);
+    // console.log('for ', accountIndex, 'length', keys.length);
+    var iterate = true;
+    
+    if(keys.length === 1) { 
+
+      while(keys.length === 1 && iterate) { 
+        // console.log('would get', accountGroup[keys[0]]);
+        group[keys[0]] = accountGroup[keys[0]];
+        // console.log('deleting', accountIndex);
+        delete group[accountIndex];
+        
+        accountGroup = group[keys[0]];
+        accountIndex = keys[0];
+
+        if(typeof(accountGroup) !== 'object') { 
+          iterate = false;
+        } else { 
+          keys = Object.keys(accountGroup);
+        }
+      }
+    } else { 
+      removeRedundant(accountGroup, 1);
+    }
+  }
+}
+
+function parseRelation(group, indexLevel) { 
+
+   
+  for(accountIndex in group) { 
+    var accountGroup = group[accountIndex]; 
+
+    // crazy hacks
+    if(accountIndex.length < accountNumberLength -1) { 
+      
+      for(account in accountGroup) { 
+        var identify = account.slice(0, indexLevel);
+
+        if(accountGroup[identify]) { 
+          accountGroup[identify][account] = account;
+        } else { 
+          accountGroup[identify] = {};
+          accountGroup[identify][account] = account;
+        }
+
+        delete accountGroup[account]; 
+      }
+
+      parseRelation(accountGroup, indexLevel+1);
+    }
+  }
 }
 
 function readFile(filePath) { 
