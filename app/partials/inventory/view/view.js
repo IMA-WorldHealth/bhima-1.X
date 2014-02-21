ae1d4eff1525270f70e32f6ff4b10388206bd54c
jniles <jonathanwniles@gmail.com>
2014-02-21 09:29:22 +0100

    inventory view now uses smarter groupby:
    Former-commit-id: 1549fa4cf405bab7301a0ab362d175b805bcd895 [formerly 52a750cfb3f6cdd30c81986af1fad24ef7c81269]
    Former-commit-id: 0bccc351751c1f7001778f05ae4349235ebf0a88

--- app/partials/inventory/view/view.js
+++ app/partials/inventory/view/view.js
@@ -3,56 +3,78 @@ angular.module('kpk.controllers')
   '$scope',
   '$q',
   '$filter',
+  'validate',
   'appstate',
   'connect',
   '$translate',
-  function ($scope, $q, $filter, appstate, connect, $translate) {
+  function ($scope, $q, $filter, validate, appstate, connect, $translate) {
     // This module provides a view into the inventory to see all registered items.
     // We display these with a slick grid to allow sorting and arbitrary
     // grouping without too much further issue.
 
-    'use strict';
-
-    var imports = {},
-        models = $scope.models = {},
-        flags = $scope.flags = {},
-        groupby = $scope.groupby = {},
-        stores = {};
-
-    // FIXME: change this.
-    imports.enterprise_id = appstate.get('enterprise').id;
+    var dependencies = {},
+        grid,
+        columns,
+        options,
+        searchStr = "",
+        dataview = $scope.dataview = new Slick.Data.DataView(),
+        groupDefinitions = $scope.groupDefinitions = [],
+        groups = [];
 
     // FIXME: Cannot do a join with inv_unit because inventory.text and inv_unit.text clash.
     // propose rename inventory.text -> inventory.label
-    imports.inventory = {
-      tables : {
-        'inventory' : {
-          columns: ['id', 'code', 'text', 'price', 'unit_id', 'unit_weight', 'unit_volume', 'stock', 'type_id', 'consumable']
+
+    dependencies.inventory = {
+      query : {
+        tables : {
+          'inventory' : {
+            columns: ['id', 'code', 'text', 'price', 'unit_id', 'unit_weight', 'unit_volume', 'stock', 'type_id', 'consumable']
+          },
+          'inv_group' : {
+            columns : ['name']
+          }
         },
-        'inv_group' : {
-          columns : ['name']
-        }
-      },
-      join: ['inventory.group_id=inv_group.id'],
-      where : ['inventory.enterprise_id='+imports.enterprise_id]
+        join: ['inventory.group_id=inv_group.id'],
+      }
     };
 
-    imports.inv_unit = {tables : { 'inv_unit': { columns : ['id', 'text' ]}}};
-    imports.inv_type = {tables : { 'inv_type': { columns : ['id', 'text' ]}}};
-
-    var dependencies = ['inventory', 'inv_unit', 'inv_type'];
-
-    function initialize (array) {
-      for (var i = 0; i < dependencies.length; i+= 1) {
-        stores[dependencies[i]] = array[i];
-        models[dependencies[i]] = array[i].data;
+    dependencies.inventory_unit = {
+      query : {
+        tables : {
+          'inv_unit': {
+            columns : ['id', 'text' ]
+          }
+        }
       }
+    };
 
-      var grid,
-          columns,
-          options,
-          searchStr = "",
-          dataview = $scope.dataview = new Slick.Data.DataView();
+    dependencies.inventory_type = {
+      query : {
+        tables : {
+          'inv_type': {
+            columns : ['id', 'text' ]
+          }
+        }
+      }
+    };
+
+    var flags = $scope.flags = {},
+        groupby = $scope.groupby = {};
+
+    appstate.register('enterprise', function (enterprise) {
+      $scope.enterprise = enterprise;
+      dependencies.inventory.query.where =
+        ['inventory.enterprise_id=' + enterprise.id]
+      validate.process(dependencies).then(buildStores);
+    });
+
+    function buildStores(models) {
+      for (var k in models) { $scope[k] = models[k]; }
+      buildGrid();
+    }
+
+
+    function buildGrid() {
 
       dataview.onRowCountChanged.subscribe(function (e, args) {
         grid.updateRowCount();
@@ -69,14 +91,15 @@ angular.module('kpk.controllers')
         {id: "COLUMNS.TEXT"        , name: "Text"               , field: "text"        , sortable: true} ,
         {id: "COLUMNS.STOCK"       , name: "Stock Count"        , field: "stock"       , sortable: true} ,
         {id: "COLUMNS.GROUP"       , name: "Inv. Group"         , field: "name"        , sortable: true} ,
-        {id: "COLUMNS.TYPE"     , name: "Type"               , field: "type_id"     , sortable: true  , formatter: formatType},
-        {id: "COLUMNS.UNIT"     , name: "Unit"               , field: "unit_id"     , sortable: true  , formatter: formatUnit},
-        {id: "COLUMNS.WEIGHT" , name: "Unit Weight"        , field: "unit_weight" , sortable: true} ,
-        {id: "COLUMNS.VOLUME" , name: "Unit Volume"        , field: "unit_volume" , sortable: true} ,
+        {id: "COLUMNS.TYPE"        , name: "Type"               , field: "type_id"     , sortable: true  , formatter: formatType},
+        {id: "COLUMNS.UNIT"        , name: "Unit"               , field: "unit_id"     , sortable: true  , formatter: formatUnit},
+        {id: "COLUMNS.WEIGHT"      , name: "Unit Weight"        , field: "unit_weight" , sortable: true} ,
+        {id: "COLUMNS.VOLUME"      , name: "Unit Volume"        , field: "unit_volume" , sortable: true} ,
         {id: "COLUMNS.CONSUMABLE"  , name: "Consumable/Durable" , field: "consumble"   , sortable: true  , formatter: formatConsumable},
         {id: "COLUMNS.PRICE"       , name: "Base Price"         , field: "price"       , sortable: true  , formatter: formatCurrency}
       ];
 
+      // FIXME : this for some reason doesn't always work.
       columns.forEach(function (col) {
         col.name = $translate(col.id);
       });
@@ -86,7 +109,7 @@ angular.module('kpk.controllers')
           col.name = $translate(col.id);
         });
       });
-      
+    
       options = {
         enableCellNavigation : true,
         forceFitColumns      : true
@@ -94,7 +117,7 @@ angular.module('kpk.controllers')
 
       grid = new Slick.Grid("#kpk-inventory-grid", dataview, columns, options);
       grid.setSelectionModel(new Slick.RowSelectionModel());
-    
+  
       // set up sorting
 
       function sorter (e, args) {
@@ -107,7 +130,7 @@ angular.module('kpk.controllers')
       grid.onSort.subscribe(sorter);
 
       // set up filtering
-    
+  
       function search (item, args) {
         if (item.searchStr !== "" && item.code.indexOf(args.searchStr) === -1 && item.text.indexOf(args.searchStr) === -1) {
           return false;
@@ -131,79 +154,93 @@ angular.module('kpk.controllers')
 
     }
 
-    $scope.$watch('models.inventory', function () {
-      if ($scope.dataview) $scope.dataview.setItems($scope.models.inventory);
+    $scope.$watch('inventory.data', function () {
+      if ($scope.dataview && $scope.inventory) $scope.dataview.setItems($scope.inventory.data);
     }, true);
-
-
 
     function formatCurrency (row, col, item) {
       return $filter('currency')(item);
     }
 
     function formatUnit (row, col, item) {
-      return stores.inv_unit ? stores.inv_unit.get(item).text : item;
+      return $scope.inventory_unit ? $scope.inventory_unit.get(item).text : item;
     }
 
     function formatType (row, col, item) {
-      return stores.inv_type ? stores.inv_type.get(item).text : item;
+      return $scope.inventory_type ? $scope.inventory_type.get(item).text : item;
     }
 
     function formatConsumable (row, col, item) {
       return item === 1 ? "Consumable" : "Durable";
     }
 
-    groupby.type = function () {
-      $scope.dataview.setGrouping({
+
+    groupDefinitions = [
+      {
+        key : 'COLUMNS.TYPE',
         getter: "type_id",
         formatter: function (g) {
-          return "<span style='font-weight: bold'>" + stores.inv_type.get(g.value).text + "</span> (" + g.count + " members)</span>";
-        }
-      });
-    };
-
-    groupby.group = function () {
-      $scope.dataview.setGrouping({
+          return "<span style='font-weight: bold'>" + $scope.inventory_type.get(g.value).text + "</span> (" + g.count + " members)</span>";
+        },
+        aggregators : []
+      },
+      {
+        key : 'COLUMNS.NAME',
         getter: "name",
         formatter: function (g) {
           return "<span style='font-weight: bold'>" + g.value + "</span> (" + g.count + " members)</span>";
-        }
-      });
-    };
-  
-    groupby.unit = function () {
-      $scope.dataview.setGrouping({
+        },
+        aggregators : []
+      },
+      {
+        key : 'COLUMNS.UNIT',
         getter: "unit_id",
         formatter: function (g) {
-          return "<span style='font-weight: bold'>" + stores.inv_unit.get(g.value).text + "</span> (" + g.count + " members)</span>";
-        }
-      });
-    };
-
-    groupby.consumable = function () {
-      $scope.dataview.setGrouping({
+          return "<span style='font-weight: bold'>" + $scope.inventory_unit.get(g.value).text + "</span> (" + g.count + " members)</span>";
+        },
+        aggregators : []
+      },
+      {
+        key : 'COLUMNS.CONSUMABLE',
         getter: "consumable",
         formatter: function (g) {
           return "<span style='font-weight: bold'>" + formatConsumable(null, null, g.value) + "</span> (" + g.count + " members)</span>";
-        }
-      });
-    };
-
-    groupby.price = function () {
-      $scope.dataview.setGrouping({
+        },
+        aggregators : []
+      },
+      {
+        key : 'COLUMNS.PRICE',
         getter: "price",
         formatter: function (g) {
           return "<span style='font-weight: bold'>" + formatCurrency(null, null, g.value) + "</span> (" + g.count + " members)</span>";
-        }
-      });
-    };
+        },
+        aggregators : []
+      }
+    ];
 
-    // init
-    $q.all([
-      connect.req(imports.inventory),
-      connect.req(imports.inv_unit),
-      connect.req(imports.inv_type)
-    ]).then(initialize);
+    //Utility method
+    function groupby(groupDefinition) {
+      var groupInstance = {};
+      if(groupExists(groupDefinition, groups)) return;
+    
+      groupInstance = JSON.parse(JSON.stringify(groupDefinition));
+      groupInstance.aggregateCollapsed = true;
+      groupInstance.aggregators = [];
+   
+      groupDefinition.aggregators.forEach(function(aggregate) {
+        groupInstance.aggregators.push(new Slick.Data.Aggregators.Sum(aggregate));
+      });
+    
+      groupInstance.formatter = groupDefinition.formatter;
+      groups.push(groupInstance);
+      dataview.setGrouping(groups);
+    }
+
+    function groupExists(targetGroup, groupList) {
+      return groupList.some(function(group) {
+        return group.getter === targetGroup.getter;
+      });
+    }
 
   }
 ]);
