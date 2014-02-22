4b564ce2543926d4736c80ecbc6aa031d3dbfba9
Steven Fountain <StevenFountain@live.co.uk>
2014-02-22 11:25:06 +0100

    Removed nested redundant title accounts, parser needs tidying but works for now

--- misc/account/tshikaji/csvparse.js
+++ misc/account/tshikaji/csvparse.js
@@ -3,87 +3,98 @@ var fs = require('fs'), q = require('q');
 var accountFilePath = "./ET09 PLAN COMPTABLE ALPHA.csv";
 var maxAccountDetails = 8, schema = {}, schemaIndex;
 
-// Account detail indexs
-var accountLabel = 4, accountNumber = 5, differentiateEnterprise = 4;
-var accountNumberLength = 7;
+// Account detail indexs (from CSV)
+var accountLabel = 4, accountNumber = 5
 
+var differentiateEnterprise = 4, accountNumberLength = 7;
 var sqlOutput = [];
+
 readFile(accountFilePath).then(parseCSV);
 
 function parseCSV(fileData) { 
-  var rawSchema = [], index = 1;
-
-  rawSchema = fileData.split(',');
+  var groups = {}, rawSchema = fileData.split(','), index = 1;
+  
+  // Store CSV data as account schema
+  do { 
+    var enterpriseId, accountInstance, indexContext = index * maxAccountDetails; 
+    var buildUniqueAccount = [];
     
-  
-  try { 
-    do { 
-      var enterpriseId, accountInstance, indexContext = index * maxAccountDetails; 
-      var buildUniqueAccount = [];
-      
-      accountInstance = { 
-        label   : rawSchema[indexContext + accountLabel],
-        number  : rawSchema[indexContext + accountNumber]
-      };
-      // FIXME hacky - is there no splice in javascript?
-      // Replace value that differentiates enterprises
-      buildUniqueAccount.push(accountInstance.number.substr(0, differentiateEnterprise));
-      buildUniqueAccount.push('0');
-      buildUniqueAccount.push(accountInstance.number.substr(differentiateEnterprise + 1, accountInstance.number.length-1));
-      
-      enterpriseId = accountInstance.number[differentiateEnterprise];
-      accountInstance.unique = buildUniqueAccount.join('');
-      index++;
-      
-      if(schema[accountInstance.unique]) { 
-        schema[accountInstance.unique].enterprises.push(enterpriseId);
-      } else { 
-        accountInstance.enterprises = [enterpriseId];
-        schema[accountInstance.unique] = accountInstance;
-      }
-    } while (accountInstance.number);
-
-    var accountIndex = JSON.parse(JSON.stringify(Object.keys(schema)));
-  
-    // console.log(accountIndex); 
+    accountInstance = { 
+      label   : rawSchema[indexContext + accountLabel],
+      number  : rawSchema[indexContext + accountNumber]
+    };
     
-    var accountGroupIndex = 1;
-    var groups = {}, identify; 
-
+    // FIXME hacky - is there no splice in javascript?
+    // Replace value that differentiates enterprises
+    buildUniqueAccount.push(accountInstance.number.substr(0, differentiateEnterprise));
+    buildUniqueAccount.push('0');
+    buildUniqueAccount.push(accountInstance.number.substr(differentiateEnterprise + 1, accountInstance.number.length-1));
+    
+    enterpriseId = accountInstance.number[differentiateEnterprise];
+    accountInstance.unique = buildUniqueAccount.join('');
+    index++;
+    
+    if(schema[accountInstance.unique]) { 
+      schema[accountInstance.unique].enterprises.push(enterpriseId);
+    } else { 
+      accountInstance.enterprises = [enterpriseId];
+      schema[accountInstance.unique] = accountInstance;
+    }
+  } while (accountInstance.number);
   
-    // for(var i=0; i < accountNumberLength; i++) { 
+  initGroups(groups, schema);
+  parseRelation(groups, 2);
+  
+  // Second pass removes nested titles, could probably be smarter about this
+  removeRedundant(groups); 
+  removeRedundant(groups); 
+  
+  sqlPrint(groups, 0);
+  console.log("insert into `account` (`fixed`, `locked`, `enterprise_id`, `account_number`, `account_txt`, `account_type_id`, `parent`) values"); 
+  console.log(sqlOutput.join(',\n') + ';');
+}
 
-      //Visit every account - base case
-      accountIndex.forEach(function (accountNumber) {
-        var account = schema[accountNumber];
-        var identify = accountNumber.slice(0, accountGroupIndex);
-        
-        if(groups[identify]) { 
-          groups[identify][accountNumber] = accountNumber;
+function initGroups(store, data) { 
+  var accountIndex = JSON.parse(JSON.stringify(Object.keys(data)));
+  var identify, accountGroupIndex = 1;
+
+  accountIndex.forEach(function (accountNumber) {
+    var account = data[accountNumber];
+    var identify = accountNumber.slice(0, accountGroupIndex);
+    
+    if(store[identify]) { 
+      store[identify][accountNumber] = accountNumber;
+    } else { 
+      store[identify] = {}
+
+      store[identify][accountNumber] = accountNumber;
+    }
+  });
+}
+
+function parseRelation(group, indexLevel) { 
+
+  for(accountIndex in group) { 
+    var accountGroup = group[accountIndex]; 
+
+    // crazy hacks
+    if(accountIndex.length < accountNumberLength -1) { 
+      
+      for(account in accountGroup) { 
+        var identify = account.slice(0, indexLevel);
+
+        if(accountGroup[identify]) { 
+          accountGroup[identify][account] = account;
         } else { 
-          groups[identify] = {}
-
-          groups[identify][accountNumber] = accountNumber;
+          accountGroup[identify] = {};
+          accountGroup[identify][account] = account;
         }
-      });
-    // }    
-    
-    accountGroupIndex++;
 
-    parseRelation(groups, accountGroupIndex);
-    removeRedundant(groups, 1); 
-    // groupDebugPrint(groups, 1);
-    //
-    // console.log(groups);
-    // console.log(groups[7][71][710]);
+        delete accountGroup[account]; 
+      }
 
-    sqlPrint(groups, 0);
-    
-    console.log("insert into `account` (`fixed`, `locked`, `enterprise_id`, `account_number`, `account_txt`, `account_type_id`, `parent`) values"); 
-    console.log(sqlOutput.join(',\n') + ';');
-    // console.log('1 10', groups[1].children[10]);
-  } catch (error) { 
-    console.log(error);
+      parseRelation(accountGroup, indexLevel+1);
+    }
   }
 }
 
@@ -120,58 +131,35 @@ function padNumber(accountNumber) {
   return accountNumber;
 }
 
-function removeRedundant(group, indexLevel) { 
-  for(accountIndex in group) { 
-    var accountGroup = group[accountIndex], keys = Object.keys(accountGroup);
-    // console.log('for ', accountIndex, 'length', keys.length);
-    var iterate = true;
+function removeRedundant(group) { 
+  for (accountIndex in group) { 
+    var keys, accountGroup = group[accountIndex], iterate;
     
-    if(keys.length === 1) { 
-
-      while(keys.length === 1 && iterate) { 
-        // console.log('would get', accountGroup[keys[0]]);
-        group[keys[0]] = accountGroup[keys[0]];
-        // console.log('deleting', accountIndex);
-        delete group[accountIndex];
-        
-        accountGroup = group[keys[0]];
-        accountIndex = keys[0];
-
-        if(typeof(accountGroup) !== 'object') { 
-          iterate = false;
-        } else { 
-          keys = Object.keys(accountGroup);
-        }
-      }
-    } else { 
-      removeRedundant(accountGroup, 1);
-    }
-  }
-}
-
-function parseRelation(group, indexLevel) { 
-
-   
-  for(accountIndex in group) { 
-    var accountGroup = group[accountIndex]; 
-
-    // crazy hacks
-    if(accountIndex.length < accountNumberLength -1) { 
+    if (typeof(accountGroup) === 'object') { 
       
-      for(account in accountGroup) { 
-        var identify = account.slice(0, indexLevel);
+      keys = Object.keys(accountGroup);
+      iterate = true;
+      
+      if(keys.length === 1) { 
 
-        if(accountGroup[identify]) { 
-          accountGroup[identify][account] = account;
-        } else { 
-          accountGroup[identify] = {};
-          accountGroup[identify][account] = account;
+        while(keys.length === 1 && iterate) { 
+          // console.log('cleaned up', keys[0]);
+          group[keys[0]] = accountGroup[keys[0]];
+          // console.log('deleting', accountIndex);
+          delete group[accountIndex];
+          
+          accountGroup = group[keys[0]];
+          accountIndex = keys[0];
+
+          if(typeof(accountGroup) !== 'object') { 
+            iterate = false;
+          } else { 
+            keys = Object.keys(accountGroup);
+          }
         }
-
-        delete accountGroup[account]; 
+      } else { 
+        removeRedundant(accountGroup, 1);
       }
-
-      parseRelation(accountGroup, indexLevel+1);
     }
   }
 }
