0c2bc28206e5e0151ef3f7dce4895f539dec41e2
Steven Fountain <StevenFountain@live.co.uk>
2014-03-03 10:16:14 +0100

    Patient group DB references updated

--- app/partials/patient_group/patient_group.js
+++ app/partials/patient_group/patient_group.js
@@ -24,7 +24,7 @@ angular.module('kpk.controllers')
       query : {
         tables : {
           'price_list'  :  {
-            columns : ['id', 'name']
+            columns : ['id', 'description']
           }
         }
       }
