a23c75b5edd9710084146705a4d84be595f5e6f9
jniles <jonathanwniles@gmail.com>
2014-03-04 07:58:26 +0100

    changed ledger query to reflect new posting journal model
    :
    
    
    Former-commit-id: d7daa6a2c6f1303232d144c2fb19e545cf64b3d1 [formerly 37993eb01300a18ae1da64c243c144617987cce2]
    Former-commit-id: e57c821b88edf62ba83713e956b475de64f3f7dc

--- scripts/lib/logic/ledger.js
+++ scripts/lib/logic/ledger.js
@@ -9,28 +9,33 @@
 // which encapsulate reporting the ledgers
 // for each group, respectively.
 
-var sanitize = require('../util/sanitize');
+var sanitize = require('../util/sanitize'),
+    util = require('../util/util');
 
 module.exports = function (db) {
   'use strict';
 
   function debitor (id, callback) {
     // debitor query
-    if (!id) return callback(new Error('No debitor id selected!'));
+    if (!id) { return callback(new Error('No debitor id selected!')); }
+    else { id = sanitize.escape(id); }
 
     var query =
-      'SELECT `account_id` FROM `debitor` JOIN `debitor_group` ' +
-      'ON `debitor`.`group_id`=`debitor_group`.`id` WHERE `debitor`.`id`=' +
-      sanitize.escape(id) + ';';
- 
-    db.execute(query, function (err, row) {
+      'SELECT c.inv_po_id, c.trans_id, c.trans_date, c.account_id FROM ((SELECT p.inv_po_id, p.trans_id, p.trans_date, p.account_id FROM posting_journal as p WHERE p.deb_cred_id = ' + id + ') ' +
+      'UNION (SELECT g.inv_po_id, g.trans_date, g.trans_id, g.account_id FROM general_ledger as g WHERE g.deb_cred_id = ' + id +')) as c;';
+
+    db.execute(query, function (err, rows) {
       if (err) return callback(err);
      
-      var accnt = row[0].account_id;
+      var invoices = rows.map(function (line) {
+        return line.inv_po_id;
+      });
+
+      var account_id = rows[0].account_id;
 
       var sql =
         'SELECT `t`.`inv_po_id`, `t`.`trans_date`, SUM(`t`.`debit_equiv`) AS `debit`,  ' +
-        'SUM(`t`.`credit_equiv`) AS `credit`,SUM(`t`.`debit_equiv`) - SUM(`t`.`credit_equiv`) as balance, ' +
+        'SUM(`t`.`credit_equiv`) AS `credit`, SUM(`t`.`credit_equiv` - `t`.`debit_equiv`) as balance, ' +
         '`t`.`account_id`, `t`.`deb_cred_id`, `t`.`currency_id`, `t`.`doc_num`, `t`.`description`, `t`.`account_id`, ' +
         '`t`.`comment`' +
         'FROM (' +
@@ -38,20 +43,18 @@ module.exports = function (db) {
             'SELECT `posting_journal`.`inv_po_id`, `posting_journal`.`trans_date`, `posting_journal`.`debit`, ' +
               '`posting_journal`.`credit`, `posting_journal`.`debit_equiv`, `posting_journal`.`credit_equiv`, ' +
               '`posting_journal`.`account_id`, `posting_journal`.`deb_cred_id`, `posting_journal`.`currency_id`, ' +
-              '`posting_journal`.`doc_num`, `posting_journal`.`description`, `posting_journal`.`comment` ' +
+              '`posting_journal`.`doc_num`, posting_journal.trans_id, `posting_journal`.`description`, `posting_journal`.`comment` ' +
             'FROM `posting_journal` ' +
-            'WHERE `posting_journal`.`deb_cred_type`=\'D\'' +
           ') UNION (' +
             'SELECT `general_ledger`.`inv_po_id`, `general_ledger`.`trans_date`, `general_ledger`.`debit`, ' +
               '`general_ledger`.`credit`, `general_ledger`.`debit_equiv`, `general_ledger`.`credit_equiv`, ' +
               '`general_ledger`.`account_id`, `general_ledger`.`deb_cred_id`, `general_ledger`.`currency_id`, ' +
-              '`general_ledger`.`doc_num`, `general_ledger`.`description`, `general_ledger`.`comment` ' +
+              '`general_ledger`.`doc_num`, general_ledger.trans_id, `general_ledger`.`description`, `general_ledger`.`comment` ' +
             'FROM `general_ledger` ' +
-            'WHERE `general_ledger`.`deb_cred_type`=\'D\'' +
           ')' +
         ') AS `t` ' +
-        'WHERE `t`.`account_id`=' + sanitize.escape(accnt) + ' AND ' +
-        ' `t`.deb_cred_id=' + sanitize.escape(id) + ' ' +
+        'WHERE `t`.`inv_po_id` IN (' + invoices.join(',') + ') ' +
+        'AND t.account_id = ' + account_id + ' ' +
         'GROUP BY `t`.`inv_po_id`;\n';
 
       db.execute(sql, function (err, rows) {
