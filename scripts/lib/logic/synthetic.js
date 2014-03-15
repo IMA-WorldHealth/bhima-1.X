12b41618a9d4aeabcb62d3b93dc5a84ea81cb702
dedrick <kitamuka@gmail.com>
2014-03-15 21:59:26 +0100

    principal cash continues
    Former-commit-id: 3baaa038da329ad0e49123ace290b88bd1fef5c5 [formerly ff0bb1bbdb695615b4f198eedf76eaa645254f00]
    Former-commit-id: 472fc2b6f17fadb8ae9f5ab3c6eb44ea5f06d084

--- scripts/lib/logic/synthetic.js
+++ scripts/lib/logic/synthetic.js
@@ -2,12 +2,14 @@
 util = require('../util/util');
 module.exports = function (db, sanitize) {
   //aB : accountBalance
+  //pcR : principal caisse report by date
   var menu_map = {
-    'aB' : aB
+    'aB' : aB,
+    'pcR': pcR
   }
 
-  function aB (enterprise_id, account_ids, callback){
-    var query = JSON.parse(account_ids);
+  function aB (enterprise_id, request, callback){
+    var query = JSON.parse(request);
     var acIds = query.accounts.map(function(item){
       return sanitize.escape(item);
     });
@@ -16,7 +18,7 @@ module.exports = function (db, sanitize) {
     var sql = 'SELECT SUM(`debit_equiv` - `credit_equiv`) as balance, `account_id` '+
       'FROM ((SELECT `debit_equiv`, `credit_equiv`, `enterprise_id`, `account_id`, `currency_id` FROM `posting_journal`)'+
       ' UNION (SELECT `debit_equiv`, `credit_equiv`, `enterprise_id`, `account_id`, `currency_id` FROM `general_ledger`)) as `t`'+
-      ' WHERE '+portion+' AND `t`.`enterprise_id`='+sanitize.escape(enterprise_id)+' GROUP BY `account_id`;';
+      ' WHERE '+portion+' AND `t`.`enterprise_id`='+sanitize.escape(enterprise_id)+' GROUP BY `account_id`';
 
     db.execute(sql, function(err, ans){
       if(err) return callback(err, null)
@@ -24,7 +26,28 @@ module.exports = function (db, sanitize) {
     });
   }
 
-  return function menu (goal, enterprise_id, account_ids, callback) {
-    return menu_map[goal] ? menu_map[goal](enterprise_id, account_ids, callback) :  new Error('Incorrect/invalid route');
+  function pcR (enterprise_id, request, callback){
+    var query = JSON.parse(request);
+    var acIds = query.accounts.map(function(item){
+      return sanitize.escape(item);
+    });
+    var portion = '';
+    if(acIds.length === 1){
+      portion = '`t`.`account_id`='+acIds[0];
+    }else{
+      portion = '`t`.`account_id`='+acIds.join(' OR `t`.`account_id`=');
+    }
+    var sql = 'SELECT SUM(`debit_equiv` - `credit_equiv`) as balance, trans_date '+
+      'FROM ((SELECT `debit_equiv`, `credit_equiv`, `enterprise_id`, `account_id`, `currency_id`, `trans_date` FROM `posting_journal`)'+
+      ' UNION (SELECT `debit_equiv`, `credit_equiv`, `enterprise_id`, `account_id`, `currency_id`, `trans_date` FROM `general_ledger`)) as `t`'+
+      ' WHERE '+portion+' AND `t`.`enterprise_id`='+sanitize.escape(enterprise_id)+' GROUP BY `trans_date` LIMIT 20;';
+    db.execute(sql, function(err, ans){
+      if(err) return callback(err, null)
+      return callback(null, ans);
+    });
+  }
+
+  return function menu (goal, enterprise_id, request, callback) {
+    return menu_map[goal] ? menu_map[goal](enterprise_id, request, callback) :  new Error('Incorrect/invalid route');
   };
 };
