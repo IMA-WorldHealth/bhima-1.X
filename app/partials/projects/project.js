81d2ee3d6b3132872aca4a49a04f47303608edc2
jniles <jonathanwniles@gmail.com>
2014-03-21 09:11:50 +0100

    added basic project management code
    Former-commit-id: 01bda601f3421f992dd169602dbf98d66eedb79e [formerly f2281a25199e7d1e2a0b461475839dce413ae79f]
    Former-commit-id: f4e818a12c299ad45787fece75b965b9ec427261

---
+++ app/partials/projects/project.js
@@ -0,0 +1,104 @@
+angular.module('kpk.controllers')
+.controller('project', [
+  '$scope',
+  '$window',
+  'validate',
+  'connect',
+  'messenger',
+  'appstate',
+  function ($scope, $window, validate, connect, messenger, appstate) {
+    var dependencies = {};
+    $scope.action = '';
+
+    dependencies.projects = {
+      query : {
+        tables : {
+          'project' : {
+            columns : ['id', 'name', 'abbr', 'enterprise_id']
+          }
+        }
+      }
+    };
+    
+    dependencies.enterprises = {
+      query : {
+        tables : {
+          'enterprise' : {
+            columns : ['id', 'name', 'abbr']
+          }
+        }
+      }
+    };
+
+    function handleErrors(err) {
+      messenger.danger('An Error occured : ' + err);
+    }
+
+    function load(models) {
+      for (var k in models) { $scope[k] = models[k]; }
+    }
+
+    $scope.print = function print() {
+      $window.print();
+    };
+
+    $scope.new = function n(project) {
+      $scope.newProject = {};
+      $scope.action = 'new';
+    };
+
+    $scope.submitNew = function submitNew() {
+      var clean = connect.clean($scope.newProject);
+      connect.basicPut('project', [clean])
+      .success(function () {
+        $scope.projects.post(clean);
+        $scope.action = 'default';
+      })
+      .error(handleErrors);
+    };
+
+    $scope.cancelNew = function canceNew() {
+      $scope.newProject = {};
+    };
+
+    $scope.edit = function edit(project) {
+      $scope.editProject = angular.copy(project);
+      $scope.raw = project;
+      $scope.action = 'edit';
+    };
+
+    $scope.submitEdit = function submitEdit() {
+      var clean = connect.clean($scope.editProject);
+      connect.basicPost('project', [clean], ['id'])
+      .success(function () {
+        $scope.projects.put(clean);
+        $scope.action = 'default';
+      })
+      .error(handleErrors);
+    };
+
+    $scope.cancelEdit = function cancelEdit() {
+      $scope.editProject = angular.copy($scope.raw);
+      $scope.action = 'edit';
+    };
+
+    $scope.delete = function d(project) {
+      connect.basicDelete('project', project, 'id')
+      .success(function () {
+        $scope.projects.remove(project.id);
+      })
+      .error(function (error) {
+        messenger.danger('An error occurred : ' + error);
+      });
+    };
+
+    appstate.register('project', function (project) {
+      $scope.currentProject = project;
+      dependencies.projects.query.tables.where =
+        ['projects.enterprise_id=' + project.enterprise_id];
+
+      validate.process(dependencies)
+      .then(load, handleErrors);
+    });
+  }
+]);
