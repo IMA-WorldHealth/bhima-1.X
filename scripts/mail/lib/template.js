1da47454abf6d960e62f26b7032286d6660944d0
Steven Fountain <StevenFountain@live.co.uk>
2014-03-29 12:16:42 +0100

    Resolved bugs/ functionality in each module, linked together with mail.js, added templating and transation files

--- scripts/mail/lib/template.js
+++ scripts/mail/lib/template.js
@@ -1,19 +1,130 @@
 var util = require('util');
+var fs = require('fs');
+var q = require('q');
 
 var template = exports;
 
-var route = { 
-  structure : '../template/template.html',
-  header : '../template/header.template.html',
-  section : '../template/section.template.html'
+var route = {
+  structure : './template/template.html',
+  header : './template/header.template.html',
+  section : './template/section.template.html',
+  reports : './template/reports/en.json'
 };
+var store = {};
+var structureCache, reports;
 
 //module.exports = (function (param) { });
 
-(function () { 
-  util.log('Initialising template');
-})();
 
-template.produceReport = function (bodyString, outputFile) { 
+// util.format - printf
 
+// FIXME hardcoded template
+template.strong = "<strong style='font-size: 18px; color: #686868;'>%s</strong>";
+
+// FIXME temporary method - work with general case
+template.writeHeader = function (headerString, reportReference) { 
+  structureCache = structureCache || template.fetch('structure');
+
+  structureCache = template.replace(structureCache, '{{HEADERLINE}}', headerString);
+  structureCache = template.replace(structureCache, '{{REPORT_REFERENCE}}', reportReference);
 };
+
+// FIXME 
+template.reports = function(category, key) { 
+  reports = reports || JSON.parse(template.fetch('reports'));
+  return reports[category][key];
+};
+
+// FIXME
+template.insertStrong = function (value) {
+  if (!isNaN(value)) value = value.toString();
+
+  return util.format(template.strong, value);
+};
+
+// FIXME
+template.replace = function (source, key, content) { 
+  return source.replace(key, content); 
+};
+
+// FIXME
+template.fetch = function (templateKey) { 
+  return store[templateKey]; 
+};
+
+// FIXME
+template.compileSection = function (heading, body) { 
+  var section = template.fetch('section');
+
+  section = template.replace(section, '{{SECTION_HEADING}}', heading);
+  section = template.replace(section, '{{SECTION_BODY}}', body);
+
+  return section;
+};
+
+// FIXME accept any number of parms + key - temporary hack
+template.compile = util.format;
+// template.compile = function (templateKey, params) { 
+//   if (!store[templateKey]) return new Error("Template not found.");
+//
+//   return util.format(store[templateKey], params);
+// };
+
+// FIXME
+template.produceReport = function (bodyString, outputFile) { 
+  structureCache = template.replace(structureCache, '{{REPORT_BODY}}', bodyString);
+  return writeFile(outputFile, structureCache);
+};
+
+template.load = function (templateConf) { 
+  var deferred = q.defer();
+  var loadStatus = [], keys = Object.keys(route);
+  
+  templateConf = templateConf || route;
+  
+  loadStatus = keys.map(function (key) { 
+    return readFile(templateConf[key]);
+  });
+  
+  q.all(loadStatus)
+  .then(function (result) { 
+    
+    keys.forEach(function (key, index) { 
+      store[key] = result[index];
+    });
+
+    deferred.resolve(template.compile);
+  })
+  .catch(function (error) { 
+    util.error(error);
+    deferred.reject(error);
+  });
+
+  return deferred.promise;
+};
+
+template.read = readFile;
+template.write = writeFile;
+
+// I/O
+function readFile(pathToFile) { 
+  var deferred = q.defer();
+    
+  fs.readFile(pathToFile, 'utf8', function (error, result) { 
+    if(error) throw error;
+    deferred.resolve(result); 
+  });
+
+  return deferred.promise;
+}
+
+function writeFile(pathToFile, fileContent) { 
+  var deferred = q.defer();
+  
+  fs.writeFile(pathToFile, fileContent, function (error) { 
+    if(error) throw error;
+    deferred.resolve();
+  });
+
+  return deferred.promise;
+}
--- scripts/mail/lib/util.js
+++ scripts/mail/lib/util.js
@@ -1,4 +1,12 @@
 var util = exports;
+var sessionDate = new Date();
+
+util.setDate = function (date) {
+  sessionDate = date;
+};
+
+util.date = configureDate();
+
 
 util.generateUuid = function () { 
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
@@ -8,3 +16,14 @@ util.generateUuid = function () {
   });
 };
 
+function configureDate() { 
+  
+  // Handle requests for tables with only timestamp fields
+  var dateFrom = "\'" + sessionDate.getFullYear() + "-0" + (sessionDate.getMonth() + 1) + "-" + sessionDate.getDate() + " 00:00:00\'";
+  var dateTo = "\'" + sessionDate.getFullYear() + "-0" + (sessionDate.getMonth() + 1) + "-" + (sessionDate.getDate() + 1)  + " 00:00:00\'"; 
+  
+  return { 
+    from : dateFrom,
+    to : dateTo
+  };
+}
