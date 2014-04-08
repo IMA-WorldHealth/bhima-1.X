c03d8836fe176441d647769738885c1e213822a4
jniles <jonathanwniles@gmail.com>
2014-04-08 16:36:05 +0100

    ui fixes ot trial balance
    Former-commit-id: 91f6cf17d601ab86ae8be64626e10ff9e56b8f3c [formerly 1a2f6d74f714dc10bf50514227300f1d59317f75]
    Former-commit-id: f17826053a86c2992ba23484b47a89ae960a1356

--- app/partials/journal/trialbalance/trialbalance.js
+++ app/partials/journal/trialbalance/trialbalance.js
@@ -9,16 +9,27 @@ angular.module('kpk.controllers')
   function ($scope, $modalInstance, $window, connect, messenger, request) {
     var hasErrors = !!request.errors;
 
-    $scope.errors = request.errors;
-    var total = $scope.total = {};
-    $scope.data = request.data;
+    var session = $scope.session = {};
 
-    $scope.data.forEach(function (item) {
+    $scope.errors = request.errors;
+    $scope.transactions = request.transactions;
+    $scope.balances = request.balances;
+    var total = $scope.total = {};
+
+    $scope.balances.forEach(function (item) {
       total.before = (total.before || 0) + item.balance;
       total.debit = (total.debit || 0) + item.debit;
       total.credit = (total.credit || 0) + item.credit;
       total.after = (total.after || 0) + item.balance + (item.credit - item.debit);
     });
+
+    var dates = $scope.transactions.map(function (row) {
+      return new Date(row.trans_date);
+    });
+
+    session.max = Math.max.apply(Math.max, dates);
+    session.min = Math.min.apply(Math.min, dates);
+    session.count = $scope.transactions.reduce(function (a,b) { return a + b.lines; }, 0);
 
     $scope.submit = function submit () {
       connect.fetch('/trialbalance/submit/'+ request.key +'/')
