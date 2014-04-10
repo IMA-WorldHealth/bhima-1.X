7b732a48b252ea0bcb5062136a02a4f024b3b553
Steven Fountain <StevenFountain@live.co.uk>
2014-04-10 11:59:55 +0100

    Income/expense list configuration, style update
    Former-commit-id: 16d175b98dddfd843695d2ef69cf975d859f6967 [formerly 4e26249d2bb5063f6f47cab4c3da9b0750e71133]
    Former-commit-id: 650bf6f39471767a1f99c26932cbcdef893186d9

--- app/partials/primary_cash/primary.js
+++ app/partials/primary_cash/primary.js
@@ -1,7 +1,41 @@
 angular.module('kpk.controllers')
 .controller('primaryCash', [
   '$scope',
-  function ($scope) { 
-    console.log('initialised primary cash');
+  'validate',
+  function ($scope, validate) { 
+    var dependencies = {}, session = $scope.session = {};
+    var configuration = $scope.configuration = {};
+
+    dependencies.cashBox = { 
+      query : { 
+        tables : { 
+          cash_box : { columns : ['id', 'text', 'project_id', 'is_auxillary'] }
+        },
+        where : ['cash_box.is_auxillary=0']
+      }
+    };
+
+    configuration.income = [
+      {
+        key : 'PURCHASE',
+        link : '/primary_cash/income/purchase/'
+      }
+    ];
+
+    configuration.expense = [
+      {
+        key : 'GENERIC_EXPENSE',
+        link : '/primary_cash/expense/generic/'
+      }
+    ];
+     
+    validate.process(dependencies).then(initialise);
+
+    function initialise(model) { 
+      angular.extend($scope, model);
+      
+      session.cashbox = model.cashBox.data[0].id;
+    }
+
   }
 ]);
