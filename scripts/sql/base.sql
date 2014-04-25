ec3d55f3236fcc6d80746e4ef8eaf1eb17f53130
Steven Fountain <StevenFountain@live.co.uk>
2014-04-25 09:45:50 +0100

    ID conflict, avoiding messy reset with remote
    Former-commit-id: fe7deb8b8b1ad8610a034d7d21adabbeb014901d [formerly 7fef08e8f8207ce6da6336651ac4a5c053e01c19]
    Former-commit-id: 8a72b999621756e31fd855a29a13826bf58a0833

--- scripts/sql/base.sql
+++ scripts/sql/base.sql
@@ -91,7 +91,7 @@ INSERT INTO `unit` VALUES
 (78,'Project','TREE.PROJECT','',1,0,'/partials/projects/','/projects'),
 (79,'Patient Standing','TREE.PATIENT_STANDING','',10,0,'/partials/reports/patient_standing/','/reports/patient_standing'),
 (80,'Employee', 'TREE.EMPLOYEE', 'Employee management', 1, 0, 'partials/employee/', '/employee'),
-(80,'Depot Management','TREE.DEPOT','',11,0,'/partials/inventory/depot/','inventory/depot');
+(81,'Depot Management','TREE.DEPOT','',11,0,'/partials/inventory/depot/','inventory/depot');
 
 -- base user & permissions
 INSERT INTO `user` (id, username, password, first, last, email, logged_in) VALUES 
