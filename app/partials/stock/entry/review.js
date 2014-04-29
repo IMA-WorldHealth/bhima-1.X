5e718e6e65e5341922606f8151eb71943c0cc779
jniles <jonathanwniles@gmail.com>
2014-04-29 15:36:30 +0100

    fleshed out stock review page

--- app/partials/stock/entry/review.js
+++ app/partials/stock/entry/review.js
@@ -2,16 +2,31 @@ angular.module('kpk.controllers')
 .controller('stock.review', [
   '$scope',
   '$location',
+  'validate',
   'appstate',
   'connect',
-  function ($scope, $location, appstate, connect) {
+  function ($scope, $location, validate, appstate, connect) {
+    /*
     var session = $scope.session = {};
 
     appstate.register('project', function (project) {
       $scope.project = project;
-      angular.extend(session, appstate.get('stock.lots'));
-      console.log('session', session);
+      angular.extend(session, appstate.get('stock.review'));
     });
+
+
+    function processStock () {
+      var stock = [];
+      session.lots.data.forEach(function (stockLot) {
+        console.log(stockLot);
+      });
+    }
+
+    $scope.submit = function () {
+      processStock();
+      //connect.basicPut('stock');
+    };
+    */
 
   }
 ]);
