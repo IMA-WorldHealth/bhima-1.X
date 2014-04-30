04390d63ea55a6e077058d71ed6a6b29d024e479
Steven Fountain <StevenFountain@live.co.uk>
2014-04-30 11:02:32 +0100

    account statement layout, server side structure
    Former-commit-id: c4a6a3b5e5aae509319469e26baba77255add690 [formerly 131cb9e29caff1034608bbfa92e58b08f1c00b8c]
    Former-commit-id: 591446071268844b0a5c195d888e562351928e66

--- app/partials/reports/account_statement/account_statement.js
+++ app/partials/reports/account_statement/account_statement.js
@@ -1,9 +1,58 @@
 angular.module('kpk.controllers').controller('accountStatement',
 [
   '$scope',
-  'validate',
-  function ($scope, validate) {
+  '$q',
+  '$http',
+  'appstate',
+  'uuid',
+  function ($scope, $q, $http, appstate, uuid) {
     var dependencies = {};
+
+    // TODO Receive this through $routeParams
+    var accountId = 1062;
+
+    // Temporary details not yet stored
+    var accountCreated = '03-05-2014';
+  
+    dependencies.report = {
+      // query : '/report/account_statement/'
+    };
+
+    processProject()
+    .then(
+      processReport
+    ).then(
+      initialise
+    );
+
+    function initialise(model) {
+      console.log('[accountStatement]', 'fetched report', model); 
+    }
+    
+    function processReport() {
+      var statementParams = {
+        dateFrom : '03-05-2014',
+        dateTo : '05-30-2014',
+        order : 'date',
+        limit : 10
+      };
+
+      dependencies.report.query =
+        '/reports/accountStatement/?' +
+        JSON.stringify(statementParams);
+
+      return $http.get(dependencies.report.query);
+    }
+
+    function processProject() {
+      var deferred = $q.defer();
+      
+      appstate.register('project', function (result) {
+        $scope.project = result;
+        deferred.resolve(result);
+      });
+      return deferred.promise;
+    }
      
   }
 ]);
