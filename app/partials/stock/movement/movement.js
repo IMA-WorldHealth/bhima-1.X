86e6acffd2f8f966597ab6a1ce1a530d2e59b5b4
jniles <jonathanwniles@gmail.com>
2014-05-04 15:22:33 +0100

    added intial stock movement module
    Former-commit-id: 30477072a28642ba3c9a91a388c62138ca3c4098 [formerly ac88b338b248ab08b7c5e9cadb16c1f13d7eae1b]
    Former-commit-id: b3de93dc92a81cc3ae32c6a128cc447477ab6ba8

---
+++ app/partials/stock/movement/movement.js
@@ -0,0 +1,114 @@
+angular.module('kpk.controllers')
+.controller('stock.movement', [
+  '$scope',
+  '$location',
+  'validate',
+  'appstate',
+  'connect',
+  'messenger',
+  'uuid',
+  function ($scope, $location, validate, appstate, connect, messenger, uuid) {
+    var dependencies = {};
+    var session = $scope.session = { doc : {}, rows : [] };
+    
+    dependencies.depots = {
+      query : {
+        tables : {
+          'depot' : {
+            columns : ['id', 'text']
+          }
+        }
+      }
+    };
+
+    dependencies.movements = {
+      query : {
+        tables : {
+          'stock_movement' : {
+            columns : ['document_id', 'tracking_number', 'direction', 'date', 'quantity', 'depot_id', 'destination']
+          }
+        }
+      }
+    };
+
+    dependencies.stock = {
+      query : {
+        tables : {
+          'stock' : {
+            columns : ['tracking_number']
+          }
+        },
+        where : ['stock.quantity>0']
+      }
+    };
+
+    function error (err) {
+      messenger.error(err);
+    }
+
+    function startup (models) {
+      angular.extend($scope, models);
+      $scope.newForm();
+    }
+
+    $scope.newForm = function newForm () {
+      session = $scope.session = { doc: {}, rows : [] };
+      session.doc.document_id = uuid();
+      session.doc.date = new Date();
+      $scope.addRow();
+    };
+
+    $scope.addRow = function addRow () {
+      session.rows.push({quantity : 0, destination : ''});
+    };
+
+    $scope.removeRow = function (idx) {
+      session.rows.splice(idx, 1);
+    };
+
+    $scope.submit = function () {
+      var rows = [];
+      session.rows.forEach(function (row) {
+        rows.push(angular.extend(row, session.doc));
+      });
+
+      connect.basicPut('stock_movement', rows)
+      .then(function (res) {
+        messenger.success('STOCK.MOVEMENT.SUCCESS');
+      })
+      .catch(function (err) {
+        messenger.error(err);
+      });
+    };
+
+    $scope.$watch('session', function () {
+      if (!session.rows) {
+        session.valid = false;
+        return;
+      }
+
+      var validRows = session.rows.every(function (item) {
+        return angular.isDefined(item.tracking_number) &&
+          angular.isDefined(item.quantity) &&
+          item.quantity > 0 &&
+          angular.isDefined(item.destination);
+      });
+
+      session.valid = validRows &&
+        angular.isDefined(session.doc.document_id) &&
+        angular.isDefined(session.doc.date) &&
+        angular.isDefined(session.doc.depot_id) &&
+        angular.isDefined(session.doc.direction);
+
+    }, true);
+
+    appstate.register('project', function (project) {
+      $scope.project = project;
+      dependencies.depots.query.where =
+        ['depot.enterprise_id=' + project.enterprise_id];
+      validate.process(dependencies, ['depots', 'stock'])
+      .then(startup).catch(error);
+    });
+  
+  }
+]);
