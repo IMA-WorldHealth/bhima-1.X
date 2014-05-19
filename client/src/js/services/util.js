0aa89b1270ebe74e30345a6464f8aec1c36ea58b
jniles <jonathanwniles@gmail.com>
2014-05-19 16:38:27 +0100

    separate service components added

---
+++ client/src/js/services/util.js
@@ -0,0 +1,37 @@
+angular.module('bhima.services')
+.service('util', function () {
+  this.formatDate = function(dateString) {
+    return new Date(dateString).toDateString();
+  };
+
+  this.convertToMysqlDate = function (dateParam) {
+    var date = new Date(dateParam),
+      annee,
+      mois,
+      jour;
+    annee = String(date.getFullYear());
+    mois = String(date.getMonth() + 1);
+    if (mois.length === 1) {
+      mois = '0' + mois;
+    }
+
+    jour = String(date.getDate());
+    if (jour.length === 1) {
+      jour = '0' + jour;
+    }
+    return annee + '-' + mois + '-' + jour;
+  };
+
+  this.isDateAfter = function(date1, date2){
+    date1 = new Date(date1).setHours(0,0,0,0);
+    date2 = new Date(date2).setHours(0,0,0,0);
+    return date1 > date2;
+  };
+
+  this.areDatesEqual = function(date1, date2) {
+    date1 = new Date(date1).setHours(0,0,0,0);
+    date2 = new Date(date2).setHours(0,0,0,0);
+    return date1 === date2;
+  };
+
+});
