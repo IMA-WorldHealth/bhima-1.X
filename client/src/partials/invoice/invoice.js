f88c3300effa180667b83b51aabb929c7e905663
Steven Fountain <StevenFountain@live.co.uk>
2014-05-27 07:57:03 +0100

    Receipt for stock movement verification

--- client/src/partials/invoice/invoice.js
+++ client/src/partials/invoice/invoice.js
@@ -169,18 +169,55 @@ angular.module('bhima.controllers')
         query : {
           tables : {
             depot : {
-              columns : ['reference', 'text']
+              columns : ['reference']
             },
             movement : {
               columns : ['uuid', 'depot_entry', 'depot_exit', 'tracking_number', 'quantity', 'date']
+            },
+            stock : {
+              columns : ['inventory_uuid', 'tracking_number', 'expiration_date', 'entry_date', 'lot_number']
+            },
+            inventory : {
+              columns : ['code', 'text']
             }
           },
           where : ['movement.document_id='+invoiceId],
-          join : ['depot.uuid=movement.depot_exit']
+          join : [
+            'depot.uuid=movement.depot_exit',
+            'movement.tracking_number=stock.tracking_number',
+            'stock.inventory_uuid=inventory.uuid'
+          ]
         }
       };
 
       validate.process(dependencies).then(fetchDepot);
+    }
+  
+    // Hack to get around not being able to perform any join through connect
+    function fetchDepot(model) {
+      var depot_entry = model.movement.data[0].depot_entry;
+      var depot_exit = model.movement.data[0].depot_exit;
+      
+      dependencies.depots = {
+        query : {
+          identifier : 'uuid',
+          tables : {
+            depot : {
+              columns : ['uuid','reference', 'text']
+            }
+          },
+          where : ['depot.uuid=' + depot_exit, 'OR', 'depot.uuid=' + depot_entry]
+        }
+      };
+
+      validate.process(dependencies)
+      .then(function (depotModel) {
+        angular.extend($scope, depotModel);
+        $scope.depotEntry = depotModel.depots.get(depot_entry);
+        $scope.depotExit = depotModel.depots.get(depot_exit);
+          
+        console.log(depotModel);
+      });
     }
 
     function parseMovement(model) {
