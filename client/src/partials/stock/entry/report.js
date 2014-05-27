2e7368dbc02aae366ad0495272bb33901e4763e4
jniles <jonathanwniles@gmail.com>
2014-05-27 16:03:23 +0100

    completed stock entry
    Former-commit-id: d8d43cb760fc1208647f1e116d155fd047766136 [formerly 3e37b91c0278f71bfec0e284019b28fa9b304441]
    Former-commit-id: f6a618fcd4fb537018e32ff18c496051d68e4663

--- client/src/partials/stock/entry/report.js
+++ client/src/partials/stock/entry/report.js
@@ -2,11 +2,14 @@ angular.module('bhima.controllers')
 .controller('stock.entry.report', [
   '$scope',
   '$routeParams',
+  '$window',
   'validate',
   'appstate',
-  function ($scope, $routeParams, validate, appstate) {
+  function ($scope, $routeParams, $window, validate, appstate) {
     var dependencies = {},
         session = $scope.session = {};
+
+    session.timestamp = new Date();
 
     dependencies.receipt = {
       query : {
@@ -15,25 +18,46 @@ angular.module('bhima.controllers')
           movement : {
             columns : ['uuid', 'depot_entry', 'tracking_number', 'quantity', 'date']
           },
+          stock : {
+            columns : ['expiration_date', 'entry_date', 'lot_number', 'purchase_order_uuid']
+          },
+          inventory : {
+            columns : ['code']
+          },
           depot : {
             columns : ['reference', 'text']
           }
         },
-        join : ['movement.depot_entry=depot.uuid']
+        join : [
+          'movement.depot_entry=depot.uuid',
+          'movement.tracking_number=stock.tracking_number',
+          'stock.inventory_uuid=inventory.uuid'
+        ]
       }
+    };
+
+    function sum(a, b) {
+      return a + b.quantity;
+    }
+
+    $scope.print = function print() {
+      $window.print();
     };
 
     function startup(models) {
       angular.extend($scope, models);
+
+      session.total = $scope.receipt.data.reduce(sum, 0);
     }
 
     appstate.register('project', function (project) {
       $scope.project = project;
 
-      session.document_id = $routeParams.document_id;
-      if (!session.document_id) { return; }
+      session.documentId = $routeParams.documentId;
 
-      dependencies.query.where =
+      if (!session.documentId) { return; }
+
+      dependencies.receipt.query.where =
         ['movement.document_id=' + session.documentId];
 
       validate.process(dependencies)
