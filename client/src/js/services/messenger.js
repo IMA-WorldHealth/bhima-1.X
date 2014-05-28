760f97ab5618190c033f9493bbb9ecc3a4842708
jniles <jonathanwniles@gmail.com>
2014-05-28 11:00:21 +0100

    messenger now supports  messages

--- client/src/js/services/messenger.js
+++ client/src/js/services/messenger.js
@@ -10,38 +10,41 @@ angular.module('bhima.services')
 
     var self = this,
       messages = self.messages = [],
-      indicies = {};
+      LIFESPAN_LIMIT = 5000,
+      timers = {};
 
-    function enqueue(data, timer) {
+    function lifespan(message) {
       var id = Date.now();
-      data.id = id;
-      data.msg = $sce.trustAsHtml(data.msg); // allow html insertion
-      messages.push(data);
-      indicies[id] = $timeout(function () {
+      message.id = id;
+      timers[id] = $timeout(function () {
         var i = messages.length;
-
         while (i--) {
-          if (messages[i].id === id) {
-            messages.splice(i, 1);
-            break;
-          }
+          if (messages[i].id === id) { break; }
         }
-
-      }, timer || 3000);
+        messages.splice(i, 1);
+      },  LIFESPAN_LIMIT);
     }
 
-    (function init() {
-      ['info', 'warning', 'danger']
-      .forEach(function (type) {
-        self[type] = function (message, timer) {
-          enqueue({type: type, msg: message }, timer);
-        };
-      });
-    })();
+    function trust(data) {
+      return $sce.trustAsHtml(data);
+    }
+
+    function enqueue(message) {
+      // trust text
+      message.namespace =  trust(message.namespace);
+      message.description = trust(message.description);
+      message.status = trust(message.status);
+
+      if (!message.closable) {
+        lifespan(message);
+      }
+
+      messages.push(message);
+    }
 
     self.close = function close(idx) {
       // cancel timeout and splice out
-      $timeout.cancel(indicies[idx]);
+      if (timers[idx]) { $timeout.cancel(timers[idx]); }
       messages.splice(idx, 1);
     };
 
@@ -57,32 +60,62 @@ angular.module('bhima.services')
           description : err
         };
       }
-      messages.push(err);
+      enqueue(err);
     };
 
-    self.primary  = function primary(data) {
-      angular.extend(data, { type : 'primary', closable : true });
-      messages.push(data);
+    self.primary  = function primary(data, closable) {
+      angular.extend(data, { type : 'primary', closable : closable });
+      enqueue(data);
     };
 
-    self.warn = function warn(data) {
-      angular.extend(data, { type : 'warning', closable : true });
-      messages.push(data);
+    self.warn = function warn(data, closable) {
+      angular.extend(data, { type : 'warning', closable : closable });
+      enqueue(data);
     };
 
-    self.success = function success(data) {
+    self.success = function success(data, closable) {
       if (typeof data === 'object') {
         angular.extend(data, { type : 'success', closable : true});
       } else {
         data = {
           type : 'success',
-          closable : true,
+          closable : closable,
           namespace : 'MESSENGER',
           description : data
         };
       }
 
-      messages.push(data);
+      enqueue(data);
+    };
+
+    self.info = function info(data, closable) {
+      if (typeof data === 'object') {
+        angular.extend(data, { type : 'info', closable : true});
+      } else {
+        data = {
+          type : 'info',
+          closable : closable,
+          namespace : 'MESSENGER',
+          description : data,
+        };
+      }
+
+      enqueue(data);
+    };
+
+    self.danger = function danger(data, closable) {
+      if (typeof data === 'object') {
+        angular.extend(data, { type : 'error', closable : true});
+      } else {
+        data = {
+          type : 'error',
+          closable : closable,
+          namespace : 'MESSENGER',
+          description : '[DEPRECATED] ' + data
+        };
+      }
+
+      enqueue(data);
     };
   }
 ]);
