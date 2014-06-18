b554edd5c981e8ed79fbaac6d2fc6d7c49eac91d
Steven Fountain <StevenFountain@live.co.uk>
2014-06-18 18:41:52 +0100

    Correctly writes consumption given correct lot assignment; consumption table primary changed to UUID, document ID is not unique

--- client/src/partials/stock/exit/distribution.js
+++ client/src/partials/stock/exit/distribution.js
@@ -15,7 +15,8 @@ angular.module('bhima.controllers')
       index : -1,
       state : null,
       depot : $routeParams.depotId,
-      lotSelectionSuccess : false
+      lotSelectionSuccess : false,
+      lotSelectionFailure : false
     };
     var distribution = {}, dependencies = {};
 
@@ -24,17 +25,17 @@ angular.module('bhima.controllers')
       {
         title : 'Locate Patient',
         template : 'patientSearch.tmpl.html',
-        method : findPatient
+        method : null
       },
       {
         title : 'Select prescription sale',
         template : 'selectSale.tmpl.html',
-        method : findPatient
+        method : null
       },
       {
         title : 'Allocate medicine',
         template : 'allocateLot.tmpl.html',
-        method : findPatient
+        method : null
       }
     ];
 
@@ -77,7 +78,7 @@ angular.module('bhima.controllers')
       // FIXME
       session.index += 1;
       session.state = moduleDefinition[session.index];
-      session.state.method();
+      // session.state.method();
     }
 
     function selectSale(sale) {
@@ -163,7 +164,7 @@ angular.module('bhima.controllers')
         });
 
         console.log('found ', validUnits, ' in', sessionLots);
-
+      
         if (validUnits < saleItem.quantity) {
           console.log('LIMITED STOCK, ');
           saleItem.stockStatus = stock.LIMITED_STOCK;
@@ -196,15 +197,18 @@ angular.module('bhima.controllers')
         console.log('validating lot', item);
 
         // ignore non consumables (FIXME better way tod do this across everything)
-        if (item.!consumable) return false;
-        if (!item.recomendedLots) return true;
-         
-        console.log('recomendedLots');
+        if (!item.consumable) return false;
+        
+        // FIXME hack - if a status has been reported, cannot be submitted
+        if (item.stockStatus) return true;
+
         // console.log('item has lots assigned');
       });
 
       console.log('looped through lots, found invalid', invalidLots);
-
+      
+      // Update on failed attempt - EVERY validation 
+      session.lotSelectionFailure = invalidLots;
       return !invalidLots;
     }
 
@@ -226,335 +230,42 @@ angular.module('bhima.controllers')
       return connect.req(query);
     }
 
-    function init() { 
+    function submitConsumption() { 
+      var submitItem = [];
+      
+      // Ensure validation is okay 
+      if (!session.lotSelectionSuccess) return messenger.danger('Cannot verify lot allocation');
+      
+      // Iterate through items, write consumption line for each lot
+      session.sale.details.forEach(function (consumptionItem) { 
+        
+        if (!angular.isDefined(consumptionItem.recomendedLots)) return;
 
-    }
+        consumptionItem.recomendedLots.forEach(function (lot) { 
+          submitItem.push({
+            uuid : uuid(),
+            depot_uuid : session.depot,
+            date : util.convertToMysqlDate(new Date()),
+            document_id : consumptionItem.sale_uuid,
+            tracking_number : lot.details.tracking_number,
+            quantity : lot.quantity
+          });
+        });
+      });
 
-    function findPatient() { 
-
+      console.log('items', submitItem);
+      connect.basicPut('consumption', submitItem)
+      .then(function () {
+        messenger.success('Consumption successfully written', true);
+      })
+      .catch(function (error) {
+        console.log(error);
+        messenger.error(error);
+      });
     }
 
     $scope.selectSale = selectSale;
     $scope.initialiseDistributionDetails = initialiseDistributionDetails;
-    // distribution.visible = true;
-    // distribution.noEmpty = false;
-    // distribution.item_records = [];
-    // distribution.moving_records = [];
-    // distribution.rows = [];
-    // distribution.sales = [];
-
-
-    // dependencies.stocks = {
-    //   query : {
-    //     tables : {
-    //       'stock' : {
-    //         columns : ['inventory_uuid', 'expiration_date', 'entry_date', 'lot_number', 'purchase_order_uuid', 'tracking_number', 'quantity']
-    //       },
-    //       'inventory' : {
-    //         columns : ['uuid', 'text', 'enterprise_id', 'code', 'price', 'stock']
-    //       }
-    //     },
-    //     join : ['stock.inventory_uuid=inventory.uuid']
-    //   }
-    // };
-
-    // dependencies.project = {
-    //   required : true,
-    //   query : {
-    //     tables : {
-    //       'project' : {
-    //         columns : ['abbr', 'name']
-    //       }
-    //     }
-    //   }
-    // };
-
-    // dependencies.debitor_group = {
-    //   required : true,
-    //   query : {
-    //     tables : {
-    //       'debitor_group' : {
-    //         columns : ['is_convention', 'name', 'uuid', 'account_id']
-    //       }
-    //     }
-    //   }
-    // };
-
-    // function initialiseDistributionDetails (selectedDebitor){
-    //   if(!selectedDebitor) return;
-    //   distribution.noEmpty = true; $scope.ready = 'ready';
-    //   distribution.selectedDebitor = selectedDebitor;
-    //   connect.fetch('/ledgers/distributableSale/' + selectedDebitor.debitor_uuid)
-    //   .then(function (data) {
-    //     data.forEach(function (row) {
-    //       row.reference = getAbbr(row.project_id)+row.reference;
-    //       row.etat = getState(row);
-    //     });
-    //     distribution.sales = data;
-    //   });
-    //   window.distribution = distribution;
-    // }
-
-    // function getAbbr(project_id){
-    //   return $scope.model.project.data.filter(function (item){
-    //     return item.id = project_id;
-    //   })[0].abbr;
-    // }
-
-    // function getState (sale){
-    //   return ($scope.model.debitor_group.data.filter(function (item) {
-    //     return item.account_id == sale.account_id;
-    //   })[0].is_convention == 1)? 'CONVENTION' : (sale.balance>0)? 'NON PAYE' : 'PAYE';
-    // }
-
-    // function init (model){
-    //   //init model
-    //   $scope.model = model;
-    // }
-    // function movement (consumption) {
-    //   this.document_id = uuid();
-    //   this.tracking_number = consumption.tracking_number;
-    //   this.direction = 'Exit';
-    //   this.date = consumption.date;
-    //   this.quantity = 0;
-    //   this.depot_id = 1;
-    //   this.destination = 1;
-    //   return this;
-    // }
-
-    // function sanitize (){
-    //   distribution.rows = $scope.selectedSale.sale_items;
-
-    //   distribution.item_records = distribution.rows.map(function (it){
-    //     it.consumption_infos = [];
-    //     var q = it.quantity;
-    //     distribution.records = it.lots.map(function (lot){
-    //       var record;
-    //       if(lot.setted){
-    //         if(q>0){
-    //           var amount;
-    //           if(q-lot.quantity>0){
-    //             q-=lot.quantity;
-    //             amount = lot.quantity;
-    //             lot.current_quantity = 0;
-    //           }else{
-    //             amount = q;
-    //             lot.current_quantity = lot.quantity - q;
-    //             q=0;
-    //           }
-    //           record = {
-    //             document_id : uuid(),
-    //             tracking_number : lot.tracking_number,
-    //             date : util.convertToMysqlDate(new Date().toString()),
-    //             depot_id : 1,
-    //             amount : amount,
-    //             sale_uuid : $scope.selectedSale.inv_po_id
-    //           }
-    //           it.consumption_infos.push(record);
-    //           return record;
-    //         }
-    //       }
-    //     })
-
-    //     return distribution.records = distribution.records.filter(function (item){
-    //       return (item)? true : false;
-    //     })
-    //   })
-
-    //   distribution.moving_records = distribution.rows.map(function (it){
-    //     return distribution.records = it.consumption_infos.map(function (consumption_info){
-
-    //           return {
-    //             document_id : uuid(),
-    //             tracking_number : consumption_info.tracking_number,
-    //             direction : 'Exit',
-    //             date : util.convertToMysqlDate(new Date().toString()),
-    //             quantity : consumption_info.amount,
-    //             depot_id : 1, //for now
-    //             destination :1 //for patient
-    //           }
-    //     })
-    //   })
-    // }
-
-    // function submit (){
-    //   sanitize();
-    //   if(stockAvailability()){
-    //     doConsumption()
-    //     .then(doMoving)
-    //     //.then(decreaseStock)
-    //     .then(function(result){
-    //       console.log('[result ...]')
-    //     });
-    //   }else{
-    //     messenger.danger('Le stock dans le (s) lot (s) selectionne (s) n\'est pas disponible pour convrir la quantite demandee');
-    //   }
-    // }
-
-    // function decreaseStock (){
-    //   console.log(distribution);
-    //   // return $q.all(
-    //   //   distribution.item_records.map(function (item_record){
-    //   //     return connect.basicPut('consumption', item_record)
-    //   //   })
-    //   // )
-    // }
-
-    // function updateStock (){
-    //   validate.refresh(dependencies, ['stock'])
-    //   .then(function (model){
-    //   })
-    // }
-
-    // function doConsumption (){
-    //   return $q.all(
-    //     distribution.item_records.map(function (item_record){
-    //       return connect.basicPut('consumption', item_record)
-    //     })
-    //   )
-    // }
-
-    // function doMoving(){
-    //   return $q.all(
-    //     distribution.moving_records.map(function (moving){
-    //       return connect.basicPut('stock_movement', moving)
-    //     })
-    //   )
-    // }
-
-    // function handleSaleResponse(result) {
-    //   //recoverCache.remove('session');
-    //   //$location.path('/invoice/sale/' + result.data.saleId);
-    // }
-
-    // function add (idx) {
-    //   if($scope.selectedSale) return;
-    //   $scope.selectedSale =  $scope.distribution.sales.splice(idx, 1)[0];
-    //   dependencies.sale_items = {
-    //     required : true,
-    //     query : {
-    //       tables : {
-    //         'sale_item' : {columns : ['uuid', 'inventory_uuid', 'quantity']},
-    //         'inventory' : {columns : ['code', 'text', 'stock']}
-    //       },
-    //       join  : ['sale_item.inventory_uuid=inventory.uuid'],
-    //       where : ['sale_item.sale_uuid='+$scope.selectedSale.inv_po_id]
-    //     }
-    //   };
-    //   validate.process(dependencies,['sale_items']).then(initialiseProcess);
-    // }
-
-    // function remove (idx) {
-    //   $scope.distribution.sales.push($scope.selectedSale);
-    //   $scope.selectedSale= null;
-    //   $scope.selected = 'null';
-    // }
-
-    // function initialiseProcess (model) {
-    //   $scope.selected = 'selected';
-    //   //var items = ;
-    //   var filtered;
-    //   filtered = model.sale_items.data.filter(function (item) {
-    //     return item.code.substring(0,1) !== '8';
-    //   });
-    //   filtered.forEach(function (it) {
-    //     it.tracking_number = null;
-    //     it.avail = (it.quantity <= it.stock) ? 'YES' : 'NO';
-    //   });
-    //   $scope.selectedSale.sale_items = filtered;
-
-    //   $scope.selectedSale.sale_items.forEach(function (sale_item){
-    //     connect.fetch('/lot/' +sale_item.inventory_uuid)
-    //     .then(function processLots (lots){
-    //       if(!lots.length){
-    //         distribution.hasLot = false;
-    //         messenger.danger('Pas de lot recuperes');
-    //         return;
-    //       }
-
-    //       distribution.hasLot = true;
-
-    //       if(lots.length && lots.length == 1){
-    //         lots[0].setted = true;
-    //         sale_item.lots = lots;
-    //         return;
-    //       }
-
-    //       tapon_lot = null;
-    //       for (var i = 0; i < lots.length -1; i++) {
-    //         for (var j = i+1; j < lots.length; j++) {
-    //           if(util.isDateAfter(lots[i].expiration_date, lots[j].expiration_date)){
-    //             tapon_lot = lots[i];
-    //             lots[i] = lots[j];
-    //             lots[j] = tapon_lot;
-    //           }
-    //         }
-    //       }
-
-    //         var som = 0;
-    //         lots.forEach(function (lot){
-    //           som+=lot.quantity;
-    //           if(sale_item.quantity > som){
-    //             lot.setted = true;
-    //           }else{
-    //             if((som - lot.quantity) < sale_item.quantity) lot.setted = true;
-    //           }
-    //         });
-    //         sale_item.lots = lots;
-    //     });
-    //   });
-    // }
-
-    // function verifySubmission (){
-    //   if(!distribution.hasLot) return true
-    //   if($scope.selectedSale){
-    //      if($scope.selectedSale.sale_items){
-    //       var availability = $scope.selectedSale.sale_items.some(function (sale_item) {
-    //         return sale_item.avail == 'NO';
-    //       })
-    //       if(availability) return availability
-    //       return false;
-    //     }else{
-    //       return true;
-    //     }
-    //   }
-    // }
-    // function handleError (){
-    //   messenger.danger('impossible de recuperer des lots !');
-    // }
-
-    // function resolve (){
-    //   return !$scope.ready;
-    // }
-
-    // function stockAvailability() {
-    //   var resultat =  $scope.selectedSale.sale_items.some(function (si){
-    //     var q = 0;
-    //     si.lots.forEach(function (lot){
-    //       if(lot.setted) q+=lot.quantity;
-    //     })
-    //     return (si.quantity > q)
-    //   })
-    //   return !resultat;
-    // }
-
-    // appstate.register('project', function (project) {
-    //   $scope.project = project;
-    //   validate.process(dependencies)
-    //   .then(init)
-    //   .catch(function (error) {
-    //     console.error(error);
-    //   });
-    // });
-
-    // //exposition
-    // $scope.distribution = distribution;
-    // $scope.initialiseDistributionDetails = initialiseDistributionDetails;
-    // $scope.submit = submit;
-    // $scope.add = add;
-    // $scope.remove = remove;
-    // $scope.resolve = resolve;
-    // $scope.verifySubmission = verifySubmission;
-    // $scope.stockAvailability = stockAvailability;
+    $scope.submitConsumption = submitConsumption; 
   }
 ]);
