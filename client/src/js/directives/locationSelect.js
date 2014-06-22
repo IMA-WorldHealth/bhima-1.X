f866bc81e051249006b786ac6906aa672081ff69
Steven Fountain <StevenFountain@live.co.uk>
2014-06-22 10:17:45 +0100

    Location select logic, refactor
    Former-commit-id: b2922c82cd7658cecd744d0750d3048b382ebaa5 [formerly 2fe042618b695946e178c094adeddf713befaaab]
    Former-commit-id: e5fff5e1d19ce35e455804d5ccaa433380a6f341

--- client/src/js/directives/locationSelect.js
+++ client/src/js/directives/locationSelect.js
@@ -1,47 +1,127 @@
 angular.module('bhima.directives')
-.directive('locationSelect', ['validate', 'connect', function (validate, connect) { 
+.directive('locationSelect', ['appstate', 'connect', function (appstate, connect) { 
   return {
     restrict : 'A',
     replace : true,
     transclue : true,
-    template : '<div><div class="form-group" ng-repeat="config in locationConfig">{{config.label}}</div></div>',
+    template : '<div><div class="form-group" ng-repeat="config in locationSelect.locationConfig"><label for="location-select-{{config.id}}" class="control-label">{{config.label | translate}}</label><div class="pull-right"><span class="glyphicon glyphicon-globe"></span> 0</div><select ng-disabled="!locationSelect.session.locationSearch" class="form-bhima" id="location-select-{{config.id}}"></select></div></div>',
     link : function (scope) {
       
+      // TODO replace with correct scope management
+      var namespace = scope.locationSelect = {};
+      var session = namespace.session = {};
+
       // TODO rename variables etc. with useful names 
       var locationIndex = {};
-      var locationConfig = scope.locationConfig = {
+      var locationConfig = namespace.locationConfig = {
         village : {
           dependency : 'sector',
           label : 'LOCATION.VILLAGE',
-          column : 'name'
+          column : 'name',
+          id : 'village'
         },
         sector : {
+          dependency : 'province',
           label : 'LOCATION.SECTOR',
-          column : 'name'
+          column : 'name',
+          id : 'sector'
         },
         province : {
           dependency : 'country',
           label : 'LOCATION.PROVINCE',
-          column : 'name'
+          column : 'name',
+          id : 'province'
         },
         country : {
           dependency : null,
           label : 'LOCATION.COUNTRY',
-          column : 'country_en'
+          column : 'country_en',
+          id : 'country'
         }
       };
-      var locationStore = scope.locationStore = {};
-      
-      // TODO refactor
-      settup();
+      var locationStore = namespace.locationStore = {};
 
-      function settup() {
+      appstate.register('project', settup);
+
+      function settup(project) {
         indexLocationDependencies();
+        defineLocationRequests();
 
-        console.log('index', locationIndex);
+        fetchInitialLocation(project.location_id)
+        .then(initialiseLocation);
+      }
 
-        console.log('sector dependency :', locationConfig.sector.dependency);
-        console.log('requires sector : ', lookupDependency('sector'));
+      function fetchInitialLocation(villageUuid) {
+        return connect.fetch('/location/' + villageUuid);
+      }
+
+      function initialiseLocation(defaultLocation) {
+        defaultLocation = defaultLocation[0];
+
+        // Populate initial values (enterprise default)
+        Object.keys(locationConfig).forEach(function (key) {
+          locationStore[key] = {model : {}, value : null};
+          locationStore[key].value = defaultLocation[formatKeyId(key)];
+        });
+
+        // Initial request, update config with no dependency
+        updateLocation(lookupDependency(null), null);
+      }
+
+      function updateLocation(key, uuidDependency) {
+        fetchLocationData(key, uuidDependency)
+        .then(function (data) {
+          return assignLocationData(key, data);
+        });
+      }
+
+      function fetchLocationData(key, uuidDependency) {
+        var config = locationConfig[key];
+        var model = locationStore[key].model;
+
+        // Conditions
+        var requiredDependencyFailed = !uuidDependency && config.requires;
+        
+        if (requiredDependencyFailed) {
+          
+          // Clear results and stop propegation 
+          model = { data : [] };
+          if (config.dependency) {
+            fetchLocationData(config.dependency, null);
+          }
+          return ;
+        }
+        
+        if (config.dependency) {
+          config.request.query.where = [key + '.' + formatKeyId(config.dependency) + '=' + uuidDependency];
+        }
+        return connect.fetch(config.request);
+      }
+
+      function assignLocationData(key, result) {
+        var currentLocationValue, invalidCurrentLocation, locationsFound;
+        var store = locationStore[key];
+        var requiresCurrentKey = lookupDependency(key);
+  
+        store.model = result;
+        currentLocationValue = store.value;
+
+        // Conditions 
+        invalidCurrentLocation = angular.isNull(store.model.get(store.value));
+        locationsFound = store.model.data.length;
+
+        if (invalidCurrentLocation) {
+          
+          // Select default value if there are any 
+          if (locationsFound) {
+            currentLocationValue = store.value = store.model.data[0];
+          }
+        }
+        
+        // Call any configuration that requires on this key
+        if (requiresCurrentKey) {
+          updateLocation(requiresCurrentKey, currentLocationValue.uuid);
+        }
       }
 
       function indexLocationDependencies() {
@@ -53,20 +133,19 @@ angular.module('bhima.directives')
       function defineLocationRequests() {
         Object.keys(locationConfig).forEach(function (key) {
           var config = locationConfig[key];
-          var requires = lookupDependency(key);
           var request = {
             query : {
               identifier : 'uuid',
               tables : {},
-              order : [config.label]
+              order : [config.column]
             }
           };
           request.query.tables[key] = {
-            columns : ['uuid', config.label]
+            columns : ['uuid', config.column]
           };
           
-          if (requires) {
-            request.query.tables[key].columns.push(formatKeyId(requires));
+          if (config.dependency) {
+            request.query.tables[key].columns.push(formatKeyId(config.dependency));
           }
           config.request = request;
         });
