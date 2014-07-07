cc3ee05779c2540974c02f399f4754c70de99494
dedrick <kitamuka@gmail.com>
2014-07-07 16:32:49 +0100

    fixing purchase order list

--- client/src/partials/records/purchase_order_records/purchase_records.js
+++ client/src/partials/records/purchase_order_records/purchase_records.js
@@ -9,39 +9,24 @@ angular.module('bhima.controllers')
     var default_purchase = ($routeParams.purchaseID || -1);
 
     function init() {
-
       $scope.selected = null;
-
       var promise = fetchRecords();
       promise
         .then(function(model) {
-          //expose scope
           $scope.purchase_model = model;
-          //Select default
           if (default_purchase > 0) { $scope.select(default_purchase); }
 
         });
 
       $scope.post = function() {
         console.log('Request for post');
-  //      This could be an arry
         var selected = $scope.selected;
         var request = [];
-        /* support multiple rows selected
-        if(selected.length>0) {
-        selected.forEach(function(item) {
-        if(item.posted==0) {
-        request.push(item.id);
-        }
-        });
-        }*/
         if(selected) { request.push(selected.id); }
-        //if(selected) request.push({transact ion_id:1, service_id:1, user_id:1});
 
         connect.journal(request)
           .then(function(res) {
             console.log(res);
-  //          returns a promise
             if (res.status === 200) {
               invoicePosted(request);
             }
@@ -53,15 +38,11 @@ angular.module('bhima.controllers')
 
     $scope.select = function(id) {
       $scope.selected = $scope.purchase_model.get(id);
-      console.log('selected', $scope.selected);
     };
 
     function invoicePosted(ids) {
       var deferred = $q.defer();
       var promise_update = [];
-      /*summary
-      *   Updates all records in the database with posted flag set to true
-      */
       ids.forEach(function(invoice_id) {
         var current_invoice = $scope.invoice_model.get(invoice_id);
         console.log('Updating \'posted\'', invoice_id, current_invoice);
@@ -81,11 +62,30 @@ angular.module('bhima.controllers')
 
     function fetchRecords() {
       var deferred = $q.defer();
-
       $scope.selected = {};
-
-      connect.req({'tables' : {'purchase' : {'columns' : ['uuid', 'cost', 'currency_id', 'creditor_uuid', 'discount', 'purchase_date', 'paid']}}})
+      var requette = {
+        'tables' : {
+          'purchase' : {
+            'columns' : ['reference', 'cost', 'discount', 'purchase_date', 'paid']
+          },
+          'creditor' : {
+            'columns' : ['text']
+          },
+          'employee' : {
+            'columns' : ['name', 'prenom']
+          },
+          'user' : {
+            'columns' : ['first', 'last']
+          }
+        },
+        join : [
+        'purchase.creditor_uuid=creditor.uuid',
+        'purchase.purchaser_id=user.id',
+        'purchase.employee_id=employee.id']
+      }
+      connect.req(requette)
         .then(function(model) {
+          console.log('voici notre model ', model)
           deferred.resolve(model);
         });
 
