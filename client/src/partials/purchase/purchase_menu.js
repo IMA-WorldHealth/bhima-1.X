2698ace04f04092e7f2c1ec256e546ae132ac1cb
dedrick <kitamuka@gmail.com>
2014-07-18 23:01:42 +0100

    adding new structure for purchase order continuing
    Former-commit-id: 43188dd198733bd63e736d45b96d94edce63cc25 [formerly 02ec499a877b37e546408843258356f419d28a47]
    Former-commit-id: 7139ee8841d71674bc045de246822e48f5dd8e37

---
+++ client/src/partials/purchase/purchase_menu.js
@@ -0,0 +1,31 @@
+angular.module('bhima.controllers')
+.controller('purchase.menu', [
+  '$scope',
+  '$location',
+  '$translate',
+  function ($scope, $location, $translate) {
+
+    var configuration = $scope.configuration = {};
+
+    configuration.operations = [
+      {
+        key : $translate.instant('PURCHASE_MENU.PUCHASE_CREATION'),
+        link : '/purchase/create/'
+      },
+
+      {
+        key : $translate.instant('PURCHASE_MENU.CONFIRM'),
+        link : '/purchase/confirm/'
+      },
+
+      {
+        key : $translate.instant('PURCHASE_MENU.VIEW'),
+        link : '/purchase/view/'
+      }
+    ];
+
+    $scope.loadPath = function loadPath(path) {
+      $location.path(path);
+    };
+  }
+]);
