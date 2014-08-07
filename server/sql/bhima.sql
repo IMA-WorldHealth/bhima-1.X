7624b7ecce9b72518de407c4b9fafdcb8bf8f6e5
Jonathan Niles <jonathanwniles@gmail.com>
2014-08-07 19:39:58 +0100

    migrate.js upgraded to use new bhima.sql columns; removed fk dependencies in bhima.sql to build current tshikaji dataset

--- server/sql/bhima.sql
+++ server/sql/bhima.sql
@@ -268,11 +268,11 @@ create table `account` (
   key `account_type_id` (`account_type_id`),
   key `enterprise_id` (`enterprise_id`),
   key `cc_id`         (`cc_id`),
-  key `pc_id`         (`pc_id`),
+  -- key `pc_id`         (`pc_id`),
   constraint foreign key (`account_type_id`) references `account_type` (`id`),
   constraint foreign key (`enterprise_id`) references `enterprise` (`id`),
-  constraint foreign key (`cc_id`)         references `cost_center` (`id`),
-  constraint foreign key (`pc_id`)         references `profit_center` (`id`)
+  constraint foreign key (`cc_id`)         references `cost_center` (`id`)
+  -- constraint foreign key (`pc_id`)         references `profit_center` (`id`)
 ) engine=innodb;
 
 drop table if exists `creditor_group`;
