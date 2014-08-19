a0ab3f97ce309c5b7a64fb350feefd2608518a1f
Jonathan Niles <jonathanwniles@gmail.com>
2014-08-18 21:37:53 -0400

    removed old createPurchase code and route
    Former-commit-id: 6829286778d1e72c2016524cf05e775fc4a0315c [formerly b0971b890a1f3358e0c7e693c43c76f804a0d44d]
    Former-commit-id: 83ded3b06e46972f66796ff67f6f457d307f3be9

--- server/routes/createPurchase.js
+++ server/routes/createPurchase.js
@@ -18,6 +18,12 @@ module.exports = function (db, parser, journal, uuid) {
   //  - On sale move money from asset account to expense account,
   //    credit sale account
   
+  function values(obj) {
+    // get an array of an object's values
+    // cannot wait for es6's Object.values(o);
+    return Object.keys(obj).map(function (k) { return obj[k]; });
+  }
+
   var mod = {};
 
   mod.run = function run(userId, data) {
@@ -26,15 +32,11 @@ module.exports = function (db, parser, journal, uuid) {
     // finally the posting_journal
     var primaryCashId = uuid();
 
-    console.log('[DEBUG] Running createSale.run()');
-    console.log('[DBG] [data]', data);
-
-    return _writeCash(primaryCashId, userId, data.details)
+    return writeCash(primaryCashId, userId, data.details)
       .then(function () {
-        return _writeCashItems(primaryCashId, data.transaction);
+        return writeCashItems(primaryCashId, data.transaction);
       })
       .then(function () {
-        console.log('[DEBUG] Posting to Journal..');
         return postToJournal(primaryCashId, userId);
       })
       .then(function () {
@@ -42,13 +44,7 @@ module.exports = function (db, parser, journal, uuid) {
       });
   };
 
-  function values(obj) {
-    // get an array of an object's values
-    // cannot wait for es6's Object.values(o);
-    return Object.keys(obj).map(function (k) { return obj[k]; });
-  }
-
-  function _writeCash(primaryCashId, userId, data) {
+  function writeCash(primaryCashId, userId, data) {
     // write items to the primary_cash table
     var sql, params;
 
@@ -66,10 +62,9 @@ module.exports = function (db, parser, journal, uuid) {
     return db.exec(sql, params);
   }
 
-  function _writeCashItems(primaryCashId, data) {
+  function writeCashItems(primaryCashId, data) {
     var sql, queries, params;
 
-    console.log('[DBG] [TXNS]', data);
     sql =
       'INSERT INTO `primary_cash_item` ' +
         '(inv_po_id, debit, credit, uuid, primary_cash_uuid) ' +
@@ -86,59 +81,13 @@ module.exports = function (db, parser, journal, uuid) {
     return q.all(queries);
   }
 
-  mod.execute = function execute(data, user, callback) {
-    var primaryCashId = uuid();
-
-    console.log('[DEBUG]', data);
-    console.log('[DEBUG]', user);
-
-    writeCash(primaryCashId, user, data.details)
-    .then(writeCashItems(primaryCashId, data.transaction))
-    .then(postToJournal(primaryCashId, user))
-    .then(function (res) {
-      console.log('[DEBUG]', res);
-      res.primaryCashId = primaryCashId;
-      callback(null, res);
-    })
-    .catch(callback);
-  };
-
-  function writeCash(uuid, user, details) {
-    var insertSQL;
-
-    details.uuid = uuid;
-    details.user_id = user;
-
-    insertSQL = parser.insert('primary_cash', details);
-
-    return db.exec(insertSQL);
-  }
-
-
-  function writeCashItems(primaryCashId, transactions) {
-    var insertSQL;
-    var requests_item = [];
-
-    transactions.forEach(function (transaction) {
-      transaction.uuid = uuid();
-      transaction.primaryCashId = primaryCashId;
-      requests_item.push(parser.insert('primary_cash_item', transaction));
-    });
-
-    return requests_item.map(function (item) {
-      return db.exec(item);
-    });
-  }
-
   function postToJournal(primaryCashId, userId) {
-    var deferred = q.defer();
-    journal.request('indirect_purchase', primaryCashId, userId, function (error, result) {
-      if (error) {
-        return deferred.reject(error);
-      }
-      return deferred.resolve(result);
+    var dfd = q.defer();
+    journal.request('indirect_purchase', primaryCashId, userId, function (err, result) {
+      if (err) { return dfd.reject(err); }
+      dfd.resolve(result);
     });
-    return deferred.promise;
+    return dfd.promise;
   }
 
   return mod;
