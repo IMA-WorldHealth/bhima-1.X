dd4fcd77932a1cb0ce920efaaed5e95e5aac6b09
Steven Fountain <StevenFountain@live.co.uk>
2014-08-25 14:38:22 +0100

    Meta templating hack for templating ng-model, functional location select directive

--- client/src/js/directives/locationSelect.js
+++ client/src/js/directives/locationSelect.js
@@ -1,17 +1,24 @@
+
+/**
+ * Ideally directive would supply a template and a link method seperately, this is a work around for 
+ * not being able to template in ng-model, maybe there's a better way. Ironically more optimal than using 
+ * ng-repeat, the number of location properties should never change.
+ */
 angular.module('bhima.directives')
-.directive('locationSelect', ['appstate', 'connect', function (appstate, connect) {
+.directive('locationSelect', ['appstate', 'connect', '$compile',  function (appstate, connect, $compile) {
   /* jshint unused : false */
   return {
     restrict : 'A',
     replace : true,
     transclue : true,
-    template : '<div><div class="form-group" ng-repeat="config in locationSelect.locationConfig"><label for="location-select-{{config.id}}" class="control-label">{{config.label | translate}}</label><div class="pull-right"><span class="glyphicon glyphicon-globe"></span> 0</div><select ng-disabled="locationSelect.session.locationSearch" class="form-bhima" id="location-select-{{config.id}}"></select></div></div>',
-    link : function (scope) {
+    // template : '<div></div>',
+    link : function (scope, element, attrs) {
       
       // TODO replace with correct scope management
       var namespace = scope.locationSelect = {};
       var session = namespace.session = {};
-
+  
+      
       // TODO rename variables etc. with useful names 
       var locationIndex = {};
       var locationConfig = namespace.locationConfig = {
@@ -26,8 +33,7 @@ angular.module('bhima.directives')
           label : 'LOCATION.SECTOR',
           column : 'name',
           id : 'sector'
-        },
-        province : {
+        }, province : {
           dependency : 'country',
           label : 'LOCATION.PROVINCE',
           column : 'name',
@@ -41,13 +47,17 @@ angular.module('bhima.directives')
         }
       };
       var locationStore = namespace.locationStore = {};
-
       appstate.register('project', settup);
 
       function settup(project) {
+        console.log('settup');
         indexLocationDependencies();
         defineLocationRequests();
-
+      
+        var template = generateTemplate('locationConfig');
+        console.log('tmpl', template);
+        element.replaceWith($compile(generateTemplate('locationConfig'))(scope));
+        
         fetchInitialLocation(project.location_id)
         .then(initialiseLocation);
       }
@@ -64,6 +74,7 @@ angular.module('bhima.directives')
           locationStore[key] = {model : {}, value : {}};
           // modelMap.push(locationStore[key].value);
           locationStore[key].value = defaultLocation[formatKeyId(key)];
+
         });
 
         // Initial request, update config with no dependency
@@ -101,6 +112,9 @@ angular.module('bhima.directives')
         // TODO Refactor : fetch and assign data from one function, each method 
         // responsible for only one thing
         connect.req(config.request).then(function (result) {
+
+          
+          console.log('[locationSelect] [fetchLocationData] Got data', result);
           return assignLocationData(key, result);
         });
       }
@@ -109,7 +123,7 @@ angular.module('bhima.directives')
         var currentLocationValue, invalidCurrentLocation, locationsFound;
         var store = locationStore[key];
         var requiresCurrentKey = lookupDependency(key);
-  
+      
         store.model = result;
         currentLocationValue = store.value;
 
@@ -125,7 +139,7 @@ angular.module('bhima.directives')
           }
         }
 
-        console.log('[assignLocationData][' + key + '] currentLocationValue', currentLocationValue);
+        //console.log('[assignLocationData][' + key + '] currentLocationValue', currentLocationValue);
         
         // Call any configuration that requires on this key
         if (requiresCurrentKey) {
@@ -157,9 +171,14 @@ angular.module('bhima.directives')
           config.request = request;
         });
       }
+    
+      function lookupModel(key) { 
+        console.log("lookupModel CALLED");
+        return locationStore[key].model || {};
+      }
 
       // Search will only ever have to hit 4 elements, convenience method
-      function lookupDependency (currentKey) {
+      function lookupDependency(currentKey) {
         
         // return locationConfig[locationIndex[currentKey]];
         return locationIndex[currentKey];
@@ -169,6 +188,33 @@ angular.module('bhima.directives')
         var uuidSuffix = '_uuid';
         return key.concat(uuidSuffix);
       }
+
+      function generateTemplate(configLabel) { 
+        var config = namespace[configLabel]; 
+        var directiveStructure = "<div>{{TEMPLATE_COMPONENTS}}</div>";
+        var compile = "";
+
+        // Such meta templating
+        var componentStructure = '<div class="form-group"><label for="location-select-<%CONFIGID%>" class="control-label">{{<%CONFIGLABEL%> | translate}}</label><div class="pull-right"><span class="glyphicon glyphicon-globe"></span> {{locationStore.<%CONFIGID%>.data.length}}</div><select ng-disabled="locationSelect.session.locationSearch" ng-model="locationRelationship.<%CONFIGID%>.value" ng-options="<%CONFIGID%>.uuid as <%CONFIGID%>.<%CONFIGCOLUMN%> for <%CONFIGID%> in locationStore.<%CONFIGID%>.data | orderBy : \'name\'" ng-change="updateLocation(\'<%CONFIGDEPEND%>\', locationRelationship.<%CONFIGID%>.value)" class="form-bhima" id="location-select-<%CONFIGID%>"></select></div>';
+
+        // var initialTemplate =
+        var templateString = "";
+        var configurationList = Object.keys(config).reverse();
+        configurationList.forEach(function (key) { 
+          var configObject = config[key];
+          var component = componentStructure;
+                    
+          component = component.replace(/<%CONFIGID%>/g, configObject.id);
+          component = component.replace(/<%CONFIGLABEL%>/g, configObject.label);
+          component = component.replace(/<%CONFIGCOLUMN%>/g, configObject.column);
+          component = component.replace(/<%CONFIGDEPEND%>/g, lookupDependency(key));
+          compile = compile.concat(component);
+        });
+
+        return compile; 
+      }
+
+      namespace.lookupModel = lookupModel;
     }
   };
 }]);
