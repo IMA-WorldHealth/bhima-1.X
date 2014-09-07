62f5c8a6f0e24e0f5803c79fd2be9992aeb49cf3
Dedrick Kitamuka <kitamuka@gmail.com>
2014-09-07 11:09:54 +0100

    finishing payroll and taxes structure

---
+++ client/src/partials/taxe/ipr/ipr.js
@@ -0,0 +1,11 @@
+angular.module('bhima.controllers')
+.controller('taxes_management.ipr', [
+  '$scope',
+  'connect',
+  'appstate',
+  'messenger',
+  'validate',
+  '$translate',
+  function ($scope, connect, appstate, messenger, validate, $translate) {
+  }
+]);
