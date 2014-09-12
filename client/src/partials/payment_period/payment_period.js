bd02fe84baccf5eddc12abc71e19389dd39419e7
mbayopanda <mbayopanda@gmail.com>
2014-09-12 11:05:12 -0700

    Gestion CRUD
    Former-commit-id: 0bc6109e15df9a5ba37f6114334c580b666db2f4 [formerly 11c31702e1b3452fb174b5cb0b3a83f959cf09bd]
    Former-commit-id: 7ae337df775172bde3ba30a33bdf5a27bcae673c

--- client/src/partials/payment_period/payment_period.js
+++ client/src/partials/payment_period/payment_period.js
@@ -14,7 +14,7 @@ function($scope, $translate, validate, messenger, connect, appstate, uuid){
 	dependencies.config_rubric = {
 		query : {
 			tables : {
-				config_rubric : { columns : ['label']}
+				config_rubric : { columns : ['id', 'label']}
 			}
 		}
 	};
@@ -22,7 +22,7 @@ function($scope, $translate, validate, messenger, connect, appstate, uuid){
 	dependencies.config_tax = {
 		query : {
 			tables : {
-				config_tax : { columns : ['label']}
+				config_tax : { columns : ['id', 'label']}
 			}
 		}
 	};
@@ -33,7 +33,6 @@ function($scope, $translate, validate, messenger, connect, appstate, uuid){
 
 	function startup (models) {
       angular.extend($scope, models);
-      console.log(models);
     }
 
 	appstate.register('enterprise', function (enterprise) {
@@ -41,6 +40,73 @@ function($scope, $translate, validate, messenger, connect, appstate, uuid){
       validate.process(dependencies)
       .then(startup);
     });
+
+    $scope.delete = function (period) {
+      var result = confirm($translate.instant('PAYMENT_PERIOD.CONFIRM'));
+      if (result) {  
+        connect.basicDelete('paiement_period', period.id, 'id')
+        .then(function () {
+          $scope.paiement_period.remove(period.id);
+          messenger.info($translate.instant('PAYMENT_PERIOD.DELETE_SUCCESS'));
+        });
+      }
+    };
+
+    $scope.edit = function (period) {
+      session.action = 'edit';
+      session.edit = angular.copy(period);
+      session.edit.dateFrom = new Date(session.edit.dateFrom);
+      session.edit.dateTo = new Date(session.edit.dateTo);
+      delete session.edit.RUBRIC;
+      delete session.edit.TAX;
+    };
+
+    $scope.new = function () {
+      session.action = 'new';
+      session.new = {};
+      session.new.dateFrom = new Date();
+      session.new.dateTo = new Date();
+    };
+
+    $scope.save = {};
+
+    $scope.save.edit = function () {
+      var record = connect.clean(session.edit);
+      delete record.reference;
+      connect.basicPost('paiement_period', [record], ['id'])
+      .then(function () {
+        messenger.success($translate.instant('PAYMENT_PERIOD.UPDATE_SUCCES')); 
+        $scope.paiement_period.put(record);
+        session.action = '';
+        session.edit = {};
+
+        validate.refresh(dependencies)
+      	.then(startup);
+
+      });
+    };
+
+    $scope.save.new = function () {
+      var record = connect.clean(session.new);
+      connect.basicPut('paiement_period', [record])
+      .then(function () {
+        messenger.success($translate.instant('PAYMENT_PERIOD.SAVE_SUCCES'));
+        record.reference = generateReference();
+        $scope.paiement_period.post(record);
+        session.action = '';
+        session.new = {};
+
+        validate.refresh(dependencies)
+      	.then(startup);
+
+      });
+    };
+
+    function generateReference () {
+      window.data = $scope.paiement_period.data;
+      var max = Math.max.apply(Math.max, $scope.paiement_period.data.map(function (o) { return o.reference; }));
+      return Number.isNaN(max) ? 1 : max + 1;
+    }
 
 
 }]);
