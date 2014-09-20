0cdd668fe078565de01ad559c15c6292c3f1bc6c
Jonathan Niles <jonathanwniles@gmail.com>
2014-09-20 19:01:55 -0400

    locationSelect migration issues:
    Former-commit-id: de17e1d1eeae394f21ffce37207d2122b85dfd33 [formerly afa000a7158ca88b0c6645dd15f5aec046e720aa]
    Former-commit-id: 723827644205d5dccbe260f8566213473e3f0336

--- client/src/js/directives/locationSelect.js
+++ client/src/js/directives/locationSelect.js
@@ -33,7 +33,8 @@ angular.module('bhima.directives')
           label : 'LOCATION.SECTOR',
           column : 'name',
           id : 'sector'
-        }, province : {
+        },
+        province : {
           dependency : 'country',
           label : 'LOCATION.PROVINCE',
           column : 'name',
@@ -46,9 +47,8 @@ angular.module('bhima.directives')
           id : 'country'
         }
       };
-      var locationStore = namespace.locationStore = {};
 
-      
+      var locationStore = namespace.locationStore = {};
 
       appstate.register('project', settup);
 
@@ -66,19 +66,18 @@ angular.module('bhima.directives')
       }
 
       function fetchInitialLocation(villageUuid) {
-        return connect.fetch('/location/' + villageUuid);
+        return connect.fetch('/location/village/' + villageUuid);
       }
 
-      function initialiseLocation(defaultLocation) {
+      function initialiseLocation(defaultLocation) {  
         defaultLocation = defaultLocation[0];
         
-
         // Populate initial values (enterprise default)
         Object.keys(locationConfig).forEach(function (key) {
           locationStore[key] = {model : {}, value : {}};
-          // modelMap.push(locationStore[key].value);
-          locationStore[key].value = defaultLocation[formatKeyId(key)];
-
+          // new API caused the following change:
+          // locationStore[key].value = defaultLocation[formatKeyId(key)];
+          // locationStore[key].value = defaultLocation[formatKeyId(key)];
         });
 
         // Initial request, update config with no dependency
