2d31cf498101444e925b617a6ab7cf1a0e7e19bc
Steven Fountain <StevenFountain@live.co.uk>
2014-09-29 12:50:35 +0100

    rewrite journal and store to allow create once use anywhere, createPurchase route

--- server/lib/store.js
+++ server/lib/store.js
@@ -1,88 +1,93 @@
+
 // /scripts/lib/util/store.js
 // the data store, similar to Dojo's Memory Store.
 
-module.exports = function Store(options) {
-  'use strict';
+console.log('[store] Configuring store');
 
-  // globals
-  options = options || {};
-  this.index = {};
-  this.data = [];
+'use strict';
 
-  // locals
-  var identifier = options.identifier || 'id'; // id property
+var store = {};
 
-  // set an array of data
-  this.setData = function (data) {
-    var index = this.index = {};
-    this.data = data;
+// globals
+var options = options || {};
+store.index = {};
+store.data = [];
 
-    for (var i = 0, l = data.length; i < l; i += 1) {
-      index[data[i][identifier]] = i;
-    }
-  };
+// locals
+var identifier = options.identifier || 'id'; // id property
 
-  // constructor function
-  var self = this;
-  (function contructor () {
-    for (var k in options) {
-      self[k] = options[k];
-    }
-    // set data if it is defined
-    if (options.data) { self.setData(options.data); }
-  })();
+// set an array of data
+store.setData = function (data) {
+  var index = store.index = {};
+  store.data = data;
 
-  // get an item from the local store
-  this.get = function (id) {
-    return this.data[this.index[id]];
-  };
-
-  // put is for UPDATES
-  this.put = function (object, opts) {
-    var data = this.data,
-        index = this.index,
-        id = object[identifier] = (opts && 'id' in opts) ? opts.id : identifier in object ?  object[identifier] : false;
-
-    // merge or overwrite
-    if (opts && opts.overwrite) {
-      data[index[id]] = object; // overwrite
-    } else {
-      var ref = data[index[id]] || {};
-      for (var k in object) {
-        ref[k] = object[k]; // merge
-      }
-    }
-  };
-
-  // post is for INSERTS
-  this.post = function (object, opts) {
-
-    var data = this.data,
-        index = this.index,
-        id = object[identifier] = (opts && 'id' in opts) ? opts.id : identifier in object ?  object[identifier] : Math.random();
-    index[id] = data.push(object) - 1;
-  };
-
-  this.remove = function (id) {
-    var data = this.data,
-        index = this.index;
-
-    if (id in index) {
-      data.splice(index[id], 1);
-      this.setData(data);
-    }
-  };
-
-  this.contains = function (id) {
-    // check to see if an object with
-    // this id exists in the store.
-    return !!this.get(id);
-  };
-
-  this.recalculateIndex = function () {
-    var data = this.data, index = this.index;
-    for (var i = 0, l = data.length; i < l; i += 1) {
-      index[data[i][identifier]] = i;
-    }
-  };
+  for (var i = 0, l = data.length; i < l; i += 1) {
+    index[data[i][identifier]] = i;
+  }
 };
+
+// constructor function
+var self = store;
+(function contructor () {
+  for (var k in options) {
+    self[k] = options[k];
+  }
+  // set data if it is defined
+  if (options.data) { self.setData(options.data); }
+})();
+
+// get an item from the local store
+store.get = function (id) {
+  return store.data[store.index[id]];
+};
+
+// put is for UPDATES
+store.put = function (object, opts) {
+  var data = store.data,
+      index = store.index,
+      id = object[identifier] = (opts && 'id' in opts) ? opts.id : identifier in object ?  object[identifier] : false;
+
+  // merge or overwrite
+  if (opts && opts.overwrite) {
+    data[index[id]] = object; // overwrite
+  } else {
+    var ref = data[index[id]] || {};
+    for (var k in object) {
+      ref[k] = object[k]; // merge
+    }
+  }
+};
+
+// post is for INSERTS
+store.post = function (object, opts) {
+
+  var data = store.data,
+      index = store.index,
+      id = object[identifier] = (opts && 'id' in opts) ? opts.id : identifier in object ?  object[identifier] : Math.random();
+  index[id] = data.push(object) - 1;
+};
+
+store.remove = function (id) {
+  var data = store.data,
+      index = store.index;
+
+  if (id in index) {
+    data.splice(index[id], 1);
+    store.setData(data);
+  }
+};
+
+store.contains = function (id) {
+  // check to see if an object with
+  // store id exists in the store.
+  return !!store.get(id);
+};
+
+store.recalculateIndex = function () {
+  var data = store.data, index = store.index;
+  for (var i = 0, l = data.length; i < l; i += 1) {
+    index[data[i][identifier]] = i;
+  }
+};
+
+module.exports = store;
