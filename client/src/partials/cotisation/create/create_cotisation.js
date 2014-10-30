2f6e5dd5915d4acb4573dbf14dd0e236473d079c
dedrick <kitamuka@gmail.com>
2014-10-30 17:48:11 +0100

    adding cotisation management
    Former-commit-id: 68e20ae063c3291f7d4826c153b3345a876f74ab [formerly a902360971e48ed7652dc0b5aec6375255d6072a]
    Former-commit-id: 5f5dd7277cd278da639d73477aafd9e04d6257e3

---
+++ client/src/partials/cotisation/create/create_cotisation.js
@@ -0,0 +1,121 @@
+angular.module('bhima.controllers')
+.controller('cotisations_management.create', [
+  '$scope',
+  '$translate',
+  '$http',
+  'validate',
+  'messenger',
+  'connect',
+  'appstate',
+  function ($scope, $translate, $http, validate, messenger, connect, appstate) {
+    var dependencies = {},
+        session = $scope.session = {};
+
+    dependencies.cotisations = {
+      query : {
+        identifier : 'id',
+        tables : {
+          'cotisation' : { columns : ['id', 'label', 'abbr', 'is_employee', 'is_percent', 'four_account_id', 'six_account_id', 'value'] }
+        }
+      }
+    };
+
+    dependencies.accounts = {
+      query : {
+        tables : {
+          'account' : {
+            columns : ['id', 'account_number', 'account_txt']
+          }
+        }
+      }
+    };
+
+    function startup (models) {
+      angular.extend($scope, models);
+    }
+
+    appstate.register('enterprise', function (enterprise) {
+      $scope.enterprise = enterprise;
+      validate.process(dependencies)
+      .then(startup);
+    });
+
+    $scope.delete = function (cotisations) {
+      var result = confirm($translate.instant('COTISATIONS.CONFIRM'));
+      if (result) {
+        connect.basicDelete('cotisation', cotisations.id, 'id')
+        .then(function () {
+          $scope.cotisations.remove(cotisations.id);
+          messenger.info($translate.instant('COTISATIONS.DELETE_SUCCESS'));
+        });
+      }
+    };
+
+    $scope.edit = function (cotisations) {
+      session.action = 'edit';
+      session.edit = angular.copy(cotisations);
+      session.edit.is_employee = session.edit.is_employee !== 0;
+      session.edit.is_percent = session.edit.is_percent !== 0;
+    };
+
+    $scope.new = function () {
+      session.action = 'new';
+      session.new = {};
+    };
+
+    $scope.save = {};
+
+    $scope.save.edit = function () {
+      $scope.session.edit.is_employee = ($scope.session.edit.is_employee)? 1 : 0;
+      $scope.session.edit.is_percent = ($scope.session.edit.is_percent)? 1 : 0;
+
+      var record = angular.copy(connect.clean(session.edit));
+      delete record.reference;
+      delete record.account_number;
+      delete record.account_txt;
+
+      if(record.abbr){
+        if(record.abbr.length <= 4){
+          connect.basicPost('cotisation', [record], ['id'])
+          .then(function () {
+            validate.refresh(dependencies)
+            .then(function (models) {
+              angular.extend($scope, models);
+              messenger.success($translate.instant('COTISATIONS.UPDATE_SUCCES'));
+              session.action = '';
+              session.edit = {};
+            });
+          });
+        } else if (record.abbr.length > 4){
+          messenger.danger($translate.instant('COTISATIONS.NOT_SUP4'));
+        }
+      }  else {
+        messenger.danger($translate.instant('COTISATIONS.NOT_EMPTY'));
+      }
+    };
+
+    $scope.save.new = function () {
+      $scope.session.new.is_employee = ($scope.session.new.is_employee)? 1 : 0;
+      $scope.session.new.is_percent = ($scope.session.new.is_percent)? 1 : 0;
+      var record = connect.clean(session.new);
+      if(record.abbr){
+        if(record.abbr.length <= 4){
+          connect.basicPut('cotisation', [record])
+          .then(function (res) {
+
+            validate.refresh(dependencies)
+            .then(function (models) {
+              angular.extend($scope, models);
+            });
+            session.action = '';
+            session.new = {};
+          });
+        } else if (record.abbr.length > 4){
+          messenger.danger($translate.instant('COTISATIONS.NOT_SUP4'));
+        }
+      }  else {
+        messenger.danger($translate.instant('COTISATIONS.NOT_EMPTY'));
+      }
+    };
+  }
+]);
