1b442bf4dbc0adb430d5eb407b1b8f726df3e741
lomamech <lomamech@gmail.com>
2014-11-14 11:32:27 +0100

    Preuve payroll

--- client/src/partials/reports/primary_cash/expense/expense_report.js
+++ client/src/partials/reports/primary_cash/expense/expense_report.js
@@ -110,6 +110,8 @@ angular.module('bhima.controllers')
         session.model.records.data.forEach(function (transaction) {
           if(transaction.service_txt === 'indirect_purchase'){
             transaction.primary_cash_uuid = transaction.document_uuid;
+          } else if (transaction.service_txt === 'payslip'){
+            transaction.primary_cash_uuid = transaction.document_uuid;
           }
 
           console.log('trans :::', transaction);
