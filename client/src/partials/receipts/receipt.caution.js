bc6a2032cfa71a0960dea2220ac5b6b66349844a
Dedrick Kitamuka <kitamuka@gmail.com>
2014-12-04 14:04:44 +0100

    finishing breaking out transfer and caution
    Former-commit-id: 4bb724a7dcdec04a1f61ce4baf88b54a3ea375e9 [formerly 47ff42158a9480a7205e792a4077febb51a91cf8]
    Former-commit-id: b774d6f22efb422d8c3ecf3b9a7ac5b4d6f6cd13

---
+++ client/src/partials/receipts/receipt.caution.js
@@ -0,0 +1,45 @@
+angular.module('bhima.controllers')
+.controller('receipt.caution', [
+  '$scope',
+  '$routeParams',
+  '$q',
+  '$http',
+  'validate',
+  'exchange',
+  'appstate',
+  'util',
+  'connect',
+  'messenger',
+  function ($scope, $routeParams, $q, $http, validate, exchange, appstate, util, connect, messenger) {
+    var dependencies = {}, model = $scope.model = {common : {}}, utility = $scope.utility = {};
+
+    dependencies.caution = {
+      required: true,
+      query:  {
+        tables: {
+          caution: { columns: ['reference', 'value', 'debitor_uuid', 'project_id', 'currency_id', 'date'] },
+          patient : {columns : ['first_name', 'last_name', 'current_location_id']}
+        },
+        join : ['caution.debitor_uuid=patient.debitor_uuid']
+      }
+    };
+
+    function buildInvoice (res) {
+      model.caution = res.caution.data.pop();
+    }
+
+  	appstate.register('receipts.commonData', function (commonData) {
+  		commonData.then(function (values) {
+        model.common.location = values.location.data.pop();
+        model.common.InvoiceId = values.invoiceId;
+        utility.convert = values.convert;
+        dependencies.caution.query.where = ['caution.uuid=' + values.invoiceId];
+        validate.process(dependencies)
+        .then(buildInvoice)
+        .catch(function (err){
+          messenger.danger('error', err);
+        });
+  		});     
+    });    
+  }
+]);
