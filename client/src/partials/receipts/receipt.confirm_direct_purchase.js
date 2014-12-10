125e30a1967bb3c891cb47ab37a422989102b9e4
mbayopanda <mbayopanda@gmail.com>
2014-12-10 11:47:14 +0100

    receipt for invoice/confirm_direct_purchase

---
+++ client/src/partials/receipts/receipt.confirm_direct_purchase.js
@@ -0,0 +1,45 @@
+angular.module('bhima.controllers')
+.controller('receipt.confirm_direct_purchase', [
+  '$scope',
+  '$routeParams',
+  '$q',
+  '$http',
+  'validate',
+  'exchange',
+  'appstate',
+  'util',
+  'connect',
+  'messenger',
+  function ($scope, $routeParams, $q, $http, validate, exchange, appstate, util, connect, messenger) {
+    var dependencies = {}, model = $scope.model = {common : {}};
+
+    dependencies.confirmDirectPurchase = {
+      query : {
+        identifier : 'uuid',
+        tables : {
+          purchase : { columns : ['uuid', 'reference', 'cost', 'creditor_uuid', 'project_id', 'purchase_date', 'note'] },
+          supplier : { columns : ['name'] }
+        },
+        join : ['purchase.creditor_uuid=supplier.creditor_uuid']
+      }
+    };
+
+    function buildInvoice (res) {
+      model.confirmDirectPurchase = res.confirmDirectPurchase.data.pop();
+    }
+
+  	appstate.register('receipts.commonData', function (commonData) {
+  		commonData.then(function (values) {
+        model.common.location = values.location.data.pop();
+        model.common.InvoiceId = values.invoiceId;
+        model.common.enterprise = values.enterprise.data.pop();
+        dependencies.confirmDirectPurchase.query.where =  ['purchase.uuid=' + values.invoiceId];
+        validate.process(dependencies)
+        .then(buildInvoice)
+        .catch(function (err){
+          messenger.danger('error', err);
+        });
+  		});     
+    });    
+  }
+]);
