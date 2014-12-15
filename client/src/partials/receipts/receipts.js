b7628542b6ea30deae4cb63c61bf001e04d3b9a7
mbayopanda <mbayopanda@gmail.com>
2014-12-15 10:32:05 +0100

    Receipt for invoice/cotisation_paiement

--- client/src/partials/receipts/receipts.js
+++ client/src/partials/receipts/receipts.js
@@ -64,6 +64,12 @@ angular.module('bhima.controllers')
       'consumption' : {
         url : '/partials/receipts/templates/receipt_consumption.html'
       },
+      'tax_payment' : {
+        url : '/partials/receipts/templates/receipt_tax_payment.html'
+      },
+      'cotisation_paiement' : {
+        url : '/partials/receipts/templates/receipt_cotisation_paiement.html'
+      }
       // 'cash' : {
       //   url : '/partials/receipts/templates/cash.html'
       // },
@@ -96,13 +102,7 @@ angular.module('bhima.controllers')
       // },
       // 'payroll' : {
       //   url : '/partials/receipts/templates/payroll.html'
-      // },
-      // 'cotisation_paiement' : {
-      //   url : '/partials/receipts/templates/cotisation_paiement.html'
-      // },
-      'tax_payment' : {
-        url : '/partials/receipts/templates/tax_payment.html'
-      }                        
+      // },                      
     };
 
     function convert (value, currency_id, date) {
