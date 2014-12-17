f65a6d6e4f3dfa4e974af976218286611a351c85
lomamech <lomamech@gmail.com>
2014-12-17 10:29:25 +0100

    Amelioration of calculating the payroll white the addition of a table to store hollydays data to avoid some operation in receipt.payslip
    Former-commit-id: e16d5afe422ed694dcf7a61145b5a6f29e90a90d [formerly 6b2aab26d7e540153d834bc7c78e3936d5965870]
    Former-commit-id: ff1ae07f445408e0ac19950a19b1dd1f75ae0bbd

--- client/src/partials/receipts/receipt.payslip.js
+++ client/src/partials/receipts/receipt.payslip.js
@@ -24,247 +24,150 @@ angular.module('bhima.controllers')
       }).
       success(function(data) {
         getPPConf();
+      function getHollyDayCount(paiement_period_confs) {        
+        dependencies.get_hollydayCount = {
+          query : {
+            tables : {
+              'hollyday_paiement' : {
+                columns : ['hollyday_id', 'hollyday_nbdays', 'hollyday_percentage', 'paiement_uuid']
+              },
+              'hollyday' : {
+                columns : ['id', 'label']
+              }
+            },
+            join : [
+              'hollyday.id=hollyday_paiement.hollyday_id'
+            ],
+            where : [
+              'hollyday_paiement.paiement_uuid=' + invoiceId
+            ]
+          }
+        };
+        validate.process(dependencies, ['get_hollydayCount'])
+        .then(function (model) {
+          $scope.total_day = data[0].working_day;
+          $scope.daly_rate = data[0].basic_salary / $scope.max_day;
 
-        function getHollyDayCount(paiement_period_confs) {
-          var defer = $q.defer();
-          var som = 0;
-          
-          $http.get('/getCheckHollyday/',{params : {
-              'dateFrom' : util.sqlDate(data[0].dateFrom), 
-              'dateTo' : util.sqlDate(data[0].dateTo),
-              'employee_id' : data[0].employee_id,
-              'line' : ''
-            }
-          }).
-          success(function(res) {
-            var hollydays = res;
-            $scope.dataHollydays = hollydays;
-            if(hollydays.length) {
-              var soms = [];
-              hollydays.forEach(function (h) {
-                var nb = 0;
-
-
-                function getValue (ppc) {
-                  //paiement period config === ppc
-                  var date_pweekfrom = new Date(ppc.weekFrom);
-                  var date_pweekto = new Date(ppc.weekTo);
-
-                  var date_hdatefrom = new Date(h.dateFrom);
-                  var date_hdateto = new Date(h.dateTo);
-
-                  var num_pweekfrom = date_pweekfrom.setHours(0,0,0,0);
-                  var num_pweekto = date_pweekto.setHours(0,0,0,0);
-
-                  var num_hdatefrom = date_hdatefrom.setHours(0,0,0,0);            
-                  var num_hdateto = date_hdateto.setHours(0,0,0,0);
-
-                  var minus_right = 0, minus_left = 0;
-
-                  if(num_pweekto > num_hdateto){
-                    //minus_right = date_pweekto.getDate() - date_hdateto.getDate();
-                    minus_right = num_pweekto - num_hdateto;
-                    minus_right /= (24*3600*1000);
-                    minus_right = parseInt(minus_right);
-                  }
-
-                  if(num_pweekfrom < num_hdatefrom){
-                    //minus_left = date_hdatefrom.getDate() - date_pweekfrom.getDate();
-                    minus_left = num_hdatefrom - num_pweekfrom;
-                    minus_left /= (24*3600*1000);
-                    minus_left = parseInt(minus_left);                    
-                  }
-
-                  var nbOffDaysPos = 0; 
-                  for(var i = 0; i < $scope.OffDaysData.length; i++){
-                    var dateOff = new Date($scope.OffDaysData[i].date);
-                    var num_dateOff = dateOff.setHours(0,0,0,0);
-                    if(((num_dateOff >= num_hdatefrom) && (num_dateOff <= num_hdateto)) && 
-                      ((num_dateOff >= num_pweekfrom) && (num_dateOff <= num_pweekto))){
-                      nbOffDaysPos++;
-                    }
-                  }                  
-
-                  var t2 = date_pweekto.getTime(); 
-                  var t1 = date_pweekfrom.getTime();
-                  var total = (parseInt((t2-t1)/(24*3600*1000))) + 1 - nbOffDaysPos;
-                  if(minus_left > total) { return 0; }
-                  if(minus_right > total) { return 0; } 
-                  return total - (minus_left + minus_right);
-                }
-
-                paiement_period_confs.forEach(function (ppc) {
-                  nb += getValue(ppc);
+          $scope.amont_payable = $scope.daly_rate * $scope.total_day; 
+          $scope.TotalPaid += $scope.amont_payable;
+          $scope.TotalNet += $scope.amont_payable;
                   
-                });
-                h.nbJour = nb; 
+          var dataHollydays = $scope.dataHollydays = model.get_hollydayCount.data,
+            cotisationValue = model.get_cotisation.data,
+            dailyRate = data[0].basic_salary / $scope.max_day;
 
-                var dailyRate = data[0].basic_salary / $scope.max_day;
-                h.dailyHollyd = dailyRate * (h.percentage /100); 
-                h.somHolly = h.dailyHollyd * h.nbJour;
-                $scope.TotalPaid += h.somHolly;
-                $scope.TotalNet += h.somHolly;
-                
-                soms.push(nb);
-              });
+          dataHollydays.forEach(function (item) {
+            item.dailyHollyd = dailyRate * (item.hollyday_percentage /100); 
+            item.somHolly = item.dailyHollyd * item.hollyday_nbdays;
 
-              som = soms.reduce(function (x, y){
-                return x+y;
-              }, 0);
+            $scope.TotalPaid += item.somHolly;
+            $scope.TotalNet += item.somHolly;
+          });       
+        });
+      }  
 
-               
-              //$scope.total_day = data[0].working_day + som;
-              $scope.total_day = data[0].working_day;
-
-              if($scope.max_day > 0){
-
-                data[0].basic_salary = exchange(
-                                data[0].basic_salary,
-                                data[0].currency_id,
-                                util.sqlDate(new Date())
-                              );
-                $scope.daly_rate = data[0].basic_salary / $scope.max_day;
-                
-
-                $scope.amont_payable = $scope.daly_rate * $scope.total_day; 
-                $scope.TotalPaid += $scope.amont_payable;
-                $scope.TotalNet += $scope.amont_payable;
-              } else {
-                $scope.daly_rate = 0;
-                $scope.amont_payable = 0; 
+      function getOffDayCount() {          
+        dependencies.offDays = {
+          query : {
+            tables : {
+              'offday' : {
+                columns : ['id', 'label', 'date', 'percent_pay']
               }
+            },
+            where : ['offday.date>=' + util.sqlDate(data[0].dateFrom), 'AND', 'offday.date<=' + util.sqlDate(data[0].dateTo)]
+          }
+        };  
 
-              defer.resolve(som); 
-            }else{
-              $scope.total_day = data[0].working_day;
+        validate.process(dependencies, ['offDays'])
+        .then(function (model) {
+          $scope.nbOffDays = model.offDays.data.length;
+          $scope.OffDaysData = model.offDays.data;
+        });
+      }
 
-              if($scope.max_day > 0){
-
-                data[0].basic_salary = exchange(
-                                data[0].basic_salary,
-                                data[0].currency_id,
-                                util.sqlDate(new Date())
-                              );
-
-                $scope.daly_rate = data[0].basic_salary / $scope.max_day;
-                $scope.amont_payable = $scope.daly_rate * $scope.total_day; 
-                $scope.TotalPaid += $scope.amont_payable;
-                $scope.TotalNet += $scope.amont_payable;
-              } else {
-                $scope.daly_rate = 0;
-                $scope.amont_payable = 0; 
+      function getOffDay() {        
+        dependencies.offDays = {
+          query : {
+            tables : {
+              'offday' : {
+                columns : ['id', 'label', 'date', 'percent_pay']
               }
-              defer.resolve(0);
-            }               
-          });
-          return defer.promise;
-        }        
+            },
+            where : ['offday.date>=' + util.sqlDate(data[0].dateFrom), 'AND', 'offday.date<=' + util.sqlDate(data[0].dateTo)]
+          }
+        };  
 
-        function getOffDayCount() {          
-          dependencies.offDays = {
-            query : {
-              tables : {
-                'offday' : {
-                  columns : ['id', 'label', 'date', 'percent_pay']
-                }
-              },
-              where : ['offday.date>=' + util.sqlDate(data[0].dateFrom), 'AND', 'offday.date<=' + util.sqlDate(data[0].dateTo)]
-            }
-          };  
+        validate.process(dependencies, ['offDays'])
+        .then(function (model) {
+          $scope.dataOffDays = model.offDays;
+          for(var i = 0; i < model.offDays.data.length; i++){
 
-          validate.process(dependencies, ['offDays'])
-          .then(function (model) {
-            $scope.nbOffDays = model.offDays.data.length;
-            $scope.OffDaysData = model.offDays.data;
-          });
-        }
+            model.offDays.data[i].rate_offDay = (model.offDays.data[i].percent_pay) * ($scope.daly_rate / 100);
+            $scope.TotalPaid += model.offDays.data[i].rate_offDay;
+            $scope.TotalNet += model.offDays.data[i].rate_offDay;
+          }
 
+        });
+      }
 
+      function getPPConf() {  
+        dependencies.paiement_period_conf = {
+          required : true,
+          query : {
+            tables : {
+              'config_paiement_period' : {
+                columns : ['id', 'weekFrom', 'weekTo']
+              }
+            },
+            where : ['config_paiement_period.paiement_period_id=' + data[0].paiement_period_id]
+          }
+        };
 
-        function getOffDay() {        
-          
-          dependencies.offDays = {
-            query : {
-              tables : {
-                'offday' : {
-                  columns : ['id', 'label', 'date', 'percent_pay']
-                }
-              },
-              where : ['offday.date>=' + util.sqlDate(data[0].dateFrom), 'AND', 'offday.date<=' + util.sqlDate(data[0].dateTo)]
-            }
-          };  
+        validate.process(dependencies, ['paiement_period_conf'])
+        .then(function (model) {
+          var paiement_period_confs = model.paiement_period_conf.data;
+          $scope.max_day = getMaxDays(paiement_period_confs);
+          getOffDayCount(); 
+          getHollyDayCount(paiement_period_confs);
+          getOffDay(); 
+        });
+      } 
 
-          validate.process(dependencies, ['offDays'])
-          .then(function (model) {
-            $scope.dataOffDays = model.offDays;
-            for(var i = 0; i < model.offDays.data.length; i++){
+      function getMaxDays (ppcs) {
+        var nb = 0;
+        ppcs.forEach(function (item) {
+          var t2 = new Date(item.weekTo).getTime();
+          var t1 = new Date(item.weekFrom).getTime();
 
-              model.offDays.data[i].rate_offDay = (model.offDays.data[i].percent_pay) * ($scope.daly_rate / 100);
-              $scope.TotalPaid += model.offDays.data[i].rate_offDay;
-              $scope.TotalNet += model.offDays.data[i].rate_offDay;
-            }
+          nb += (parseInt((t2-t1)/(24*3600*1000))) + 1;
+        });
+        return nb;
+      }         
+      $scope.dataPaiements = data;
+    });
 
-          });
-        }
-
-        function getPPConf() {  
-          dependencies.paiement_period_conf = {
-            required : true,
-            query : {
-              tables : {
-                'config_paiement_period' : {
-                  columns : ['id', 'weekFrom', 'weekTo']
-                }
-              },
-              where : ['config_paiement_period.paiement_period_id=' + data[0].paiement_period_id]
-            }
-          };
-
-          validate.process(dependencies, ['paiement_period_conf'])
-          .then(function (model) {
-            var paiement_period_confs = model.paiement_period_conf.data;
-            $scope.max_day = getMaxDays(paiement_period_confs);
-            getOffDayCount(); 
-            getHollyDayCount(paiement_period_confs);
-            getOffDay(); 
-          });
+    $http.get('/getDataRubrics/',{params : {
+          'invoiceId' : invoiceId
+        }  
+    }).
+    success(function(data) {
+      $scope.dataRubrics = data;
+      data.forEach(function (item) {
+        if(item.is_discount === 0){
+          $scope.TotalPaid += item.value;
+          item.valueP = item.value;
+          item.valueR = 0;
+          $scope.TotalNet += item.value;
+        } else if(item.is_discount === 1){
+          $scope.TotalWithheld += item.value;
+          item.valueP = 0;
+          item.valueR = item.value;
+          $scope.TotalNet -= item.value;
         } 
-
-        function getMaxDays (ppcs) {
-          var nb = 0;
-          ppcs.forEach(function (item) {
-            var t2 = new Date(item.weekTo).getTime();
-            var t1 = new Date(item.weekFrom).getTime();
-
-            nb += (parseInt((t2-t1)/(24*3600*1000))) + 1;
-          });
-          return nb;
-        }         
-        $scope.dataPaiements = data;
-      });
-
-      $http.get('/getDataRubrics/',{params : {
-            'invoiceId' : invoiceId
-          }  
-      }).
-      success(function(data) {
-        $scope.dataRubrics = data;
-        data.forEach(function (item) {
-          if(item.is_discount === 0){
-            $scope.TotalPaid += item.value;
-            item.valueP = item.value;
-            item.valueR = 0;
-            $scope.TotalNet += item.value;
-          } else if(item.is_discount === 1){
-            $scope.TotalWithheld += item.value;
-            item.valueP = 0;
-            item.valueR = item.value;
-            $scope.TotalNet -= item.value;
-          } 
-          
-        });  
         
-      });
+      });  
+      
+    });
 
       $http.get('/getDataTaxes/',{params : {
             'invoiceId' : invoiceId
@@ -298,7 +201,6 @@ angular.module('bhima.controllers')
       };
       validate.process(dependencies, ['get_cotisation'])
       .then(function (model) {
-        console.log('Les donnees',model.get_cotisation.data);
         $scope.dataCotisation = model.get_cotisation.data;
         var cotisationValue = model.get_cotisation.data;
         cotisationValue.forEach(function (item) {
@@ -306,7 +208,6 @@ angular.module('bhima.controllers')
           $scope.TotalNet -= item.value;
         });
       });
-
     } 
 
     function promiseInvoiceId (invoiceId) {
