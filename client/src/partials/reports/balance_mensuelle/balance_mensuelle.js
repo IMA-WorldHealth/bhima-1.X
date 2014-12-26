9fd88835eee5784b121a46df32a80da9e23d8637
mbayopanda <mbayopanda@gmail.com>
2014-12-26 09:55:23 +0100

    Rapport Balance Mensuelle
    Former-commit-id: dba416fa2bc02e9364af6f85d185fad64aed0341 [formerly bfd7e6c5b564796e9e3046e6eaffc5dc20891424]
    Former-commit-id: 0142f48d783920c733855f959404fbda83e4273d

---
+++ client/src/partials/reports/balance_mensuelle/balance_mensuelle.js
@@ -0,0 +1,75 @@
+angular.module('bhima.controllers')
+.controller('report.balance_mensuelle', [
+  '$scope',
+  '$translate',
+  '$window',
+  'validate',
+  'appstate',
+  'util',
+  'messenger',
+  function ($scope, $translate, $window, validate, appstate, util, messenger) {
+    var dependencies = {}, session = $scope.session = {};
+
+    session.classes = [
+      { number : 1, name : 'Fonds propres'},
+      { number : 2, name : 'Valeurs immob. & Eng.'},
+      { number : 3, name : 'Valeurs d\'exploitation'},
+      { number : 4, name : 'Fournisseurs, Tiers & Regular.'},
+      { number : 5, name : 'Comptes Financiers'},
+      { number : 6, name : 'Charges et Pertes'},
+      { number : 7, name : 'Produits et Profits'}
+    ];
+
+    dependencies.currencies = {
+      query : {
+        tables : {
+          'currency' : { columns : ['id', 'name', 'symbol'] }
+        }
+      }
+    };
+
+    appstate.register('project', function (project) {
+      session.project = project.id;
+    });
+
+    function startup(model) {
+      angular.extend($scope, model);
+    }
+
+    function getAccountBalance() {
+      if(session.classe && session.periode){
+        var params = {
+          project : session.project,
+          classe  : session.classe.number,
+          periode : util.sqlDate(session.periode)
+        };
+
+        dependencies.balance_mensuelle = { query : 'reports/balance_mensuelle/?' + JSON.stringify(params) };
+        validate.process(dependencies, ['balance_mensuelle'])
+        .then(startup)
+        .then(calculTotaux);
+      }else {
+        messenger.info($translate.instant('BALANCE_MENSUELLE.INFO_FILL_INPUT'), true);
+      }
+    }
+
+    function formatAccount(classe) {
+      return '[' + classe.number + ']' + classe.name;
+    }
+
+    function print() { $window.print(); }
+
+    function calculTotaux() {
+      session.sum_old_debit = $scope.balance_mensuelle.data.reduce(function sum(a, b) { return a + b.old_debit; },0);
+      session.sum_old_credit = $scope.balance_mensuelle.data.reduce(function sum(a, b) { return a + b.old_credit; },0);
+      session.sum_debit = $scope.balance_mensuelle.data.reduce(function sum(a, b) { return a + b.debit; },0);
+      session.sum_credit = $scope.balance_mensuelle.data.reduce(function sum(a, b) { return a + b.credit; },0);
+    }
+    
+
+    // Exports
+    $scope.getAccountBalance = getAccountBalance;
+    $scope.formatAccount = formatAccount;
+    $scope.print = print;
+  }
+]);
