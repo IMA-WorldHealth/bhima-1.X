78d63783762d7e8ed38d461ff280014b6d80fa7e
Steven Fountain <StevenFountain@live.co.uk>
2014-12-29 09:54:11 +0100

    Seperate data request and PDF rendering

--- server/controllers/reports_proposed/reports.js
+++ server/controllers/reports_proposed/reports.js
@@ -2,37 +2,33 @@ var path        = require('path');
 var fs          = require('fs');
 var q           = require('q');
 
+// Import and compile template files
 var dots        = require('dot').process({path : path.join(__dirname, 'templates')});
+
 var wkhtmltopdf = require('wkhtmltopdf');
 var numeral     = require('numeral');
-
 var uuid        = require('./../../lib/guid');
-var db          = require('./../../lib/db');
-
 var config      = require('./config');
 
+// Model compilation controllers 
+var invoice     = require('./data/invoice');
+
+// Module configuration 
 var writePath = path.join(__dirname, 'out/');
 
-// HTTP Controllers 
-
-// Serve a generated PDF - allowing cleanup of files following
 exports.serve = function (req, res, next) {
   var target = req.params.target;
   var options = {root : writePath};
   
-  console.log('serving a file');
-
   res.sendFile(target.concat('.pdf'), options, function (err) { 
     if (err) { 
       console.log(err);
       res.status(err.status).end();
     } else { 
-      console.log('Sent :', target);
 
-      // Delete file now
+      // Delete (unlink) served file
       fs.unlink(path.join(__dirname, 'out/').concat(target, '.pdf'), function (err) { 
         if (err) throw err;
-        console.log('Removed and cleaned up report');
       });
     }
   });
@@ -40,93 +36,32 @@ exports.serve = function (req, res, next) {
 
 exports.build = function (req, res, next) {  
   
-
-  collectInvoiceData()
-  .then(compileReport)
+  //var report = map[request]; report.buildContext()
+  invoice.buildContext()
+  .then(renderPDF)
   .catch(function (err) { 
-    console.log('top level', err);
+    console.log(err);
+    res.status(500).end();
   });
 
      
-  function compileReport(reportData) { 
-    var compiledReport = dots.invoice(reportData);
-    
+  function renderPDF(reportData) { 
+    var compiledReport;
     var hash = uuid();
     var configuration = buildConfiguration(hash, req.params.size); 
-     
+    
+    // Ensure templates have path data
+    reportData.path = reportData.path || __dirname;
+    compiledReport = dots.invoice(reportData);
+    
+    // TODO Handle exceptions
     var pdf = wkhtmltopdf(compiledReport, configuration, function (code, signal) { 
       res.send('<a target="_blank" href="/report/serve/' + hash + '">Generated PDF</a');
     });
   }
 };
 
-function collectInvoiceData() { 
-  var deferred = q.defer();
-  var reportData = {};
-  
-  console.log('[collectInvoiceData]');
-
-  // TODO This should be provided by the session (req)
-  var enterpriseId = 200;
-  
-  // TODO This should be provided in the request
-  var saleId = '1e012f69-c615-4df8-a85c-878099b857c1';
-
-  reportData.path = __dirname;
-
-  // Invoice receipt to test break point
-  var saleQuery = 'SELECT * FROM sale_item LEFT JOIN sale ON sale_item.sale_uuid = sale.uuid LEFT JOIN inventory ON sale_item.inventory_uuid = inventory.uuid LEFT JOIN project ON sale.project_id = project.id WHERE sale.uuid = ? ORDER BY inventory.code';
-  
-  var enterpriseQuery = 'SELECT * FROM enterprise WHERE id = ?';
-
-  var recipientQuery = 'SELECT * FROM debitor JOIN patient ON debitor.uuid = patient.debitor_uuid WHERE debitor.uuid = ?'; 
-
-  // Query for sale information 
-  db.exec(saleQuery, [saleId])
-    .then(function (result) { 
-      reportData.invoice = {items : result};
-      reportData.invoice.totalCost = sumCosts(result);
-      formatCurrency(reportData.invoice.items);
-
-      console.log(result);
-      
-      // Query for enterprise information 
-      return db.exec(enterpriseQuery, [enterpriseId]);
-    })
-    .then(function (result) { 
-      var initialLineItem = reportData.invoice.items[0];
-      reportData.enterprise = result[0];
-      
-      reportData.invoice.reference = initialLineItem.reference; 
-      reportData.invoice.id = initialLineItem.abbr.concat(initialLineItem.reference);
-      reportData.invoice.date = initialLineItem.invoice_date;
-
-      // Query for recipient information 
-      return db.exec(recipientQuery, [initialLineItem.debitor_uuid]);
-    })
-    .then(function (result) { 
-      reportData.recipient = result[0];
-      
-      deferred.resolve(reportData); 
-    })
-    .catch(function (err) { 
-      deferred.reject(err);
-    });
-  
-  function sumCosts(lineItems) { 
-    return lineItems.reduce(function (a, b) { return a + b.credit - b.debit; }, 0); 
-  }
-
-  function formatCurrency(lineItems) { 
-    lineItems.forEach(function (lineItem) { 
-      lineItem.formattedPrice = numeral(lineItem.transaction_price).format('$0,0.00');
-      lineItem.formattedTotal = numeral(lineItem.credit - lineItem.debit).format('$0,0.00');
-    });
-  }
-
-  return deferred.promise;
-}
-
+// Utility methods
 // Return configuration object for wkhtmltopdf process
 function buildConfiguration(hash, size) { 
   var context = config[size] || config.standard;
@@ -139,5 +74,4 @@ function buildConfiguration(hash, size) {
 function initialise() { 
   
   // Ensure write folder exists - wkhtmltopdf will silently fail without this
-  
 }
