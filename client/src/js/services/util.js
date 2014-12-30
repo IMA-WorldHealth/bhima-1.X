295583f59f82978d65f4511b0ac84816e515a4be
Jonathan Cameron <jmcameron@gmail.com>
2014-12-30 11:23:29 +0100

    Fixes to normalizeName to handle undefined values.
    Former-commit-id: 9b203bd6e9b6451b8506ceececb8851ca0673525 [formerly 7a9988176997a5e96add0701b4cf84ce9c6e3423]
    Former-commit-id: 7f8eb5c3bdf3927010f8cd1ea27a0d765dc776e5

--- client/src/js/services/util.js
+++ client/src/js/services/util.js
@@ -27,34 +27,39 @@ angular.module('bhima.services')
     return annee + '-' + mois + '-' + jour;
   };
 
-  this.isDateAfter = function(date1, date2){
+  this.sqlDate = this.convertToMysqlDate;
+
+  this.isDateAfter = function (date1, date2) {
     date1 = new Date(date1).setHours(0,0,0,0);
     date2 = new Date(date2).setHours(0,0,0,0);
     return date1 > date2;
   };
 
-  this.areDatesEqual = function(date1, date2) {
+  this.areDatesEqual = function (date1, date2) {
     date1 = new Date(date1).setHours(0,0,0,0);
     date2 = new Date(date2).setHours(0,0,0,0);
     return date1 === date2;
   };
 
-  this.isDateBetween = function (date, dateFrom, dateTo){
+  this.isDateBetween = function (date, dateFrom, dateTo) {
     date = new Date(date).setHours(0,0,0,0);
     dateFrom = new Date(dateFrom).setHours(0,0,0,0);
     dateTo = new Date(dateTo).setHours(0,0,0,0);
     return ((date>=dateFrom) && (date<=dateTo));
   };
 
-  this.sqlDate = this.convertToMysqlDate;
-
   // Normalize a name:
-  //  - remove all extra whitespace
-  //  - capitalize the first letter of each word in the name (lowercase the rest of each word)
+  //  * remove all extra whitespace
+  //  * capitalize the first letter of each word in the name 
+  //    (lowercase the rest of each word)
+  //  * Undefined names are not changed (for form fields)
   this.normalizeName = function (name) {
+    if (typeof name === 'undefined') {
+      return name;
+      }
     var names = name.trim().split(/\s+/);
     for(var i = 0; i < names.length; i++) {
-	names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase();
+      names[i] = names[i].charAt(0).toUpperCase() + names[i].slice(1).toLowerCase();
     }
     return names.join(' ');
   };
