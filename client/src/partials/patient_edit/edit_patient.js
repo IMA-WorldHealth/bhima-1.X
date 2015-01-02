91a4a7f855056d5e602fc55b32ebd1c71658a2cc
Jonathan Cameron <jmcameron@gmail.com>
2015-01-02 10:12:23 +0100

    Work in progress

--- client/src/partials/patient_edit/edit_patient.js
+++ client/src/partials/patient_edit/edit_patient.js
@@ -14,28 +14,60 @@ angular.module('bhima.controllers')
 	// Set up the query for the patient data
 	dependencies.patient = {
 	    query : {
-		// identifier : 'id',
 		tables : {
 		    'patient' : {
-			columns : ['uuid', 'first_name', 'last_name']
+			columns : ['uuid', 'project_id', 'reference', 'debitor_uuid', 'creditor_uuid',
+				   'first_name', 'last_name', 'dob', 'sex',
+				   'father_name', 'mother_name',
+				   'profession', 'employer',
+				   'marital_status', 'spouse', 'spouse_profession', 'spouse_employer',
+				   'religion',
+				   'phone', 'email', 'addr_1','addr_2',
+				   'renewal',
+				   'origin_location_id',
+				   'current_location_id',
+				   'registration_date']
+		    },
+		    'debitor' : { 
+			columns : ['group_uuid::debitor_group_id', 'text::debitor_name']
+		    },
+		    'debitor_group' : { 
+			columns : ['name::debitor_group_name']
 		    }
-		}
+		},
+		join : [ 'patient.debitor_uuid=debitor.uuid',
+			 'debitor.group_uuid=debitor_group.uuid'
+		       ],
+		where : [ 'patient.uuid=' +  patient_uuid ]
 	    }
 	};
 
+	// Location methods
+	function setOriginLocation(uuid) {
+	    session.originLocationUuid = uuid;
+	}
+	$scope.setOriginLocation = setOriginLocation;
+
+	function setCurrentLocation(uuid) {
+	    session.currentLocationUuid = uuid;
+	}
+	$scope.setCurrentLocation = setCurrentLocation;
+
 	function startup (models) {
 	    $scope.patient = models.patient.data[0];
+
+	    // Extract the yob for convenience (???)
+	    // var dob = new Date($scope.patient.dob);
+	    // $scope.patient.yob = dob.getFullYear();
+
+	    console.log(models);
 	}
 
 	appstate.register('enterprise', function (enterprise) {
 	    $scope.enterprise = enterprise;
 	    if (patient_uuid) {
-		dependencies.patient.query.where = [ 'patient.uuid=' +  patient_uuid ];
 		validate.process(dependencies)
-		    .then(startup);
-	    }
-	    else {
-		$scope.patient = {};
+		.then(startup);
 	    }
 	});
 
