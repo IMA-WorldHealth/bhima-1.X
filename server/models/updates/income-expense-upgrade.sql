54341055bb225540ed9e01c2fc606e85ffdd9a8a
Jonathan Niles <jonathanwniles@gmail.com>
2015-01-07 15:39:34 +0100

    income/expense upgrade script now removes improperly annotated income accounts
    Former-commit-id: 4b4e2445b802dac3274811b2f0fdfecba6f1d724 [formerly bf221befdd7e220da19db3d6a81ae7e54d47a2ab]
    Former-commit-id: a2323aeb4daffdd22e283f2ddc343474ed02e6e5

--- server/models/updates/income-expense-upgrade.sql
+++ server/models/updates/income-expense-upgrade.sql
@@ -3,6 +3,9 @@
 
 use bhima;
 
+-- remove poorly labeled income accounts
+update account as a JOIN account as b on a.id = b.id set a.account_type_id = 2 where b.account_type_id = 1;
+
 -- set up income accounts as OHADA
 update account as a JOIN account as b on a.id = b.id set a.account_type_id = 1 where b.account_type_id != 3 and b.account_number like '6%';
 -- set up expense accounts as OHADA
