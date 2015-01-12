57432f6ff1f11b1fa306b538c6bfaa1a5f833b97
Jonathan Niles <jonathanwniles@gmail.com>
2015-01-12 12:26:12 +0100

    corrected names in models/updates

--- utilities/migrate/sql/upgrade-v1.sql
+++ utilities/migrate/sql/upgrade-v1.sql
@@ -56,6 +56,7 @@ update account as a JOIN account as b on a.id = b.id set a.account_type_id = 2 w
 
 -- set up income accounts as OHADA
 update account as a JOIN account as b on a.id = b.id set a.account_type_id = 1 where b.account_type_id != 3 and b.account_number like '6%';
+
 -- set up expense accounts as OHADA
 update account as a JOIN account as b on a.id = b.id set a.account_type_id = 4 where b.account_type_id != 3 and b.account_number like '7%';
 -- Updates to fiscal year data
