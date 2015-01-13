255b9d0944e76f9bb657852d53ad17c6f16a1e95
Jonathan Cameron <jmcameron@gmail.com>
2015-01-13 14:59:39 +0100

    Added SQL to add the new menu item
    Former-commit-id: 91e03211e666604fb968ff23adb377aab7f4b264 [formerly 9b8c5d696463995490f0c37ddfcf06be591577f3]
    Former-commit-id: b1e14e1d88a52e1aefdc575e53049413f8b02957

---
+++ server/models/updates/2015-01-13-Add-budget-edit-menu.sql
@@ -0,0 +1,11 @@
+-- Update to units/menu
+--
+-- Add add menu/unit item for the budget menu item.
+--
+-- Date: 2015-01-13
+-- By: Jonathan Cameron
+
+USE bhima;
+
+INSERT INTO `unit` VALUES
+(7,'Edit Account Budget','TREE.EDIT_BUDGET','',8,0,'/partials/budget/edit/','/budget/edit');
