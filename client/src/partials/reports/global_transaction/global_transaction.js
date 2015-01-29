aea71a04e2696f0321bb65dfdf22b15c9c8256b7
mbayopanda <mbayopanda@gmail.com>
2015-01-29 11:05:26 +0100

    Report config for global transactions

--- client/src/partials/reports/global_transaction/global_transaction.js
+++ client/src/partials/reports/global_transaction/global_transaction.js
@@ -9,11 +9,10 @@ angular.module('bhima.controllers')
   'exchange',
   function ($scope, connect, appstate, $translate, validate, util, exchange) {
 
-    var dependencies = {}, map = {};
-    $scope.model = {};
-    $scope.model.sources = [$translate.instant('SELECT.ALL'), $translate.instant('SELECT.POSTING_JOURNAL'), $translate.instant('SELECT.GENERAL_LEDGER')];
-    $scope.somDebit = 0;
-    $scope.somCredit = 0;
+    var session = $scope.session = {},
+        state = $scope.state = {},
+        dependencies = {}, 
+        map = {};
 
     dependencies.accounts = {
       required : true,
@@ -42,9 +41,11 @@ angular.module('bhima.controllers')
       }
     };
 
-    $scope.dates = {};
-    $scope.state = {};
     $scope.account = {};
+    $scope.model = {};
+    $scope.model.sources = [$translate.instant('SELECT.ALL'), $translate.instant('SELECT.POSTING_JOURNAL'), $translate.instant('SELECT.GENERAL_LEDGER')];
+    session.somDebit = 0;
+    session.somCredit = 0;
 
     function formatAccount(account) {
       return [
@@ -53,7 +54,10 @@ angular.module('bhima.controllers')
     }
 
     function init(model) {
+      session.dateFrom = new Date();
+      session.dateTo = new Date();
       angular.extend($scope, model);
+
       $scope.accounts.data.forEach(function (account) {
         account.account_number = String(account.account_number);
       });
@@ -76,8 +80,8 @@ angular.module('bhima.controllers')
       var url = '/reports/allTransactions/?source=' + $scope.model.source_id +
         '&enterprise_id=' + $scope.enterprise.id + 
         '&account_id=' + $scope.model.account_id + 
-        '&datef=' + util.sqlDate($scope.dates.from) + 
-        '&datet=' + util.sqlDate($scope.dates.to);
+        '&datef=' + util.sqlDate(session.dateFrom) + 
+        '&datet=' + util.sqlDate(session.dateTo);
 
       $scope.model.account_number = $scope.accounts.get($scope.model.account_id).account_number;
       connect.fetch(url)
@@ -98,8 +102,8 @@ angular.module('bhima.controllers')
       var url = '/reports/allTransactions/?source=' + $scope.model.source_id +
         '&enterprise_id=' + $scope.enterprise.id + 
         '&account_id=0' + 
-        '&datef=' + util.sqlDate($scope.state.from) + 
-        '&datet=' + util.sqlDate($scope.state.to);
+        '&datef=' + util.sqlDate(session.dateFrom) + 
+        '&datet=' + util.sqlDate(session.dateTo);
 
       connect.fetch(url)
       .then(function (res) {
@@ -112,11 +116,6 @@ angular.module('bhima.controllers')
           $scope.records = res;
           getTotal(res);
         });
-    }
-
-    function dateWatcher () {
-      $scope.state.from = util.sqlDate($scope.dates.from);
-      $scope.state.to = util.sqlDate($scope.dates.to);
     }
 
     function getValue (obj, val, cVal) {
@@ -137,8 +136,8 @@ angular.module('bhima.controllers')
       var url = '/reports/allTransactions/?source=' + $scope.model.source_id +
         '&enterprise_id=' + $scope.enterprise.id + 
         '&account_id=' + $scope.model.account_id + 
-        '&datef=' + util.sqlDate($scope.dates.from) + 
-        '&datet=' + util.sqlDate($scope.dates.to);
+        '&datef=' + util.sqlDate(session.dateFrom) + 
+        '&datet=' + util.sqlDate(session.dateTo);
 
       connect.fetch(url)
       .then(function (res) {
@@ -155,9 +154,11 @@ angular.module('bhima.controllers')
 
     function getTotal(items) {
       var sCredit = 0, 
-        sDebit = 0;
-      $scope.somCredit = 0;
-      $scope.somDebit = 0;
+          sDebit = 0;
+
+      session.somCredit = 0;
+      session.somDebit = 0;
+
       items.forEach(function (item) {
         if($scope.enterprise.currency_id !== item.currency_id){
           sCredit += item.credit / exchange.rate(item.credit,item.currency_id,new Date()); 
@@ -167,30 +168,41 @@ angular.module('bhima.controllers')
           sDebit += item.debit;
         }                
       });
+
       if($scope.enterprise.currency_id === $scope.model.c){
-        $scope.somCredit = sCredit;
-        $scope.somDebit = sDebit;
+        session.somCredit = sCredit;
+        session.somDebit = sDebit;
       } else {
-        $scope.somCredit = sCredit * exchange.rate(sCredit,$scope.model.c,new Date());
-        $scope.somDebit = sDebit * exchange.rate(sDebit,$scope.model.c,new Date());
+        session.somCredit = sCredit * exchange.rate(sCredit,$scope.model.c,new Date());
+        session.somDebit = sDebit * exchange.rate(sDebit,$scope.model.c,new Date());
       }
 
     }
 
     appstate.register('enterprise', function (enterprise) {
       $scope.enterprise = enterprise;
-      $scope.dates.from = new Date();
-      $scope.dates.to = new Date();
       dependencies.accounts.query.where = ['account.enterprise_id='+enterprise.id];
       validate.process(dependencies)
       .then(init);
     });
 
-    $scope.$watch('dates', dateWatcher, true);
-    $scope.$watch('model.account_id', fill);
-    $scope.$watch('model.c', fill);
+    function generate() {
+      fill();
+      $scope.state = 'generate';
+    }
+
+    function reconfigure() {
+      $scope.state = null;
+    }
+
+    function printReport() {
+      print();
+    }
 
     $scope.formatAccount = formatAccount;
     $scope.search = search;
+    $scope.generate = generate;
+    $scope.reconfigure = reconfigure;
+    $scope.printReport = printReport;
   }
 ]);
