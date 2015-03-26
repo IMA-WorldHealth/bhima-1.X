126515080c5c97bab08b3e00e184a33bcea7fdb4
lomamech <lomamech@gmail.com>
2015-03-26 16:57:20 +0100

    Integration of the cancellation of the payday advance during the actual payment of wages
    Former-commit-id: 1e24dccbe1aaf9281d0149241d8448e7643cdd08 [formerly 2c9aa61c237ae3a639974a5234b06747ac92c0b5]
    Former-commit-id: ffe34c3dae32355f7ab4f2438a78c3f738b6059a

--- server/models/updates/synt.sql
+++ server/models/updates/synt.sql
@@ -528,3 +528,14 @@ INSERT INTO `unit` VALUES
 INSERT INTO `transaction_type` (`service_txt`) VALUES
 ('reversing');
 
+-- Updates to rubric structure
+-- 
+-- ADD 'is_advance' field
+--
+-- Date: 2015-03-25
+-- By: Chris LOMAME
+
+USE bhima;
+
+ALTER TABLE `rubric`
+ADD `is_advance` boolean;
