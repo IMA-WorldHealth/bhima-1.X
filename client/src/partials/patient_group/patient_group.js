c55e27516bc444abb4d253e7f05ee007e8440e29
lomamech <lomamech@gmail.com>
2015-03-31 13:53:40 +0100

    little fix around translation and more
    Former-commit-id: fb30711350165048ee08f918b0f0350a941ef69e [formerly ed90a53648e4827f5a61f9f2c03cf2cd426a31a0]
    Former-commit-id: 3ad3e75684dddd67746534ae96fd4e37f5d9b266

--- client/src/partials/patient_group/patient_group.js
+++ client/src/partials/patient_group/patient_group.js
@@ -115,7 +115,7 @@ angular.module('bhima.controllers')
       connect.put('patient_group', [packaged], ['uuid'])
       .then(function () {
         $scope.group.put(packaged);
-        messenger.success('Successfully modified the group.');
+		messenger.success($translate.instant('PATIENT_GRP.SUCCESS'));
       });
     };
 
