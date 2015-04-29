1a074f045a1caccd987ed894c8b5b81bab4ad8be
lomamech <lomamech@gmail.com>
2015-04-29 06:32:15 +0100

    merge with development
    Former-commit-id: afbaa3dc1d21d7183813abfef9897fdf1b1fab0a [formerly a721dda4fea110d5d5606d77a2f94b96dc72aabe]
    Former-commit-id: 30cabe6dc7368529f794dc993c245a9ff91b76b1

--- server/models/updates/imck/imck_synt.sql
+++ server/models/updates/imck/imck_synt.sql
@@ -1,11 +1,18 @@
+USE bhima;
+
 -- Updates account
 --
 -- ADD 'is_ohada' field
 --
 -- Date: 2015-04-27
 -- By: Chris LOMAME
-
-USE bhima;
-
 ALTER TABLE `account`
 ADD `is_ohada` BOOLEAN NULL;
+
+-- Updates unit
+--
+-- Move Account, Fiscal_year, Creditor_group, debitor_group to Accountant Node
+--
+-- Date: 2015-04-28
+-- By: Bruce Mbayo
+UPDATE `unit` SET `unit`.`parent`=30 WHERE `unit`.`id` IN (6, 13, 23, 24); 
