b58a916873a07f1a905f71123f634bddb57c3b13
mbayopanda <mbayopanda@gmail.com>
2015-04-30 15:13:00 +0100

    Cashbox and Cashbox account currency modules
    Former-commit-id: 25e2e51f0fc9313a979f7ab677581fda7bd42a8f [formerly 7fa4dee87d13b35edca4e18741d89ecda8287cec]
    Former-commit-id: 2b90149a31154452829e6019d65d004a96af08e2

--- server/models/updates/imck/imck_synt.sql
+++ server/models/updates/imck/imck_synt.sql
@@ -16,3 +16,17 @@ ADD `is_ohada` BOOLEAN NULL;
 -- Date: 2015-04-28
 -- By: Bruce Mbayo
 UPDATE `unit` SET `unit`.`parent`=30 WHERE `unit`.`id` IN (6, 13, 23, 24); 
+
+-- Cashbox and Cashbox account currency
+--
+-- Date: 2015-04-30
+-- By: Bruce Mbayo
+INSERT INTO `unit` VALUES
+(105,'Cashbox Management','TREE.CASHBOX_MANAGEMENT','',1,0,'/partials/cash/cashbox/','/cashbox_management/'),
+(106,'Cashbox Account currency Management','TREE.CASHBOX_ACCOUNT_MANAGEMENT','',30,0,'/partials/cash/cashbox_account_currency/','/cashbox_account_management/');
+
+ALTER TABLE `cash_box`
+ADD `is_bank` BOOLEAN NULL;
+
+ALTER TABLE `cash_box_account_currency`
+ADD `virement_account_id` int(11) NULL;
