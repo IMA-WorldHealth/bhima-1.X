9d96261d65bc68456581bf84c9769f886b93d904
Jonathan Niles <jonathanwniles@gmail.com>
2015-04-30 12:04:54 -0400

    implimented morgan logger; requires npm install
    Former-commit-id: 420cb7b573dc14d1a8f0d42cfcdbc0fd7c42501d [formerly dcc5269f7a52c9e480fed0c034c7b89523398c7b]
    Former-commit-id: a82d881011fc385e0c335cc64fb414f100be2cf6

--- server/config/express.js
+++ server/config/express.js
@@ -1,12 +1,13 @@
 /**
  * Express Server Configuration 
- *
  */
 var express       = require('express');
 var compress      = require('compression');
 var bodyParser    = require('body-parser');
 var session       = require('express-session');
 var cookieParser  = require('cookie-parser');
+var morgan        = require('morgan');
+var fs            = require('fs');
 
 var cfg           = require('./../config/environment/server');
 var liberror      = require('./../lib/liberror')(null);
@@ -14,11 +15,22 @@ var liberror      = require('./../lib/liberror')(null);
 // Accept generic express/ authentication instances (initialised in app.js)
 module.exports = function (app, authentication) { 
   console.log('[config/express] Configure express');
-
+  
+  // middleware
   app.use(compress());
   app.use(bodyParser());
   app.use(cookieParser());
   app.use(session(cfg.session));
+  
+  // morgan logger setup
+  // options: combined | common | dev | short | tiny |
+  // Recommend 'combined' for production settings.
+  //
+  // Uncomment if you want logs written to a file instead
+  // of piped to standard out (default).
+  //var logFile = fs.createWriteStream(__dirname + '/access.log', {flags : 'a'});
+  //app.use(morgan('short', { stream : logFile }));
+  app.use(morgan('short'));
    
   // FIXME Only one static directory should be served
   app.use('/css', express.static('client/dest/css', { maxAge : 10000 }));
