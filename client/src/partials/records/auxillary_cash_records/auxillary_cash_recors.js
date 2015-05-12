0167ed347f2572924cb15a8f2bf030575890a8e1
lomamech <lomamech@gmail.com>
2015-05-12 04:26:50 +0100

    Update report of sale record and auxillary cash report

--- client/src/partials/records/auxillary_cash_records/auxillary_cash_recors.js
+++ client/src/partials/records/auxillary_cash_records/auxillary_cash_recors.js
@@ -165,10 +165,16 @@ angular.module('bhima.controllers')
     }
 
     function convert (){
+      console.log('PPPPPPPPPPPPPPPPPPPPPP');
+      console.log($scope.model.cash.data);
+      console.log('PPPPPPPPPPPPPPPPPPPPPP');
+
       session.sum = 0;
       if($scope.model.cash.data) {
         $scope.model.cash.data.forEach(function (cash) {
-          session.sum += exchange.convertir(cash.cost, cash.currency_id, session.currency, new Date());
+          if(!cash.cashDiscardId){
+            session.sum += exchange.convertir(cash.cost, cash.currency_id, session.currency, new Date());
+          }
         });
       }
     }
