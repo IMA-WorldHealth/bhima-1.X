7ffb6e688482a5403cdb66a6bce08dbe896ef32e
lomamech <lomamech@gmail.com>
2015-05-21 04:19:05 +0100

    Fix error and improve reports

--- client/src/partials/receipts/receipt.generic_income.js
+++ client/src/partials/receipts/receipt.generic_income.js
@@ -33,8 +33,26 @@ angular.module('bhima.controllers')
       }
     };
 
+    dependencies.getGeneraLedger = {
+      query : {
+        identifier : 'uuid',
+        tables : {
+          primary_cash : { columns : ['uuid'] },
+          primary_cash_item : { columns : ['document_uuid'] },
+          general_ledger : { columns : ['trans_id'] }
+        },
+        distinct : true,
+        join : ['primary_cash.uuid=primary_cash_item.primary_cash_uuid','general_ledger.inv_po_id=primary_cash_item.document_uuid']
+      }
+    };  
+
+
     function buildInvoice (res) {
-      $scope.trans_id = res.getTransaction.data[0].trans_id;
+      if(res.getTransaction.data.length){
+        $scope.trans_id = res.getTransaction.data[0].trans_id;  
+      } else {
+        $scope.trans_id = res.getGeneraLedger.data[0].trans_id;  
+      }
       
       model.genericIncome = res.genericIncome.data.pop();
     }
@@ -46,6 +64,8 @@ angular.module('bhima.controllers')
         model.common.enterprise = values.enterprise.data.pop();
         dependencies.genericIncome.query.where = ['primary_cash.uuid=' + values.invoiceId];
         dependencies.getTransaction.query.where = ['primary_cash.uuid=' + values.invoiceId];
+        dependencies.getGeneraLedger.query.where = ['primary_cash.uuid=' + values.invoiceId];
+        
         validate.process(dependencies)
         .then(buildInvoice)
         .catch(function (err){
