d805635c249dcdc40bacf995a256507434c992d1
lomamech <lomamech@gmail.com>
2015-06-10 15:49:23 +0100

    Adding unit and module management for reference and reference group
    Former-commit-id: 032502d584fc148458f1b80cee795fb2aca18503 [formerly cb0b5ee2237ef591998fe766328bb57bf168935c]
    Former-commit-id: aa7635fd4d1539b02b4a00bd2d69f123f39d65e3

---
+++ client/src/partials/reference/reference.js
@@ -0,0 +1,138 @@
+angular.module('bhima.controllers')
+.controller('reference', [
+  '$scope',
+  '$translate',
+  '$http',
+  'validate',
+  'messenger',
+  'connect',
+  'appstate',
+  function ($scope, $translate, $http, validate, messenger, connect, appstate) {
+    var dependencies = {},
+        session = $scope.session = {};
+
+    dependencies.references = {
+      query : {
+        identifier : 'id',
+        tables : {
+          'reference' : {
+            columns : ['id', 'ref', 'text', 'position', 'reference_group_id', 'section_resultat_id']
+          },
+          'reference_group' : {
+            columns : ['text::reference_group_txt' ]
+          },
+          'section_resultat' : {
+            columns : ['text::section_resultat_txt' ]
+          }                    
+        },
+        join : [
+          'reference_group.id=reference.reference_group_id',
+          'section_resultat.id=reference.section_resultat_id'
+        ],
+      }
+    };
+
+    dependencies.reference_groups = {
+      query : {
+        tables : {
+          'reference_group' : {
+            columns : ['id', 'text']
+          }
+        }
+      }
+    };
+
+    dependencies.section_resultat = {
+      query : {
+        tables : {
+          'section_resultat' : {
+            columns : ['id', 'text']
+          }
+        }
+      }
+    };
+
+    dependencies.enterprise = {
+      query : {
+        tables : {
+          'enterprise' : {
+            columns : ['currency_id']
+          },
+          'currency' : {
+            columns : ['id', 'symbol']
+          }
+        },
+        join : [
+            'enterprise.currency_id=currency.id'
+          ]
+        }
+    };
+
+    function startup (models) {
+      angular.extend($scope, models);
+    }
+
+    appstate.register('enterprise', function (enterprise) {
+      $scope.enterprise = enterprise;
+      validate.process(dependencies)
+      .then(startup);
+    });
+
+    $scope.delete = function (reference) {
+      var result = confirm($translate.instant('REFERENCE.CONFIRM'));
+      if (result) {  
+        connect.basicDelete('reference', reference.id, 'id')
+        .then(function () {
+          $scope.references.remove(reference.id);
+          messenger.info($translate.instant('REFERENCE.DELETE_SUCCESS'));
+        });
+      }
+    };
+
+    $scope.edit = function (reference) {
+      session.action = 'edit';
+      session.edit = angular.copy(reference);
+    };
+
+    $scope.new = function () {
+      session.action = 'new';
+      session.new = {};
+    }; 
+
+    $scope.save = {};
+
+    $scope.save.edit = function () {
+      var record = connect.clean(session.edit);
+      delete record.reference;
+      delete record.reference_group_txt;
+      delete record.section_resultat_txt;
+
+      connect.basicPost('reference', [record], ['id'])
+      .then(function () {
+        messenger.success($translate.instant('REFERENCE.UPDATE_SUCCES')); 
+        $scope.references.put(record);
+        session.action = '';
+        session.edit = {};
+      });
+    };
+
+    $scope.save.new = function () {
+      var record = connect.clean(session.new);
+
+      connect.basicPut('reference', [record])
+      .then(function () {
+        messenger.success($translate.instant('REFERENCE.SAVE_SUCCES'));        
+        record.reference = generateReference(); 
+        $scope.references.post(record);
+        session.action = '';
+        session.new = {};
+      });
+    };
+
+    function generateReference () {
+      window.data = $scope.references.data;
+      var max = Math.max.apply(Math.max, $scope.references.data.map(function (o) { return o.reference; }));
+      return Number.isNaN(max) ? 1 : max + 1;
+    }
+  }  
+]);
---
+++ client/src/partials/reference_group/reference_group.js
@@ -0,0 +1,126 @@
+angular.module('bhima.controllers')
+.controller('reference_group', [
+  '$scope',
+  '$translate',
+  '$http',
+  'validate',
+  'messenger',
+  'connect',
+  'appstate',
+  function ($scope, $translate, $http, validate, messenger, connect, appstate) {
+    var dependencies = {},
+        session = $scope.session = {};
+
+    dependencies.reference_groups = {
+      query : {
+        identifier : 'id',
+        tables : {
+          'reference_group' : {
+            columns : ['id', 'is_report', 'reference_group', 'text', 'position', 'section_bilan_id']
+          },
+          'section_bilan' : {
+            columns : ['text::section_bilan_txt']
+          }          
+        },
+        join : [
+          'reference_group.section_bilan_id=section_bilan.id'
+        ],
+      }
+    };
+
+    dependencies.section_bilan = {
+      query : {
+        tables : {
+          'section_bilan' : {
+            columns : ['id', 'text']
+          }
+        }
+      }
+    };
+
+    dependencies.enterprise = {
+      query : {
+        tables : {
+          'enterprise' : {
+            columns : ['currency_id']
+          },
+          'currency' : {
+            columns : ['id', 'symbol']
+          }
+        },
+        join : [
+            'enterprise.currency_id=currency.id'
+          ]
+        }
+    };
+
+    function startup (models) {
+      angular.extend($scope, models);
+    }
+
+    appstate.register('enterprise', function (enterprise) {
+      $scope.enterprise = enterprise;
+      validate.process(dependencies)
+      .then(startup);
+    });
+
+    $scope.delete = function (reference_group) {
+      var result = confirm($translate.instant('REFERENCE_GROUP.CONFIRM'));
+      if (result) {  
+        connect.basicDelete('reference_group', reference_group.id, 'id')
+        .then(function () {
+          $scope.reference_groups.remove(reference_group.id);
+          messenger.info($translate.instant('REFERENCE_GROUP.DELETE_SUCCESS'));
+        });
+      }
+    };
+
+    $scope.edit = function (reference_group) {
+      session.action = 'edit';
+      session.edit = angular.copy(reference_group);
+      session.edit.is_report = session.edit.is_report !== 0;
+    };
+
+    $scope.new = function () {
+      session.action = 'new';
+      session.new = {};
+    };
+
+    $scope.save = {};
+
+    $scope.save.edit = function () {
+      var record = connect.clean(session.edit);
+      delete record.reference;
+      delete record.section_bilan_txt;
+      record.is_report = (session.edit.is_report)?1:0;
+
+      connect.basicPost('reference_group', [record], ['id'])
+      .then(function () {
+        messenger.success($translate.instant('REFERENCE_GROUP.UPDATE_SUCCES')); 
+        $scope.reference_groups.put(record);
+        session.action = '';
+        session.edit = {};
+      });
+    };
+
+    $scope.save.new = function () {
+      var record = connect.clean(session.new);
+      record.is_report = (session.new.is_report)?1:0;
+
+      connect.basicPut('reference_group', [record])
+      .then(function () {
+        messenger.success($translate.instant('REFERENCE_GROUP.SAVE_SUCCES'));        
+        record.reference = generateReference(); 
+        $scope.reference_groups.post(record);
+        session.action = '';
+        session.new = {};
+      });
+    };
+
+    function generateReference () {
+      window.data = $scope.reference_groups.data;
+      var max = Math.max.apply(Math.max, $scope.reference_groups.data.map(function (o) { return o.reference; }));
+      return Number.isNaN(max) ? 1 : max + 1;
+    }
+  }  
+]);
