a375abb5ea16e66d8e3d68d4790290eaba67dd5e
Jonathan Niles <jonathanwniles@gmail.com>
2015-06-15 16:51:22 -0400

    rm preprocess - can be done in index.js

--- plugins/mail/index.js
+++ plugins/mail/index.js
@@ -30,6 +30,9 @@ var q = require('q'),
 var render = require(path.join(__dirname, 'lib/render')),
     mailer = require(path.join(__dirname, 'lib/mailer')),
     archiver = require(path.join(__dirname, 'lib/archiver')),
+    query = require(path.join(__dirname,'lib/query')),
+    util = require(path.join(__dirname, 'lib/util')),
+    locales = require(path.join(__dirname,'lib/locales')),
     addressBook = require(path.join(__dirname, 'addresses/list.json'));
 
 function MailPlugin(options) {
@@ -62,7 +65,7 @@ MailPlugin.prototype._configure = function () {
     var timer = later.setInterval(function () {
       var addresses = addressBook[email.addressList];
       addresses.forEach(function (contact) {
-        self.send(email.addressList, email.name, contact);
+        self.send(email.addressList, email.name, contact, new Date());
       });
     }, schedule);
 
@@ -70,22 +73,92 @@ MailPlugin.prototype._configure = function () {
   });
 };
 
-// renders an email and sends it
-MailPlugin.prototype.send = function (list, email, contact) {
+// renders an email and sends it to a given contact
+MailPlugin.prototype.send = function (list, email, contact, date) {
   var base = __dirname + email + '/',
-      data = require(base + 'preprocess.js')(contact.language),
-      date = new Date();
+      queries = require(path.join(base,  'queries.json')),
+      text = require(path.join(base, 'lang', contact.language + '.json')),
+      tpl = fs.readFileSync(path.join(base, 'daily.tmpl.html'), 'utf8'),
+      message;
 
-  // render the email by templating data into the template
-  var message = render(data.template, data.options);
+  // convert date into a database-friendly date
+  var dateFrom = '\'' + date.getFullYear() + '-0' + (date.getMonth() + 1) + '-' + date.getDate() + ' 00:00:00\'',
+      dateTo = '\'' + date.getFullYear() + '-0' + (date.getMonth() + 1) + '-' + (date.getDate() + 1)  + ' 00:00:00\'';
 
-  // use mailer to send the message
-  mailer(list, contact, message, date)
+  // loop through the queries and do the following:
+  // 1) template in the date fields
+  // 2) execute the queries using the query() function
+  // 3) upon successful execution, convert result into proper locale
+  var promises = Object.keys(queries).map(function (k) {
+    var dfd = q.defer(),
+        template = queries[k],
+
+        // template the sql query with correct dates
+        sql = render(template, { date : { to : dateTo, from : dateFrom }});
+
+    // execute the templated query
+    query(sql)
+    .then(function (rows) {
+
+      // all queries return a single number in the `total` field
+      var result = rows[0].total;
+
+      // if the result is a currency, make sure it is in the correct locality
+      // NOTE : this is NOT the email locality, it is defined by the query.
+      if (template.type === 'currency') {
+        template.result = locales.currency(result, template.format);
+      } else {
+        template.result = result;
+      }
+
+      // resolve the original query with the data formatted and
+      // cached in the result property
+      dfd.resolve(template);
+    })
+    .catch(dfd.reject)
+    .done();
+
+    return dfd.promise;
+  });
+
+  // all queries have been executed and the data is cached in the result
+  // property
+  q.all(promises)
+  .then(function (results) {
+    var data = {},
+        templatedText,
+        options;
+
+    // convert promise array to a data object
+    Object.keys(queries).forEach(function (key, idx) {
+      data[key] = results[idx].result;
+    });
+
+    // now, we want to render the language file
+    templatedText = util.recurse(text, function (node, key) {
+      node[key] = render(node[key], data);
+    });
+
+
+    // collate data and text for email templating
+    options = {
+      data : data,
+      text : templatedText
+    };
+
+    // render the email
+    message = render(tpl, options);
+
+    // use mailer to send the message
+    return mailer(list, contact, message, date);
+  })
   .then(function () {
+
+    // after message is sent, archive a copy in /archive
     archiver(list + '-' + contact.name, message, date);
   })
-  .catch(function (err) {
-    throw err;
+  .catch(function (error) {
+    throw error;
   })
   .done();
 };
--- plugins/mail/lib/util.js
+++ plugins/mail/lib/util.js
@@ -1,29 +1,32 @@
-var util = exports;
-var sessionDate = new Date();
+/**
+ * mail plugin utilities
+ *
+ */
 
-util.setDate = function (date) {
-  sessionDate = date;
-};
-
-util.date = configureDate();
-
-
-util.generateUuid = function () { 
+// generate a uuid version 4
+function uuid() {
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
     var r = Math.random() * 16 | 0,
     v = c === 'x' ? r : (r & 0x3 | 0x8);
     return v.toString(16);
   });
-};
-
-function configureDate() { 
-  
-  // Handle requests for tables with only timestamp fields
-  var dateFrom = "\'" + sessionDate.getFullYear() + "-0" + (sessionDate.getMonth() + 1) + "-" + sessionDate.getDate() + " 00:00:00\'";
-  var dateTo = "\'" + sessionDate.getFullYear() + "-0" + (sessionDate.getMonth() + 1) + "-" + (sessionDate.getDate() + 1)  + " 00:00:00\'"; 
-  
-  return { 
-    from : dateFrom,
-    to : dateTo
-  };
 }
+
+// recurively loops through an object (tree), applying the
+// function fn to each value in the tree.
+function recurse(object, fn) {
+  for (var key in object) {
+    if (typeof object[key] === 'object' && object[key] !== null) {
+
+      // descend a level
+      recurse(object[key], fn);
+    } else {
+
+      // apply the function
+      fn(object, key);
+    }
+  }
+}
+
+exports.uuid = uuid;
+exports.recurse = recurse;
