325ba1620c29c9a911f45c48ea748dfb083ffd59
Jonathan Niles <jonathanwniles@gmail.com>
2015-06-17 11:45:06 -0400

    clean up the build code; modular namespaces; fix sanitization scheme
    Former-commit-id: 40597e6f58d7db1a5696acee8eda79be4147464d [formerly ecee6f0f353f853bb9b5c3c2d43048e214ef3ce1]
    Former-commit-id: 360ddc732372e0176566f40977cb35ff6be0fbe0

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -754,9 +754,7 @@
       suffix: '.json'
     });
 
-    // use angular's $sanitize to parse HTML and prevent HTML injects
-    // ref: http://angular-translate.github.io/docs/#/guide/19_security
-    $translateProvider.useSanitizeValueStrategy('sanitize');
+    $translateProvider.useSanitizeValueStrategy('escape');
 
     //TODO Try and assign the previous sessions language key here
     $translateProvider.preferredLanguage('fr');
