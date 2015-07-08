5d9206a54a47cd1b81869eddf73ba62cf9f6f1d5
lomamech <lomamech@gmail.com>
2015-07-08 11:01:04 +0100

    Improve report confirmation stock entry ans receipt in adding ID Transaction Numbers

--- client/src/partials/receipts/receipt.confirm_indirect_purchase.js
+++ client/src/partials/receipts/receipt.confirm_indirect_purchase.js
@@ -5,7 +5,7 @@ angular.module('bhima.controllers')
   'appstate',
   'messenger',
   function ($scope, validate, appstate, messenger) {
-    var dependencies = {}, model = $scope.model = {common : {}};
+    var dependencies = {}, model = $scope.model = {common : {}}, service_txt = 'confirm_purchase';
 
     dependencies.confirmIndirectPurchase = {
       query : {
@@ -55,7 +55,38 @@ angular.module('bhima.controllers')
       }
     };
 
+    dependencies.getTransaction = {
+      query : {
+        identifier : 'uuid',
+        tables : {
+          posting_journal : { columns : ['trans_id'] },
+          transaction_type : {columns : ['id']}
+        },
+        distinct : true,
+        join : ['posting_journal.origin_id=transaction_type.id'],
+        where : ['transaction_type.service_txt=' + service_txt]
+      }
+    };
+
+    dependencies.getGeneraLedger = {
+      query : {
+        identifier : 'uuid',
+        tables : {
+          general_ledger : { columns : ['trans_id'] },
+          transaction_type : {columns : ['id']}
+        },
+        distinct : true,
+        join : ['general_ledger.origin_id=transaction_type.id'],
+        where : ['transaction_type.service_txt=' + service_txt]
+      }
+    };
+
     function buildInvoice (res) {
+      if(res.getTransaction.data.length){
+        $scope.trans_id = res.getTransaction.data[0].trans_id;
+      } else if (res.getGeneraLedger.data.length){
+        $scope.trans_id = res.getTransaction.data[0].trans_id;
+      }
       model.indirectPurchase = res.indirectPurchases.data;
       model.confirmIndirectPurchase = res.confirmIndirectPurchase.data.pop();
     }
@@ -69,6 +100,8 @@ angular.module('bhima.controllers')
 
         dependencies.confirmIndirectPurchase.query.where =  ['purchase.uuid=' + values.invoiceId];
         dependencies.indirectPurchases.query.where =  ['purchase.uuid=' + values.invoiceId];
+        dependencies.getTransaction.query.where = ['posting_journal.inv_po_id=' + values.invoiceId , 'AND', 'transaction_type.service_txt=' + service_txt];
+        dependencies.getGeneraLedger.query.where = ['general_ledger.inv_po_id=' + values.invoiceId , 'AND', 'transaction_type.service_txt=' + service_txt];
 
         validate.process(dependencies)
         .then(buildInvoice)
