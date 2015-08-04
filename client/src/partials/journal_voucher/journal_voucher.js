0f0845cf3d5c1703171350a00921d70c3a343f0e
Jonathan Niles <jonathanwniles@gmail.com>
2015-08-04 14:14:36 +0100

    rm journal voucher service, ng-submit behaves finally
    Former-commit-id: 7a086becf31dceef19b4474bede0cd919cbf0b8d [formerly 2df7cd45b5223f76e1272bd57170d9ccb940dc79]
    Former-commit-id: 767e664acfda75f4d9e2e96fd38909665cae3593

--- client/src/partials/journal_voucher/journal_voucher.js
+++ client/src/partials/journal_voucher/journal_voucher.js
@@ -7,10 +7,25 @@ angular.module('bhima.controllers')
   'exchange',
   function ($http, AppCache, messenger, uuid, exchange) {
 
+    /* This controller wraps all the global metadata
+     * for the journal voucher and the JournalVoucherTableController.
+     * It is responsible for validation checks, submitting
+     * the form, and any error handling.
+    */
+
     var dependencies = {},
+        isDefined = angular.isDefined,
+
+        // cache TODO
         db = new AppCache('journal.voucher');
 
+    // alias this
     var self = this;
+
+    // the master form
+    self.master = {
+      date : new Date()
+    };
 
     // load dependencies
     $http.get('/finance/currencies')
@@ -38,18 +53,41 @@ angular.module('bhima.controllers')
     };
 
     // do the final submit checks
-    self.submitForm = function (isValid) {
-      // TODO
+    self.submitForm = function (voucher) {
+
     };
 
+
+  // ensure that the table portion is valid before submitting
+  function validateTableInput() {
+    var validRows = self.rows.every(function (row) {
+
+      // must have a one non-zero value
+      var validAmount =
+          (row.debit > 0 && !row.credit) ||
+          (!row.debit && row.credit > 0);
+
+      // must have either a debitor/creditor switch
+      // or an account
+      var validAccount =
+          (isDefined(row.deb_cred_uuid) && isDefined(row.deb_cred_type)) ||
+           isDefined(row.account_id);
+
+      return validAmount && validAccount;
+    });
+
+    var validTotals = self.totals.credits === self.totals.debits;
+
+    self.valid = validRows && validTotals;
   }
-])
+
+}])
 
 .controller('JournalVoucherTableController', ['$http', '$q', function ($http, $q) {
 
   /* This controller is somewhat complex because it handles the
    * behavior of either specifying an account OR a debtor/creditor
-   * to debit/credit.
+   * to debit/credit in the journal voucher table.
    *
    * If the user has seleted an account, we must remove the deb_cred_type
    * before submitting to the server. This is done in the .submitForm()
@@ -65,10 +103,11 @@ angular.module('bhima.controllers')
    *
    * Every time a row is updated, the totals must be recalculated.  Since a
    * transaction can only be in one currency, we don't need to know the
-   * curreny - simply that the transaction balances.  The currency is specified
+   * currency - simply that the transaction balances.  The currency is specified
    * in the parent controller (JournalVoucherController).
    *
   */
+
 
   // alias this
   var self = this;
@@ -131,7 +170,11 @@ angular.module('bhima.controllers')
   };
 
   // switches the debtor or creditor type
+  // NOTE switching debtor/creditor type destroys
+  // previously cached data
   self.setDebtorOrCreditorType = function (row, type) {
+    row.deb_cred_uuid = undefined;
+    row.entity = undefined;
     row.deb_cred_type = type;
   };
 
@@ -163,4 +206,25 @@ angular.module('bhima.controllers')
     self.rows.splice(idx, 1);
   };
 
+  /* formatters */
+  self.fmtAccount = function (account) {
+    return account ?  account.account_number + ' ' +  account.account_txt : '';
+  };
+
+  // Set a debtor or creditor for the row
+  // First, remove the old account_id.
+  // Then, set the deb_cred_uuid and account_id properly
+  self.setDebtorOrCreditor = function (row) {
+    row.account_id = row.entity.account_id;
+    row.deb_cred_uuid = row.entity.uuid;
+  };
+
+  // set the account for a row
+  // remove all debtor/creditor properties that may
+  // have been set on a previous selection
+  self.setAccount = function (row) {
+    row.account_id = row.account.id;
+    row.deb_cred_uuid = undefined;
+    row.entity = undefined;
+  };
 }]);
