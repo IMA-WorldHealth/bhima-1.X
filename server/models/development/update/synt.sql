3c02569fbc6eb613e254379d08a8f0d3b5f4f6b8
mbayopanda <mbayopanda@gmail.com>
2015-08-05 13:42:27 +0100

    Improve monthly Balance, and adding option for watch balance for all account
    Former-commit-id: 049c46fe0c6d095a653a3e3504aca161956582c1 [formerly 723db9776a0e102ebdd44238ef31eff74c67da70]
    Former-commit-id: 58d00cb15acccd0f0637e0bf6ce5f8fda7b59b73

---
+++ server/models/development/update/synt.sql
@@ -0,0 +1,18 @@
+-- Updates table account
+-- Updating the is_asset column following the accounting standard
+--
+-- Date: 2015-08-04
+-- By: Chris LOMAME
+
+USE bhima;
+UPDATE `account` SET `is_asset`=FALSE;
+UPDATE `account` SET `is_asset`=TRUE WHERE `classe`=2 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE `classe`=3 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE LEFT(`account_number`,2)=41 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE LEFT(`account_number`,4)=4711 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE LEFT(`account_number`,3)=476 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE LEFT(`account_number`,3)=478 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE `classe`=5 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE `classe`=6 AND account_number AND `account_number` NOT LIKE '603%' AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=TRUE WHERE LEFT(`account_number`,2)=73 AND `is_ohada` =1;
+UPDATE `account` SET `is_asset`=NULL WHERE `parent`=0 AND `is_ohada` =1;
