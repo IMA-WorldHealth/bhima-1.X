08152006526ec9190b3f277d16bf802027d2e7a3
Jonathan Niles <jonathanwniles@gmail.com>
2015-08-13 10:57:59 +0100

    Add users controller shell
    Former-commit-id: 938bc4dd2eea4d7ecbf39d48037b26a15e004a88 [formerly 35f8fd3c968cb9bcad314ccc7692723d9cf7184a]
    Former-commit-id: 845a15589f63e13847e25c5c7a08e3669c9c8267

---
+++ server/controllers/users.js
@@ -0,0 +1,16 @@
+
+
+// POST /users
+exports.createUser = function (req, res, next) {
+
+};
+
+// GET /users
+exports.getUsers = function (req, res, next) {
+
+}
+
+// GET /users/:id
+exports.getUserById = function (req, res, next) {
+
+};
