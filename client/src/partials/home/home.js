25d568fe1f931a6fdd3064c72088a4f9b34b7a1c
Jonathan Niles <jonathanwniles@gmail.com>
2015-08-13 16:01:03 +0100

    logout behaves as expected; redirection; HomeCtrl updated to reflect deltas
    Former-commit-id: 4e516ddc7e0b141b1c467b0fecd0038efa5e820d [formerly 1dae65db303aaee8d7fb4690fbd65dd13f17f8c3]
    Former-commit-id: c731cbd1dbadfb3409a74737dcf6747d7f5d979b

--- client/src/partials/home/home.js
+++ client/src/partials/home/home.js
@@ -1,48 +1,20 @@
 angular.module('bhima.controllers')
-.controller('home', [
+.controller('HomeController', [
   '$scope',
   '$translate',
   'appstate',
-  'validate',
   'exchange',
-  function ($scope, $translate, appstate, validate, exchange) {
-  	var session = $scope.session = {},
-  			dependencies = {};
+  'SessionService',
+  function ($scope, $translate, appstate, exchange, SessionService) {
+    var self = this;
 
-  	dependencies.register = {
-      query : 'user_session'
-    };
+    self.today = new Date();
+    self.project = SessionService.project;
+    self.user = SessionService.user;
+    self.hasDailyRate = exchange.hasDailyRate();
 
-    function init (model) {
-    	angular.extend($scope, model);
-    	session.date = new Date();
-      session.hasDailyRate = exchange.hasDailyRate();
-    	session.exchangeRate = session.hasDailyRate ? '1 $ = ' + exchange.rate(100, 1, session.date) + ' Fc' : $translate.instant('HOME.UNDEFINED');
-    	handleUserInfo();
-    }
-
-    function handleUserInfo () {
-    	session.user = $scope.user.data[0];
-    }
-
-    function treePermission (model) {
-      dependencies.user = {
-        query : {
-          tables : {
-            user : { columns : ['first', 'last'] }
-          },
-          where : ['user.id=' + model.register.data.id]
-        }
-      };
-	    validate.process(dependencies, ['user'])
-	    .then(init);
-    }
-
-    appstate.register('project', function (project) {
-      $scope.project = project;
-      validate.process(dependencies, ['register'])
-      .then(treePermission);
-    });
-
+    // FIXME
+    // This doesn't account for multiple currencies
+    self.exchangeRate = self.hasDailyRate ? '1 $ = ' + exchange.rate(100, 1, self.today) + ' Fc' : $translate.instant('HOME.UNDEFINED');
   }
 ]);
