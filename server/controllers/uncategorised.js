67cc116faf816198ea23ba7416c21dd8c5fc9cbe
lomamech <lomamech@gmail.com>
2015-08-24 15:24:57 +0100

    Improve Report Operating account and other fixes

--- server/controllers/uncategorised.js
+++ server/controllers/uncategorised.js
@@ -1397,36 +1397,6 @@ exports.lookupPeriod = function (req, res, next) {
   .done();
 };
 
-exports.listExploitationAccount = function (req, res, next) {
-  console.log(req.query.fiscal_year_id);
-  var sql = "SELECT period_total.period_id, account.account_number, account.account_txt, period_total.credit, period_total.debit"
-          + " FROM period_total"
-          + " JOIN account ON account.id = period_total.account_id"
-          + " WHERE period_total.period_id = " + sanitize.escape(req.query.period_id)
-          + " AND (account.account_number LIKE '6%' OR account.account_number LIKE '7%')"
-          + " ORDER BY account.account_number ASC";
-
-  console.log(req.query.period_id);
-  if(req.query.period_id === 'all'){
-    var sql = "SELECT period_total.period_id, account.account_number, account.account_txt, SUM(period_total.credit) AS 'credit',"
-            + " SUM(period_total.debit) AS 'debit'"
-            + " FROM period_total"
-            + " JOIN account ON account.id = period_total.account_id"
-            + " WHERE period_total.fiscal_year_id = " + sanitize.escape(req.query.fiscal_year_id)
-            + " AND (account.account_number LIKE '6%' OR account.account_number LIKE '7%')"
-            + " GROUP BY account.account_number"
-            + " ORDER BY account.account_number ASC";
-  }
-
-  req.query.fiscal_year_id
-  db.exec(sql)
-  .then(function (result) {
-    res.send(result);
-  })
-  .catch(function (err) { next(err); })
-  .done();
-};
-
 exports.payCotisation = function (req, res, next) {
   cotisationPayment.execute(req.body, req.session.user.id, function (err, ans) {
     if (err) { return next(err); }
