02da138f0b09212c2e6cdcbfbd137006b64b9872
kitamuka <kitamuka@gmail.com>
2015-08-24 19:20:03 +0100

    cash flow structure continues and some bugs fixing
    Former-commit-id: d4724c7ffaeee5844a754b89e7b5c1597aad0a4a [formerly 45d2d7d5659c338c5fc9110ac0c903c557fa62cb]
    Former-commit-id: 51c40b51c9273dca9a4b13fd51caa93d43e9bd3c

--- server/config/environment/server.js
+++ server/config/environment/server.js
@@ -30,7 +30,7 @@ var config = {
       'emails' : [{
         'name' : 'daily',
         //'frequency' : '0 23 * * *',
-        'frequency' : '* * * * 1', // every 3 minutes
+        'frequency' : '* * * * 5', //
         'addressList' : 'test',
       }, {
         'name' : 'weekly',
@@ -38,7 +38,7 @@ var config = {
         'addressList' : 'test'
       }, {
         'name' : 'monthly',
-        'frequency' : '* * * * 1',
+        'frequency' : '* * * * 5',
         'addressList' : 'test'
       }]
     }
