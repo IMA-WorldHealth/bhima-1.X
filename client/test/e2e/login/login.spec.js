059fc9f23664265b37ddec0e7a82e071297f6aa0
Jonathan Niles <jonathanwniles@gmail.com>
2015-08-26 14:35:47 +0100

    Yay!  First passing test!
    Former-commit-id: 7460b116398943a22ec306648dcc8ce3845f1712 [formerly f4bc6f9f9c988e935c527fde4f9e14ea893bc4b9]
    Former-commit-id: 2c2fe5fce1fc963e62f583310df15f6be171c5b4

--- client/test/e2e/login/login.spec.js
+++ client/test/e2e/login/login.spec.js
@@ -1,17 +1,23 @@
 /*global describe, it, beforeEach, inject, browser */
 
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+
+chai.use(chaiAsPromised);
+var expect = chai.expect;
+
 describe('bhima login page', function () {
 
   it('rejects an undefined user', function () {
 
-     browser.get('https://localhost:8080/#/login');
+    browser.get('https://localhost:8080/#/login');
 
     element(by.model('LoginCtrl.credentials.username')).sendKeys('undefineds');
     element(by.model('LoginCtrl.credentials.password')).sendKeys('undefined1');
 
     element(by.id('submit')).click();
 
-    expect(element(by.css('.help-block')).isPresent()).to.be.false;
+    expect(element(by.css('.help-block')).isPresent()).to.eventually.be.true;
   });
 
 });
