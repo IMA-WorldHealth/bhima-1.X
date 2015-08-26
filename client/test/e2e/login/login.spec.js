538055e8a71b4a678347642b9bda33e91b9bdc1f
Jonathan Niles <jonathanwniles@gmail.com>
2015-08-26 14:26:18 +0100

    removed karma in favor of protractor
    Former-commit-id: 7045c17e80f3b2c0a39cc0a1dce8294d8e990ba2 [formerly a939b5be86cbabc8dd7ca34c6ae93e33d04449e7]
    Former-commit-id: 4ef7c468e82ef1939c31a9c69e1eb368e34968bd

--- client/test/e2e/login/login.spec.js
+++ client/test/e2e/login/login.spec.js
@@ -1,17 +1,10 @@
 /*global describe, it, beforeEach, inject, browser */
 
-var chai = require('chai');
-var chaiAsPromised = require('chai-as-promised');
-chai.use(chaiAsPromised);
-var expect = chai.expect;
-
 describe('bhima login page', function () {
 
-  beforeEach(function () {
-     browser.get('https://localhost:8080/#/login');
-  });
-
   it('rejects an undefined user', function () {
+
+     browser.get('https://localhost:8080/#/login');
 
     element(by.model('LoginCtrl.credentials.username')).sendKeys('undefineds');
     element(by.model('LoginCtrl.credentials.password')).sendKeys('undefined1');
