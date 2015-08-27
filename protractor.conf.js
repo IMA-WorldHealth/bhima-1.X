a671deb31c989da4569e681fc3175750a574c543
Jonathan Niles <jonathanwniles@gmail.com>
2015-08-27 15:11:46 +0100

    configure protractor to use Travis only on PRs
    Former-commit-id: 66ab029ed5ff39ab053b27ee154b155e5a8ab341 [formerly 1b42b74e3aed2727ea4298b815eac65f04ec5a3c]
    Former-commit-id: 7c98fe5a5daf3f6b15d594518f0f04ab7cd877a4

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,26 +2,27 @@
 
 var q = require('q');
 
-exports.config = {
+// we want to make sure we run tests locally, but TravisCI
+// should run tests on it's own driver.  To find out if it
+// is Travis loading the configuration, we parse the
+// process.env.TRAVIS_BUILD_NUMBER and reconfigure for travis
+// as appropriate.
+
+var config = {
   seleniumAddress: 'http://localhost:4444/wd/hub',
-
-  // configuration for running on SauceLabs
-  sauceUser: process.env.SAUCE_USERNAME,
-  sauceKey: process.env.SAUCE_ACCESS_KEY,
-
-  multiCapabilities: [{
-    'browserName': 'firefox',
-     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  }, {
-    'browserName': 'chrome',
-     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
-  }],
 
   specs: ['client/test/e2e/**/*.spec.js'],
 
   framework : 'mocha',
   baseUrl : 'https://localhost:8080/',
   allScriptsTimeout : 30000,
+
+  // default browsers to run
+  multiCapabilities: [{
+    'browserName': 'firefox',
+  }, {
+    'browserName': 'chrome',
+  }],
 
   // this will log the user in to begin with
   onPrepare : function () {
@@ -37,3 +38,25 @@ exports.config = {
     }).delay(3100);
   }
 };
+
+// configuration for running on SauceLabs via Travis
+if (process.env.TRAVIS_BUILD_NUMBER) {
+
+  // SauceLabs credentials
+  config.sauceUser = process.env.SAUCE_USERNAME;
+  config.sauceKey = process.env.SAUCE_ACCESS_KEY;
+
+  // modify the browsers to use Travis identifiers
+  config.multiCapabilities = [{
+    'browserName': 'firefox',
+     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+     'build': process.env.TRAVIS_BUILD_NUMBER,
+  }, {
+    'browserName': 'chrome',
+     'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+     'build': process.env.TRAVIS_BUILD_NUMBER,
+  }];
+}
+
+// expose to the outside world
+exports.config = config;
