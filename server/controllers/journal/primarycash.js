44886e61ff4169719f76ec6b7d7c5b23d713d2cd
Jonathan Niles <jonathanwniles@gmail.com>
2015-09-01 12:42:45 +0100

    finish refactoring primary cash refund
    Former-commit-id: 17f1f3ef81fbd28bd9e72a576525eb0fa1bf5cb7 [formerly c9121aad42604be7a165a01198b792091a2d3bcd]
    Former-commit-id: d5d65d6375131866266725b622ac9c6e92cad93b

--- server/controllers/journal/primarycash.js
+++ server/controllers/journal/primarycash.js
@@ -5,6 +5,7 @@ var q = require('q'),
     db = require('../../lib/db');
 
 exports.transfer = transfer;
+exports.refund = refund;
 
 /*
  * Transfer cash from one cashbox to another
@@ -66,7 +67,12 @@ function transfer(id, userId, cb) {
         'currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id ) '+
       'VALUES (?);';
 
-    params = [uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(), cfg.description, reference.account_id, reference.cost, 0, cfg.valueExchanged, 0, reference.currency_id, null, null, id, cfg.originId, userId];
+    params = [
+      uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(), cfg.description,
+      reference.account_id, reference.cost, 0, cfg.valueExchanged, 0, reference.currency_id, null, null, id,
+      cfg.originId, userId
+    ];
+
     return db.exec(sql, [params]);
   })
   .then(function () {
@@ -78,7 +84,12 @@ function transfer(id, userId, cb) {
         'currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id ) '+
       'VALUES (?);';
 
-    params = [uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(), cfg.description, reference.virement_account_id, 0, reference.cost, 0, cfg.valueExchanged, reference.currency_id, null, null, id, cfg.originId, userId];
+    params = [
+      uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(), cfg.description,
+      reference.virement_account_id, 0, reference.cost, 0, cfg.valueExchanged, reference.currency_id, null, null,
+      id, cfg.originId, userId
+    ];
+
     return db.exec(sql, [params]);
   })
 
@@ -96,9 +107,11 @@ function transfer(id, userId, cb) {
         'currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id ) '+
       'VALUES (?);';
 
-    params = [uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(),
-        cfg.description, reference.virement_account_id, reference.cost, 0, cfg.valueExchanged,
-        0, reference.currency_id, null, null, id, cfg.originId, userId];
+    params = [
+      uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(),
+      cfg.description, reference.virement_account_id, reference.cost, 0, cfg.valueExchanged,
+      0, reference.currency_id, null, null, id, cfg.originId, userId
+    ];
 
     return db.exec(sql, [params]);
   })
@@ -113,9 +126,12 @@ function transfer(id, userId, cb) {
       'WHERE cash_box_account_currency.cash_box_id = ? ' +
         'AND cash_box_account_currency.currency_id = ?;';
 
-    params = [uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(),
-        cfg.description, 0, reference.cost, 0, cfg.valueExchanged, reference.currency_id, null, null,
-        id, cfg.originId, userId, reference.cash_box_id, reference.currency_id];
+    params = [
+      uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(),
+      cfg.description, 0, reference.cost, 0, cfg.valueExchanged, reference.currency_id, null, null,
+      id, cfg.originId, userId, reference.cash_box_id, reference.currency_id
+    ];
+
     return db.exec(sql, params);
   })
 
@@ -138,6 +154,101 @@ function transfer(id, userId, cb) {
     })
     .then(function () {
       cb(error);
+    })
+    .catch(cb)
+    .done();
+  })
+  .done();
+}
+
+/*
+ * Refund cash to an organisation from the Primary Cash Box
+ *
+ * This is used when you need to pay someone back for a previous (paid) bill.
+ */
+function refund(id, userId, cb) {
+  var sql, data, params, reference, cfg = {}, queries = {};
+
+  // TODO : Formalize this
+  sql =
+    'SELECT * FROM primary_cash WHERE primary_cash.uuid = ?;';
+
+  // TODO -- any checks?
+  db.exec(sql, [id])
+  .then(function (results) {
+    if (results.length === 0) {
+      throw new Error('No primary_cash by the uuid: ' + id);
+    }
+
+    reference = results[0];
+    data = results;
+    var date = util.toMysqlDate(reference.date);
+
+    return core.queries.myExchangeRate(date);
+  })
+  .then(function (exchangeRateStore) {
+    var dailyExchange = exchangeRateStore.get(reference.currency_id);
+    cfg.valueExchanged = parseFloat((1/dailyExchange.rate) * reference.cost).toFixed(4);
+
+    return q([core.queries.origin('cash_return'), core.queries.period(reference.date)]); // should be core.queries.origin(pcash_transfert);
+  })
+  .spread(function (originId, periodObject) {
+    cfg.originId = originId;
+    cfg.periodId = periodObject.id;
+    cfg.fiscalYearId = periodObject.fiscal_year_id;
+
+    return core.queries.transactionId(reference.project_id);
+  })
+  .then(function (transId) {
+    cfg.transId = transId;
+
+    sql =
+      'INSERT INTO posting_journal (' +
+        'uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, ' +
+        'description, account_id, credit, debit, credit_equiv, debit_equiv, ' +
+        'currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id) ' +
+      'SELECT ?,?,?,?,?,?, account_id,?,?,?,?,?,?,?,?,?,? ' +
+      'FROM cash_box_account_currency ' +
+      'WHERE cash_box_account_currency.cash_box_id = ? AND ' +
+          'cash_box_account_currency.currency_id = ?;';
+
+    params = [
+      uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, reference.date,
+      reference.description, reference.cost, 0, cfg.valueExchanged, 0, reference.currency_id, null, null,
+      id, cfg.originId, userId, reference.cash_box_id, reference.currency_id
+    ];
+
+    return db.exec(sql, params);
+  })
+  .then(function () {
+    sql  =
+      'INSERT INTO posting_journal (uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, ' +
+        'description, account_id, credit, debit, credit_equiv, debit_equiv, ' +
+        'currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id) '+
+      'VALUES (?);';
+
+    params = [
+      uuid(), reference.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, reference.date,
+      reference.description, reference.account_id, 0, reference.cost, 0, cfg.valueExchanged,
+      reference.currency_id, reference.deb_cred_uuid, reference.deb_cred_type, id, cfg.originId,
+      userId
+    ];
+
+    return db.exec(sql, params);
+  })
+  .then(function (rows) {
+    cb(null, rows);
+  })
+  .catch(function (err) {
+    sql = 'DELETE FROM primary_cash_item WHERE primary_cash_uuid = ?;';
+
+    db.exec(sql, [id])
+    .then(function() {
+      sql = 'DELETE FROM primary_cash WHERE uuid = ?;;';
+      return db.exec(sql, [id]);
+    })
+    .then(function () {
+      cb(err);
     })
     .catch(cb)
     .done();
