25d5fa4221375e734e34643080b4bda560b17f2d
Jonathan Niles <jonathanwniles@gmail.com>
2015-09-02 12:13:38 +0100

    convention invoice route refactor complete
    Former-commit-id: df06997c16f81686be7e8553936ff57b093bb5ed [formerly 247587fd11a0238a6cf3923f1af1c2d773b0cc0b]
    Former-commit-id: f88f02873b118d5e5efd72aabab7eb797799c99c

--- server/controllers/journal/convention.js
+++ server/controllers/journal/convention.js
@@ -2,7 +2,6 @@ var q        = require('q'),
     db       = require('../../lib/db'),
     uuid     = require('../../lib/guid'),
     validate = require('../../lib/validate')(),
-    util     = require('../../lib/util'),
     core     = require('./core');
 
 exports.invoice = invoice;
@@ -63,51 +62,75 @@ function invoice(id, userId, cb) {
     cfg.period_id = periodObject.id;
     cfg.fiscal_year_id = periodObject.fiscal_year_id;
 
-    references.forEach(function (row) {
+    return core.queries.transactionId(cfg.project_id);
+  })
+  .then(function (transId) {
+    var description, params;
 
-      return core.queries.transactionId(cfg.project_id)
-      .then(function  (trans_id) {
-        cfg.descript = trans_id.substring(0,4) + '_VENTE_CHARITE/' + new Date().toISOString().slice(0, 10).toString();
-        var debitSql=
-          'INSERT INTO posting_journal ' +
-          '  (uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, ' +
+    cfg.queries = [];
+
+    // loop through the array of rows and generate a debit and credit SQL
+    // query for each row
+    references.forEach(function (row) {
+      description = transId.substring(0,4) + '_VENTE_CHARITE/' + new Date().toISOString().slice(0, 10).toString();
+
+      sql =
+        'INSERT INTO posting_journal (' +
+          'uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, ' +
           'description, account_id, debit, credit, debit_equiv, credit_equiv, ' +
           'currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id) ' +
-          'SELECT ' +
-            [sanitize.escape(uuid()), cfg.project_id, cfg.fiscal_year_id, cfg.period_id, trans_id,  new Date() , sanitize.escape(cfg.descript)].join(', ') + ', ' +
-          'debitor_group.account_id, group_invoice_item.cost, ' +
-          '  0, group_invoice_item.cost, 0, enterprise.currency_id, ' +
-          '  null, null, group_invoice_item.invoice_uuid, ' +
-          [cfg.originId, userId].join(', ') + ' ' +
-          'FROM group_invoice JOIN group_invoice_item JOIN debitor_group JOIN sale JOIN project JOIN enterprise ON ' +
-          '  group_invoice.uuid = group_invoice_item.payment_uuid AND ' +
-          '  group_invoice.group_uuid = debitor_group.uuid  AND ' +
-          '  group_invoice_item.invoice_uuid = sale.uuid AND ' +
-          '  group_invoice.project_id = project.id AND ' +
-          '  project.enterprise_id = enterprise.id ' +
-          'WHERE group_invoice_item.uuid = ' + sanitize.escape(row.gid);
-        var credit_sql=
-          'INSERT INTO posting_journal ' +
-          '  (project_id, uuid, fiscal_year_id, period_id, trans_id, trans_date, ' +
+        'SELECT ?, group_invoice.project_id, ?, ?, ?, ?, ?, debitor_group.account_id, group_invoice_item.cost, ' +
+          '0, group_invoice_item.cost, 0, enterprise.currency_id, null, null, ' +
+          'group_invoice_item.invoice_uuid, ?, ? ' +
+        'FROM group_invoice JOIN group_invoice_item JOIN debitor_group JOIN sale JOIN project JOIN enterprise ON ' +
+          'group_invoice.uuid = group_invoice_item.payment_uuid AND ' +
+          'group_invoice.group_uuid = debitor_group.uuid  AND ' +
+          'group_invoice_item.invoice_uuid = sale.uuid AND ' +
+          'group_invoice.project_id = project.id AND ' +
+          'project.enterprise_id = enterprise.id ' +
+        'WHERE group_invoice_item.uuid = ?;';
+
+      params = [
+        uuid(), cfg.fiscal_year_id, cfg.period_id,
+        transId,  new Date(), description, cfg.originId, userId, row.gid
+      ];
+
+      // push the debit sql query
+      cfg.queries.push({ sql : sql, params : params });
+
+      sql =
+        'INSERT INTO posting_journal (' +
+          'uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, ' +
           'description, account_id, debit, credit, debit_equiv, credit_equiv, ' +
-          '  currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id) ' +
-          'SELECT group_invoice.project_id, ' +
-            [sanitize.escape(uuid()), cfg.fiscal_year_id, cfg.period_id, trans_id, new Date(), sanitize.escape(cfg.descript)].join(', ') + ', ' +
+          'currency_id, deb_cred_uuid, deb_cred_type, inv_po_id, origin_id, user_id) ' +
+        'SELECT ?, group_invoice.project_id,  ?, ?, ?, ?, ?, ' +
           'debitor_group.account_id, 0, group_invoice_item.cost, ' +
           '0, group_invoice_item.cost, enterprise.currency_id,  ' +
-          'group_invoice.debitor_uuid, \'D\', group_invoice_item.invoice_uuid, ' +
-          [ cfg.originId, userId].join(', ') + ' ' +
-          'FROM group_invoice JOIN group_invoice_item JOIN debitor JOIN debitor_group JOIN sale JOIN project JOIN enterprise ON ' +
-          '  group_invoice.uuid = group_invoice_item.payment_uuid AND ' +
-          '  group_invoice.debitor_uuid = debitor.uuid  AND ' +
-          '  debitor.group_uuid = debitor_group.uuid AND ' +
-          '  group_invoice_item.invoice_uuid = sale.uuid AND ' +
-          '  group_invoice.project_id = project.id AND ' +
-          '  project.enterprise_id = enterprise.id ' +
-          'WHERE group_invoice_item.uuid = ' + sanitize.escape(row.gid);
-        return q.all([db.exec(debitSql), db.exec(credit_sql)]);
-      });
+          'group_invoice.debitor_uuid, \'D\', group_invoice_item.invoice_uuid, ?, ? ' +
+        'FROM group_invoice JOIN group_invoice_item JOIN debitor JOIN debitor_group JOIN sale JOIN project JOIN enterprise ON ' +
+          'group_invoice.uuid = group_invoice_item.payment_uuid AND ' +
+          'group_invoice.debitor_uuid = debitor.uuid  AND ' +
+          'debitor.group_uuid = debitor_group.uuid AND ' +
+          'group_invoice_item.invoice_uuid = sale.uuid AND ' +
+          'group_invoice.project_id = project.id AND ' +
+          'project.enterprise_id = enterprise.id ' +
+        'WHERE group_invoice_item.uuid = ?;';
+
+      // we have to reset the uuid!
+      params = [
+        uuid(), cfg.fiscal_year_id, cfg.period_id,
+        transId,  new Date(), description, cfg.originId, userId, row.gid
+      ];
+
+      // push the credit sql query
+      cfg.queries.push({ sql : sql, params : params });
     });
+
+    // map the queries through db.exec, templating and
+    // executing them one after another
+    return q.all(cfg.queries.map(function (query) {
+      return db.exec(query.sql, query.params);
+    }));
   })
   .then(function (res) {
     cb(null, res);
