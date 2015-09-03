1881f747de1679a1368ad2b3a9afdc74d1da852d
Jonathan Niles <jonathanwniles@gmail.com>
2015-09-03 14:09:27 +0100

    migrated payEmployee route
    Former-commit-id: a60e8440937aca808a9d5a07945e54d8ae326690 [formerly cb81fa0ee092112c52b327b19ce134e539c1a5d3]
    Former-commit-id: fe8f9db90a37411831416c1eb91a2c96a254f2cb

--- server/controllers/journal.js
+++ server/controllers/journal.js
@@ -22,122 +22,6 @@ function lookupTable(req, res, next) {
 }
 
 
-function handleEmployee (id, user_id, done) {
-  var dayExchange = {}, reference = {}, cfg = {};
-  var sql = 'SELECT * FROM `primary_cash` WHERE `primary_cash`.`uuid`='+sanitize.escape(id)+';';
-
-  function getRecord (records) {
-    if (records.length === 0) { throw new Error('pas enregistrement'); }
-    reference.reference_pcash = records[0];
-    sql = 'SELECT * FROM `primary_cash_item` WHERE `primary_cash_item`.`primary_cash_uuid`='+sanitize.escape(id)+';';
-    return db.exec(sql);
-  }
-
-  function getItems (records) {
-    if (records.length === 0) { throw new Error('pas enregistrement'); }
-    reference.reference_pcash_items = records;
-    var date = util.toMysqlDate(reference.reference_pcash.date);
-    return core.queries.myExchangeRate(date);
-  }
-
-  function getExchange (exchangeStore) {
-    dayExchange = exchangeStore.get(reference.reference_pcash.currency_id);
-    return q([core.queries.origin('pcash_employee'), core.queries.period(reference.date)]);
-  }
-
-  function getDetails (originId, periodObject) {
-    cfg.originId = originId;
-    cfg.periodId = periodObject.id;
-    cfg.fiscalYearId = periodObject.fiscal_year_id;
-    return core.queries.transactionId(reference.reference_pcash.project_id);
-  }
-
-  function getTransId (trans_id) {
-    cfg.trans_id = trans_id;
-    cfg.descrip =  trans_id.substring(0,4)  + '_CAISSEPRINCIPALE_EMPLOYEE' + new Date().toISOString().slice(0, 10).toString();
-    return q.when();
-  }
-
-  function debit () {
-    return q.all(
-      reference.reference_pcash_items.map(function (ref_pcash_item) {
-        var valueExchanged = parseFloat((1/dayExchange.rate) * ref_pcash_item.debit).toFixed(4);
-
-        var sql =
-          'INSERT INTO posting_journal ' +
-          '(`uuid`,`project_id`, `fiscal_year_id`, `period_id`, `trans_id`, `trans_date`, ' +
-          '`description`, `account_id`, `debit`, `credit`, `debit_equiv`, `credit_equiv`, ' +
-          '`currency_id`, `deb_cred_uuid`, `deb_cred_type`, `inv_po_id`, `origin_id`, `user_id` ) ' +
-          'SELECT ' +
-            [
-              sanitize.escape(uuid()),
-              reference.reference_pcash.project_id,
-              cfg.fiscalYearId,
-              cfg.periodId,
-              cfg.trans_id, '\'' + getDate() + '\'', sanitize.escape(cfg.descrip)
-            ].join(',') + ', `account_id`, ' +
-            [
-              ref_pcash_item.debit, 0,
-              valueExchanged, 0,
-              reference.reference_pcash.currency_id
-            ].join(',') + ', null, \'C\', '+
-          [sanitize.escape(ref_pcash_item.inv_po_id), cfg.originId, user_id].join(',') +
-          ' FROM cash_box_account_currency WHERE `cash_box_account_currency`.`cash_box_id`=' + sanitize.escape(reference.reference_pcash.cash_box_id) +
-          ' AND `cash_box_account_currency`.`currency_id`=' + sanitize.escape(reference.reference_pcash.currency_id);
-
-        return db.exec(sql);
-      })
-    );
-  }
-
-  function credit () {
-    return q.all(
-      reference.reference_pcash_items.map(function (ref_pcash_item) {
-        var valueExchanged = parseFloat((1/dayExchange.rate) * ref_pcash_item.debit).toFixed(4);
-
-        var credit_sql =
-          'INSERT INTO posting_journal ' +
-          '(`uuid`,`project_id`, `fiscal_year_id`, `period_id`, `trans_id`, `trans_date`, ' +
-          '`description`, `account_id`, `debit`, `credit`, `debit_equiv`, `credit_equiv`, ' +
-          '`currency_id`, `deb_cred_uuid`, `deb_cred_type`, `inv_po_id`, `origin_id`, `user_id` ) ' +
-          'VALUES (' +
-            [
-              sanitize.escape(uuid()),
-              reference.reference_pcash.project_id,
-              cfg.fiscalYearId,
-              cfg.periodId,
-              cfg.trans_id, '\''+getDate()+'\'',  sanitize.escape(cfg.descrip), reference.reference_pcash.account_id
-            ].join(',') + ', ' +
-            [
-              0, ref_pcash_item.debit,
-              0, valueExchanged,
-              reference.reference_pcash.currency_id
-            ].join(',') + ', ' + sanitize.escape(reference.reference_pcash.deb_cred_uuid) + ', \'C\', '+
-            [sanitize.escape(ref_pcash_item.inv_po_id), cfg.originId, user_id].join(',') +
-          ');';
-        return db.exec(credit_sql);
-      })
-    );
-  }
-
-  function handleError (err) {
-    console.log('[voici errer]', err);
-  }
-
-  db.exec(sql)
-  .then(getRecord)
-  .then(getItems)
-  .then(getExchange)
-  .spread(getDetails)
-  .then(getTransId)
-  .then(debit)
-  .then(credit)
-  .then(function (res) {
-    return done(null, res);
-  })
-  .catch(handleError);
-
-}
 
 function handleGenericExpense(id, user_id, done) {
   var sql, state = {}, data, reference, cfg = {};
@@ -2518,7 +2402,7 @@ tableRouter = {
   'credit_note'             : require('./journal/sale').creditNote,
   'caution'                 : require('./journal/sale').caution,
   'pcash_convention'        : require('./journal/primarycash').convention,
-  'pcash_employee'          : handleEmployee,
+  'pcash_employee'          : require('./journal/primarycash').payEmployee,
   'primary_expense'         : handleGenericExpense,
   'primary_income'          : handleGenericIncome,
   'indirect_purchase'       : handleIndirectPurchase,
