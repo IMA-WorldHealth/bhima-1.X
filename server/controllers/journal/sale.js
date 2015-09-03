ea114b93024f4612fa71ece1a55c2391303d9272
Jonathan Niles <jonathanwniles@gmail.com>
2015-09-03 11:35:31 +0100

    refactored credit note route

--- server/controllers/journal/sale.js
+++ server/controllers/journal/sale.js
@@ -6,11 +6,140 @@ var q = require('q'),
     util = require('../../lib/util'),
     db = require('../../lib/db');
 
+exports.create = create;
+exports.creditNote = creditNote;
+
+/*
+ * Create a credit note for a patient
+ *
+ *
+ */
+function creditNote(id, userId, cb) {
+  var sql, data, reference, cfg = {}, queries = {};
+
+  sql =
+    'SELECT credit_note.project_id, project.enterprise_id, credit_note.cost, credit_note.debitor_uuid, note_date, credit_note.sale_uuid, ' +
+      'credit_note.description, inventory_uuid, quantity, sale_item.uuid as item_uuid, ' +
+      'transaction_price, debit, credit, service.profit_center_id ' +
+    'FROM credit_note JOIN sale JOIN service JOIN sale_item JOIN inventory JOIN inventory_unit JOIN project ' +
+      'ON credit_note.sale_uuid=sale.uuid AND ' +
+      'sale.service_id = service.id AND ' +
+      'sale_item.sale_uuid = sale.uuid AND ' +
+      'sale_item.inventory_uuid = inventory.uuid AND ' +
+      'project.id = credit_note.project_id AND ' +
+      'inventory.unit_id = inventory_unit.id ' +
+    'WHERE credit_note.uuid = ?;';
+
+  db.exec(sql, [id])
+  .then(function (results) {
+    if (results.length === 0) {
+      throw new Error('No credit note by the id: ' + id);
+    }
+
+    data = results;
+    reference = results[0];
+
+    return core.checks.validPeriod(reference.enterprise_id, reference.note_date);
+
+  })
+
+  // ensure a credit note has not already been assigned to this sale
+  // should return one value at most
+  .then(function () {
+    sql = 'SELECT uuid FROM credit_note WHERE sale_uuid = ?;';
+    return db.exec(sql, [reference.sale_uuid]);
+  })
+
+  .then(function (rows) {
+
+    // There should only be one sale here
+    if (rows.length > 1) {
+      throw new Error('This sale has already been reversed with a credit note');
+    }
+
+    // cost positive checks
+    var costPositive = data.every(function (row) { return validate.isPositive(row.cost); });
+    if (!costPositive) {
+      throw new Error('Negative cost detected for invoice id: ' + id);
+    }
+
+    // all checks have passed - prepare for writing to the journal.
+    return q([ core.queries.origin('credit_note'), core.queries.origin('sale'), core.queries.period(reference.note_date) ]);
+
+  })
+  .spread(function (originId, saleOrigin, periodObject) {
+    // we now have the origin!
+    // we now have the relevant period!
+
+    cfg.saleOrigin = saleOrigin;
+    cfg.originId = originId;
+    cfg.periodId = periodObject.id;
+    cfg.fiscalYearId = periodObject.fiscal_year_id;
+
+    return core.queries.transactionId(reference.project_id);
+  })
+  .then(function (transId) {
+    cfg.transId = transId;
+
+  sql =
+    'SELECT uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, doc_num, ' +
+      'description, account_id, debit, credit, debit_equiv, credit_equiv, deb_cred_type, currency_id, ' +
+      'deb_cred_uuid, inv_po_id, cost_ctrl_id, origin_id, user_id, cc_id, pc_id ' +
+    'FROM posting_journal ' +
+    'WHERE posting_journal.inv_po_id = ? AND posting_journal.origin_id = ? ' +
+    'UNION ' +
+    'SELECT uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, doc_num, ' +
+      'description, account_id, debit, credit, debit_equiv, credit_equiv,  deb_cred_type, currency_id, ' +
+      'deb_cred_uuid, inv_po_id, cost_ctrl_id, origin_id, user_id, cc_id, pc_id ' +
+    'FROM general_ledger ' +
+    'WHERE general_ledger.inv_po_id = ? AND general_ledger.origin_id = ? ';
+
+    return db.exec(sql, [reference.sale_uuid, cfg.saleOrigin, reference.sale_uuid, cfg.saleOrigin]);
+  })
+
+  .then(function (results) {
+    sql =
+        'INSERT INTO posting_journal ' +
+          '(uuid, project_id, fiscal_year_id, period_id, trans_id, trans_date, doc_num, ' +
+          'description, account_id, debit, credit, debit_equiv, credit_equiv, currency_id, ' +
+          'deb_cred_uuid, inv_po_id, cost_ctrl_id, origin_id, user_id, cc_id, pc_id) ' +
+        'VALUES (?);';
+
+
+    //  loop through and results to queries (reversing the
+    //  debits and credits).  Parameters passed via escaping.
+    return q.all(results.map(function (item) {
+      var params;
+
+      item.deb_cred_uuid = item.deb_cred_uuid || null;
+
+      params = [
+        uuid(), item.project_id, cfg.fiscalYearId, cfg.periodId, cfg.transId, new Date(),
+        item.doc_num, reference.description, item.account_id, item.credit, item.debit, item.credit_equiv,
+        item.debit_equiv, item.currency_id, item.deb_cred_uuid, item.inv_po_id, item.cost_ctrl_id, cfg.originId,
+        item.user_id, item.cc_id, item.pc_id
+      ];
+
+      // return a templated SQL query and fire it immediately
+      return db.exec(sql, [params]);
+    }));
+  })
+
+  // update the credit note to say it was posted (to the journal)
+  .then(function () {
+    sql = 'UPDATE credit_note SET posted = 1 WHERE uuid = ?;';
+    return db.exec(sql, [id]);
+  })
+  .then(function (rows) {
+    cb(null, rows);
+  })
+  .catch(cb)
+  .done();
+}
+
 // TODO
 // convert everything to using db.exec()
 // parameter parsing.
-
-
 function getSubsidy(id) {
   var sql =
     'SELECT sale_subsidy.value, debitor_group.account_id, subsidy.text, sale.uuid ' +
@@ -24,7 +153,7 @@ function getSubsidy(id) {
 
 
 // TODO Only has project ID passed from sale reference, need to look up enterprise ID
-module.exports = function (id, user_id, callback, caution) {
+function create(id, user_id, callback, caution) {
   'use strict';
 
   var sql, data, reference, cfg = {}, queries = {}, subsidyReferences = [];
@@ -219,4 +348,4 @@ module.exports = function (id, user_id, callback, caution) {
     callback(err);
   })
   .done();
-};
+}
