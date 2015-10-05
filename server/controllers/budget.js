106dbb0ce1c427ac15559a2ef22ee3634ab04b1d
Bruce Mbayo <mbayopanda@gmail.com>
2015-10-05 14:29:15 +0100

    Fixing callback hell

--- server/controllers/budget.js
+++ server/controllers/budget.js
@@ -16,13 +16,13 @@ module.exports = {
 };
 
 function uploadedFile(req, res, next) {
-	/*
-		* Objectif : handling the uploaded file and generate an array (csvArray) from the file
-	*/
-	var csvArray = [];
-	var file = req.files.file;
-	var fiscal_year_id = req.body.fiscal_year_id;
-	var period = JSON.parse(req.body.period);
+	/**
+	  * Objectif : handling the uploaded file and generate an array (csvArray) from the file
+	  */
+	var csvArray       = [],
+		file           = req.files.file,
+		fiscal_year_id = req.body.fiscal_year_id,
+		period         = JSON.parse(req.body.period);
 
 	if (file && file.path && file.name) {
 		var fileName = uploadDir + file.name.replace(/ /g, '_');
@@ -37,8 +37,10 @@ function uploadedFile(req, res, next) {
 					csvArray.push(data);
 				})
 				.on('end', function () {
-					createBudget(csvArray, fiscal_year_id, period);
-					res.status(200).send('uploaded');
+					createBudget(csvArray, fiscal_year_id, period)
+					.then(success(res))
+					.catch(error)
+					.done();
 				});
 			});
 		});
@@ -47,55 +49,62 @@ function uploadedFile(req, res, next) {
 
 function createBudget(csvArray, fiscal_year_id, period) {
 
-	/*
-		* Objectif : create a new budget via params given
-		* Params : 
-		*	- csvArray : an array of the budget csv file
-		* - fiscal_year_id : the fiscal year id
-		* - period : the period
-	*/
+	/**
+	  * Objectif : create a new budget via params given
+	  * Params : 
+	  *	- csvArray : an array of the budget csv file
+	  * - fiscal_year_id : the fiscal year id
+	  * - period : the period
+	  */
 
-	if (csvArray.length) {
+	if (!csvArray.length) { return q.reject('Error with `CSV ARRAY`'); }
 
-		if (period.period_number === 0) {
-			// Budget for the fiscal year
-			var queryPeriods = 'SELECT period.id, period.period_number FROM period WHERE period.fiscal_year_id = ? ';
-			db.exec(queryPeriods, [fiscal_year_id])
-			.then(function (data) {
-				var periodIds = data;
-				var periodLength = periodIds.length - 1 > 0 ? periodIds.length - 1 : 1;
-				periodIds.forEach(function (item) {
-					if (item.period_number !== 0) {
-						csvArray.forEach(function (acc) {
-							// Hack for performance : Number(acc.Budget) !== 0 skip default budget=0
-							// If we choose to set a budget to zero in csv file, it's will be impossible to import it
-							if (acc.Type !== 'title' && Number(acc.Budget) !== 0 && !isNaN(Number(acc.Budget))) {
-								var sql = 'INSERT INTO `budget` (account_id, period_id, budget) VALUES (?, ?, ?) ;';
-								var value = [acc.AccountId, item.id, acc.Budget/periodLength];
-								clearPreviousBudgets(item.id)
-								.then(function () {
-									db.exec(sql, value);
-								});
-							}
-						});
-					}
-				});
-			});
+	var queryInsertBudget = 'INSERT INTO `budget` (account_id, period_id, budget) VALUES (?, ?, ?) ;';
 
-		} else if (period.period_number >= 1 && period.period_number <= 12) {
-			// Budget for a particular period in fiscal year
-			csvArray.forEach(function (item) {
-				if (item.Type !== 'title' && Number(item.Budget) !== 0 && !isNaN(Number(item.Budget))) {
-					var sql = 'INSERT INTO `budget` (account_id, period_id, budget) VALUES (?, ?, ?) ;';
-					var value = [item.AccountId, period.id, item.Budget];
-					clearPreviousBudgets(period.id)
-					.then(function () {
-						db.exec(sql, value);
-					});
-				}
-			});
+	if (period.period_number === 0) {
+		// Budget for the fiscal year
+		var queryPeriods = 'SELECT period.id, period.period_number FROM period WHERE period.fiscal_year_id = ? ';
+		db.exec(queryPeriods, [fiscal_year_id])
+		.then(createAnnualBudget)
+		.catch(error);
 
-		} 
+	} else if (period.period_number >= 1 && period.period_number <= 12) {
+		// Budget for a particular period in fiscal year
+		handleCSVArray(period.id);
+	}
+
+	function createAnnualBudget (periods) {
+		/**
+		  * This function is responsible to create an annual budget
+		  * the `periodLength` variable bellow is used in division of budget by period 
+		  */
+		var periodIds    = periods;
+		var periodLength = periodIds.length - 1 > 0 ? periodIds.length - 1 : 1;
+		periodIds.forEach(function (item) {
+			if (item.period_number !== 0) {
+				handleCSVArray(item.id, periodLength);
+			}
+		});
+	}
+
+	function handleCSVArray(period_id, periodLength) {
+		/**
+		  * This function is responsible to get budget from csvArray
+		  * and insert these data into budget table in database 
+		  * the `periodLength` variable is used in divison of budget by period
+		  */
+		periodLength = (periodLength && periodLength > 0) ? periodLength : 1;
+		csvArray.forEach(function (acc) {
+			// Hack for performance : Number(acc.Budget) !== 0 skip default budget=0
+			// If we choose to set a budget to zero in csv file, it's will be impossible to import it
+			if (acc.Type !== 'title' && Number(acc.Budget) !== 0 && !isNaN(Number(acc.Budget))) {
+				clearPreviousBudgets(period_id)
+				.then(function () {
+					db.exec(queryInsertBudget, [acc.AccountId, period_id, acc.Budget/periodLength]);
+				})
+				.catch(error);
+			}
+		});
 	}
 
 	function clearPreviousBudgets(period_id) {
@@ -104,22 +113,30 @@ function createBudget(csvArray, fiscal_year_id, period) {
 		var sql = 'DELETE FROM budget WHERE period_id = ? ;';
 		return db.exec(sql, [period_id]);
 	}
+
+	// this function must return a promise to handle errors
+	// and for sending success status to client
+	return q(true);
 }
 
 function updateBudget(req, res, next) {
 	var budgets = req.body.params.budgets;
 	var accountId = req.body.params.accountId;
-	var dbPromises = [];
-
-	budgets.forEach(function (bud) {
-		var sql = 'UPDATE budget SET budget.account_id=?, budget.period_id=?, budget.budget=? WHERE budget.id=? ;';
-		var value = [accountId, bud.period_id, bud.budget, bud.id];
-		dbPromises.push(db.exec(sql, value));
+	var queryUpdateBudget = 'UPDATE budget SET budget.account_id=?, budget.period_id=?, budget.budget=? WHERE budget.id=? ;';
+	var dbPromises = budgets.map(function (bud) {
+		return db.exec(queryUpdateBudget, [accountId, bud.period_id, bud.budget, bud.id]);
 	});
 
 	q.all(dbPromises)
-	.then(function () {
-		res.status(200).send('update');
-	});
+	.then(success(res))
+	.catch(error)
+	.done();
+}
 
+function success(res) {
+	res.status(200).send('success');
+}
+
+function error(err) {
+	console.error('[Error] ', err);
 }
