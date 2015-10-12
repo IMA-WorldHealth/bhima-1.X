c350ac5d02c32339a667458d4200a53789195d48
Bruce Mbayo <mbayopanda@gmail.com>
2015-10-12 14:14:43 +0100

    Fixing cost and profit center
    Using Controller aliases in cost and profit center
    
    Aliasing in cost center menu
    
    Using aliasing and loader indicator for Cost Center
    
    Enhencement of cost and profit center

--- client/src/js/app.js
+++ client/src/js/app.js
@@ -279,19 +279,19 @@ function bhimaconfig($routeProvider) {
     templateUrl: 'partials/cash_discard/cash_discard.html'
   })
   .when('/cost_center/', {
-    controller: 'costCenter',
+    controller: 'CostCenterController as CenterCtrl',
     templateUrl: 'partials/cost_center/cost_center.html'
   })
   .when('/profit_center/', {
-    controller: 'profitCenter',
+    controller: 'ProfitCenterController as CenterCtrl',
     templateUrl: 'partials/profit_center/profit_center.html'
   })
   .when('/profit_center/center/', {
-    controller: 'profitCenterAnalyse',
+    controller: 'AnalysisProfitCenterController as CenterCtrl',
     templateUrl: 'partials/profit_center/center/analysis_profit_center.html'
   })
   .when('/cost_center/center/', {
-    controller: 'analysisCenter',
+    controller: 'AnalysisCostCenterController as CenterCtrl',
     templateUrl: 'partials/cost_center/center/analysis_center.html'
   })
   .when('/cost_center/assigning/', {
@@ -299,11 +299,11 @@ function bhimaconfig($routeProvider) {
     templateUrl: 'partials/cost_center/assigning/assigning.html'
   })
   .when('/cost_center/allocation/', {
-    controller: 'costCenter.allocation',
+    controller: 'CostCenterAllocationController as CenterCtrl',
     templateUrl: 'partials/cost_center/allocation/allocation.html'
   })
   .when('/profit_center/allocation/', {
-    controller: 'profitCenter.allocation',
+    controller: 'ProfitCenterAllocationController as ProfitCtrl',
     templateUrl: 'partials/profit_center/allocation/allocation.html'
   })
   .when('/section_bilan/', {
@@ -750,7 +750,7 @@ function bhimaconfig($routeProvider) {
   .when('/justifyModal/', {
     controller : 'justifyModal',
     templateUrl : 'partials/cash/justify_modal.html'
-  })        
+  })
   .when('/dashboards/finance', {
     templateUrl : 'partials/dashboard/finance.html'
   })
@@ -769,7 +769,7 @@ function translateConfig($translateProvider) {
   $translateProvider.preferredLanguage('fr');
 }
 
-function localeConfig(tmhDynamicLocaleProvider) { 
+function localeConfig(tmhDynamicLocaleProvider) {
 
   // TODO Hardcoded default translation/ localisation
   tmhDynamicLocaleProvider.localeLocationPattern('/i18n/locale/angular-locale_{{locale}}.js');
