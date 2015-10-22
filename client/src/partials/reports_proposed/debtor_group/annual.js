7429a74a50f0a722ff72724a47e4754a5c692c75
Jonathan Niles <jonathanwniles@gmail.com>
2015-10-22 09:41:25 +0100

    FEEDBACK(report): Implements feedback on report
    This commit solves some issues enumerated below:
     1. remove extraneous console.log()s.
     2. we now use the report configuration service on the client.
     3. report configuration service updated to conform to the Angular Styleguide
     4. translations added for en/fr
     5. removed excess files added by accident.
     6. translations on the client improved.
    
    
    Former-commit-id: e1c5d5fc4059dbab3830e1c0ed5ac2a312485c90 [formerly 126ccc2a8295a8661c1585b83af504748f6014c4]
    Former-commit-id: 1163cc653df520b1d9eb26ad792e22ee08198738

--- client/src/partials/reports_proposed/debtor_group/annual.js
+++ client/src/partials/reports_proposed/debtor_group/annual.js
@@ -1,7 +1,7 @@
 angular.module('bhima.controllers')
 .controller('DebtorGroupAnnualReportController', DebtorGroupAnnualReportController);
 
-DebtorGroupAnnualReportController.$inject = [ '$http', '$sce' ];
+DebtorGroupAnnualReportController.$inject = [ '$http', 'reportConfigService'];
 
 /**
 * Debtor Group Annual PDF Report
@@ -9,28 +9,15 @@ DebtorGroupAnnualReportController.$inject = [ '$http', '$sce' ];
 * This report provides an overview of all debits and credits accrued by each
 * debtor group over the selected fiscal year.  This should only be done for
 * fiscal years that are closed, and therefore have closing balances posted.
-*
-* DEV NOTE
-*   This controller is largely based of the Balance Sheet PDF report, with a
-* slight code refresh (controller as syntax, etc).  If there are critical bugs
-* or updates to be made there, it is likely the case that they need to be
-* changed here as well.
 */
-function DebtorGroupAnnualReportController($http, $sce) {
+function DebtorGroupAnnualReportController($http, ReportConfigService) {
   var vm = this;
 
   vm.state = 'fresh';
 
   // TODO -- is this defined in a central location somewhere?  Maybe a service?
   // Configuration objects optionally passed to /report/build - drives configuration UI
-  vm.configuration = {
-    language : {
-      options : [
-        {value : 'en', label : 'English'},
-        {value : 'fr', label : 'French'}
-      ]
-    }
-  };
+  vm.configuration = ReportConfigService.configuration;
 
   vm.setLanguage = setLanguage;
   vm.generateDocument = generateDocument;
@@ -86,19 +73,6 @@ function DebtorGroupAnnualReportController($http, $sce) {
     .catch(handler)
     .finally(function () { vm.state = 'done'; });
   }
-
-  // Utility method - GET PDF blob displaying embedded object
-  function downloadDocument(url) {
-    $http.get(url, { responseType : 'arraybuffer' })
-    .then(function (response) {
-      var file = new Blob([response.data], {type: 'application/pdf'});
-      var fileURL = URL.createObjectURL(file);
-
-      // Expose document to scope
-      vm.pdfContent = $sce.trustAsResourceUrl(fileURL);
-    });
-  }
-
   function clearPath() {
     vm.generatedDocumentPath = null;
   }
