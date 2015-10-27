61a7bb75ce7bedb84d64a3461d2176cd424fd2b5
Jonathan Niles <jonathanwniles@gmail.com>
2015-10-27 11:40:01 +0100

    FIX(depot loss): purchase unit_price for valuation
    This commit allows the inventory lookup to lookup the original unit
    price of the stock item from the purchase table.  The stock is therefore
    valued throughout the enterprise according to the initial purchase
    price.

--- server/controllers/depot.js
+++ server/controllers/depot.js
@@ -272,11 +272,11 @@ function getAvailableLots(req, res, next) {
       depot = req.params.depotId;
 
   sql =
-    'SELECT s.tracking_number, s.lot_number, s.quantity, s.code, s.expiration_date, s.label FROM (' +
-      'SELECT stock.tracking_number, stock.lot_number, outflow.depot_entry, outflow.depot_exit, ' +
+    'SELECT s.unit_price, s.tracking_number, s.lot_number, s.quantity, s.code, s.expiration_date, s.label FROM (' +
+      'SELECT purchase_item.unit_price, stock.tracking_number, stock.lot_number, outflow.depot_entry, outflow.depot_exit, ' +
         'SUM(CASE WHEN outflow.depot_entry = ? THEN outflow.quantity ELSE -outflow.quantity END) AS quantity, ' +
         'stock.expiration_date, inventory.code, inventory.text AS label ' +
-      'FROM inventory JOIN stock JOIN (' +
+      'FROM inventory JOIN stock JOIN purchase JOIN purchase_item JOIN (' +
         'SELECT uuid, depot_entry, depot_exit, tracking_number, quantity, date ' +
         'FROM movement ' +
         'UNION ' +
@@ -285,10 +285,12 @@ function getAvailableLots(req, res, next) {
         'WHERE consumption.canceled = 0' +
       ') AS outflow ON ' +
         'inventory.uuid = stock.inventory_uuid AND ' +
-        'stock.tracking_number = outflow.tracking_number ' +
+        'stock.tracking_number = outflow.tracking_number AND ' +
+        'purchase.uuid = stock.purchase_order_uuid AND ' +
+        'purchase_item.purchase_uuid = purchase.uuid ' +
       'WHERE outflow.depot_entry = ? OR outflow.depot_exit = ? ' +
       'GROUP BY stock.tracking_number' +
-    ') AS s ' + 
+    ') AS s ' +
     'WHERE s.quantity > 0;';
 
   return db.exec(sql, [depot, depot, depot])
