6e2b5d7d43effc76c81a365fd4be0e1ecc904db7
Bruce Mbayo <mbayopanda@gmail.com>
2015-10-28 13:29:14 +0100

    Fixing Global Transactions Module

--- client/src/partials/reports/global_transaction/global_transaction.js
+++ client/src/partials/reports/global_transaction/global_transaction.js
@@ -1,211 +1,202 @@
 angular.module('bhima.controllers')
-.controller('report.global_transaction', [
-  '$scope',
-  'connect',
-  'appstate',
-  '$translate',
-  'validate',
-  'util',
-  'exchange',
-  function ($scope, connect, appstate, $translate, validate, util, exchange) {
+.controller('ReportGlobalTransactionController', ReportGlobalTransactionController);
 
-    var session = $scope.session = {},
-        state = $scope.state = {},
-        dependencies = {}, 
-        map = {};
+ReportGlobalTransactionController.$inject = [
+  'connect', '$translate', 'validate', 'util', 'exchange', 'SessionService', 'exportFile'
+];
 
-    dependencies.accounts = {
-      required : true,
-      query : {
-        tables : {'account' : {columns : ['id', 'account_number', 'account_txt', 'account_type_id']}}
-      }
-    };
+/**
+  * Reprot Global Transaction Controller
+  * This controller is responsible for managing report of all transactions
+  */
+function ReportGlobalTransactionController (connect, $translate, validate, util, exchange, SessionService, exportFile) {
+  var vm = this,
+      session = vm.session = {},
+      state = vm.state = {},
+      dependencies = {},
+      map = {};
 
-    dependencies.exchange_rate = {
-      query : {
-        tables : {
-          'exchange_rate' : {
-            columns : ['id', 'foreign_currency_id', 'rate', 'date']
-          }
+  dependencies.accounts = {
+    required : true,
+    query : {
+      tables : {'account' : {columns : ['id', 'account_number', 'account_txt', 'account_type_id']}}
+    }
+  };
+
+  dependencies.exchange_rate = {
+    query : {
+      tables : {
+        'exchange_rate' : {
+          columns : ['id', 'foreign_currency_id', 'rate', 'date']
         }
       }
-    };
+    }
+  };
 
-    dependencies.currencies = {
-      query : {
-        tables : {
-          'currency' : {
-            columns : ['id', 'symbol']
-          }
+  dependencies.currencies = {
+    query : {
+      tables : {
+        'currency' : {
+          columns : ['id', 'symbol']
         }
       }
-    };
+    }
+  };
 
-    $scope.account = {};
-    $scope.model = {};
-    $scope.model.sources = [$translate.instant('SELECT.ALL'), $translate.instant('SELECT.POSTING_JOURNAL'), $translate.instant('SELECT.GENERAL_LEDGER')];
-    session.somDebit = 0;
+  vm.account          = {};
+  vm.model            = {};
+  vm.model.sources    = [$translate.instant('SELECT.ALL'), $translate.instant('SELECT.POSTING_JOURNAL'), $translate.instant('SELECT.GENERAL_LEDGER')];
+  session.somDebit    = 0;
+  session.somCredit   = 0;
+  session.loaderState = 'loading';
+
+  // Expose to the view
+  vm.formatAccount = formatAccount;
+  vm.generate      = generate;
+  vm.reconfigure   = reconfigure;
+  vm.printReport   = printReport;
+  vm.download      = download;
+
+  // Startup
+  startup();
+
+  // Functions
+  function startup() {
+    vm.enterprise = SessionService.enterprise;
+    dependencies.accounts.query.where = ['account.enterprise_id=' + vm.enterprise.id];
+    validate.process(dependencies)
+    .then(init);
+  }
+
+  function formatAccount(account) {
+    return [
+      account.account_number, account.account_txt
+    ].join(' -- ');
+  }
+
+  function init(model) {
+    session.dateFrom = new Date();
+    session.dateTo = new Date();
+    angular.extend(vm, model);
+
+    vm.accounts.data.forEach(function (account) {
+      account.account_number = String(account.account_number);
+
+      // Define formatted account string at load time as this will never have to change again
+      account.display = formatAccount(account);
+    });
+    vm.model.c = vm.enterprise.currency_id;
+    vm.model.account_id = 0;
+    vm.model.source_id = 0;
+
+    vm.exchange_rate.data.forEach(function (item) {
+      map[util.sqlDate(new Date())] = {c_id : item.foreign_currency_id, rate : item.rate};
+    });
+  }
+
+  function loadView() {
+    vm.state         = 'generate';
+    session.loaderState  = 'loading';
+    session.buttonLoader = false;
+  }
+
+  function getValue (obj, val, cVal) {
+    if (cVal === vm.model.c) { return val; }
+    return (obj.c_id === cVal)? 1 : (obj.rate) * val; //not good because it supporte only two currency, I will fix it very soon
+  }
+
+  function getTotal(items) {
+    var sCredit = 0,
+        sDebit = 0;
+
     session.somCredit = 0;
+    session.somDebit = 0;
 
-    function formatAccount(account) {
-      return [
-        account.account_number, account.account_txt
-      ].join(' -- ');
-    }
-
-    function init(model) {
-      session.dateFrom = new Date();
-      session.dateTo = new Date();
-      angular.extend($scope, model);
-
-      $scope.accounts.data.forEach(function (account) {
-        account.account_number = String(account.account_number);
-        
-        // Define formatted account string at load time as this will never have to change again
-        account.display = formatAccount(account);
-      });
-      $scope.model.c = $scope.enterprise.currency_id;
-      $scope.model.account_id = 0;
-      $scope.model.source_id = 0;
-
-      $scope.exchange_rate.data.forEach(function (item) {
-        map[util.sqlDate(new Date())] = {c_id : item.foreign_currency_id, rate : item.rate};
-      });
-    }
-
-    function fill() {
-      if (!$scope.enterprise || !$scope.exchange_rate) {return;}
-      var f = ($scope.model.account_id && $scope.model.account_id > 0) ? selective($scope.model.account_id) : all();
-    }
-
-    function selective() {
-      $scope.mode = 'selected';
-      var url = '/reports/allTransactions/?source=' + $scope.model.source_id +
-        '&enterprise_id=' + $scope.enterprise.id + 
-        '&account_id=' + $scope.model.account_id + 
-        '&datef=' + util.sqlDate(session.dateFrom) + 
-        '&datet=' + util.sqlDate(session.dateTo);
-
-      $scope.model.account_number = $scope.accounts.get($scope.model.account_id).account_number;
-      connect.fetch(url)
-      .then(function (res) {
-        res.map(function (item) {
-          if($scope.enterprise.currency_id !== item.currency_id){
-            item.debit *= exchange.rate(item.debit,item.currency_id,new Date());
-            item.credit *= exchange.rate(item.debit,item.currency_id,new Date()); 
-          }           
-        });
-        $scope.records = res;
-        getTotal(res);
-      });
-    }
-
-    function all() {
-      $scope.mode = 'all';
-      var url = '/reports/allTransactions/?source=' + $scope.model.source_id +
-        '&enterprise_id=' + $scope.enterprise.id + 
-        '&account_id=0' + 
-        '&datef=' + util.sqlDate(session.dateFrom) + 
-        '&datet=' + util.sqlDate(session.dateTo);
-
-      connect.fetch(url)
-      .then(function (res) {
-          res.map(function (item) {
-            if($scope.enterprise.currency_id !== item.currency_id){
-              item.debit *= exchange.rate(item.debit,item.currency_id,new Date());
-              item.credit *= exchange.rate(item.debit,item.currency_id,new Date()); 
-            }
-          });
-          $scope.records = res;
-          getTotal(res);
-        });
-    }
-
-    function getValue (obj, val, cVal) {
-      if (cVal === $scope.model.c) { return val; }
-      return (obj.c_id === cVal)? 1 : (obj.rate) * val; //not good because it supporte only two currency, I will fix it very soon
-    }
-
-    function search () {
-      var hasSelectedAccount = $scope.model.account_id && $scope.model.account_id > 0;
-      $scope.mode = (hasSelectedAccount) ? 'selected' : 'all';
-      
-      if (hasSelectedAccount) {
-        $scope.model.account_number = $scope.accounts.get($scope.model.account_id).account_number;       
+    items.forEach(function (item) {
+      if(vm.enterprise.currency_id !== item.currency_id){
+        sCredit += item.credit / exchange.rate(item.credit,item.currency_id,new Date());
+        sDebit += item.debit / exchange.rate(item.debit,item.currency_id,new Date());
       } else {
-        $scope.model.account_number = 0;
+        sCredit += item.credit;
+        sDebit += item.debit;
       }
-
-      var url = '/reports/allTransactions/?source=' + $scope.model.source_id +
-        '&enterprise_id=' + $scope.enterprise.id + 
-        '&account_id=' + $scope.model.account_id + 
-        '&datef=' + util.sqlDate(session.dateFrom) + 
-        '&datet=' + util.sqlDate(session.dateTo);
-
-      connect.fetch(url)
-      .then(function (res) {
-        res.map(function (item) {
-            if($scope.enterprise.currency_id !== item.currency_id){
-              item.debit *= exchange.rate(item.debit,item.currency_id,new Date());
-              item.credit *= exchange.rate(item.debit,item.currency_id,new Date()); 
-            }
-        });
-        $scope.records = res;
-        getTotal(res);
-      });
-    }
-
-    function getTotal(items) {
-      var sCredit = 0, 
-          sDebit = 0;
-
-      session.somCredit = 0;
-      session.somDebit = 0;
-
-      items.forEach(function (item) {
-        if($scope.enterprise.currency_id !== item.currency_id){
-          sCredit += item.credit / exchange.rate(item.credit,item.currency_id,new Date()); 
-          sDebit += item.debit / exchange.rate(item.debit,item.currency_id,new Date());
-        } else {
-          sCredit += item.credit;
-          sDebit += item.debit;
-        }                
-      });
-
-      if($scope.enterprise.currency_id === $scope.model.c){
-        session.somCredit = sCredit;
-        session.somDebit = sDebit;
-      } else {
-        session.somCredit = sCredit * exchange.rate(sCredit,$scope.model.c,new Date());
-        session.somDebit = sDebit * exchange.rate(sDebit,$scope.model.c,new Date());
-      }
-
-    }
-
-    appstate.register('enterprise', function (enterprise) {
-      $scope.enterprise = enterprise;
-      dependencies.accounts.query.where = ['account.enterprise_id='+enterprise.id];
-      validate.process(dependencies)
-      .then(init);
     });
 
-    function generate() {
-      fill();
-      $scope.state = 'generate';
+    if(vm.enterprise.currency_id === vm.model.c){
+      session.somCredit = sCredit;
+      session.somDebit = sDebit;
+    } else {
+      session.somCredit = sCredit * exchange.rate(sCredit,vm.model.c,new Date());
+      session.somDebit = sDebit * exchange.rate(sDebit,vm.model.c,new Date());
     }
 
-    function reconfigure() {
-      $scope.state = null;
-    }
+    // End loading indicator
+    session.loaderState = 'loaded';
 
-    function printReport() {
-      print();
-    }
-
-    $scope.formatAccount = formatAccount;
-    $scope.search = search;
-    $scope.generate = generate;
-    $scope.reconfigure = reconfigure;
-    $scope.printReport = printReport;
   }
-]);
+
+  function generate() {
+    session.buttonLoader = true;
+    if (!vm.enterprise || !vm.exchange_rate) {return;}
+    var accountFilter = (vm.model.account_id && vm.model.account_id > 0) ? vm.model.account_id : 0;
+    var url = '/reports/allTransactions/?source=' + vm.model.source_id +
+      '&enterprise_id=' + vm.enterprise.id +
+      '&account_id=' + accountFilter +
+      '&datef=' + util.sqlDate(session.dateFrom) +
+      '&datet=' + util.sqlDate(session.dateTo);
+
+    session.account_number = vm.model.account_id ? vm.accounts.get(vm.model.account_id).account_number : '';
+    connect.fetch(url)
+    .then(function (res) {
+      res.map(function (item) {
+        if(vm.enterprise.currency_id !== item.currency_id){
+          item.debit *= exchange.rate(item.debit,item.currency_id,new Date());
+          item.credit *= exchange.rate(item.debit,item.currency_id,new Date());
+        }
+      });
+      vm.records = res;
+      return res;
+    })
+    .then(getTotal)
+    .then(loadView)
+    .catch(handleError);
+  }
+
+  function reconfigure() {
+    vm.state = null;
+  }
+
+  function printReport() {
+    print();
+  }
+
+  function handleError(err) {
+    console.error(err);
+  }
+
+  function download() {
+    var fileData = {};
+    var fileName = $translate.instant('ALLTRANSACTIONS.TITLE');
+
+    fileData.column = [
+      $translate.instant('COLUMNS.TRANS_ID'),
+      $translate.instant('COLUMNS.TRANSACTION_DATE'),
+      $translate.instant('COLUMNS.ACCOUNT_NUMBER'),
+      $translate.instant('COLUMNS.DESCRIPTION'),
+      $translate.instant('COLUMNS.DEBIT'),
+      $translate.instant('COLUMNS.CREDIT')
+    ];
+    fileData.data = vm.records.map(function (item) {
+      return {
+        'trans_id'       : item.trans_id,
+        'trans_date'     : util.htmlDate(item.trans_date),
+        'account_number' : item.account_number,
+        'description'    : item.description,
+        'debit'          : item.debit,
+        'credit'         : item.credit
+      };
+    });
+    exportFile.csv(fileData, fileName, true);
+  }
+
+}
