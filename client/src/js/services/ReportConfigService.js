4df1607e0eddc8d88d1d2da7a7cf5bf5a75d1924
Jonathan Niles <jonathanwniles@gmail.com>
2015-10-28 11:33:45 +0100

    FIX(patient invoice): reimplement PDF facture
    Fixes #761.
    
    The commit reimplements the PDF patient invoice "proof of concept"
    document, removed sometime earlier in the project.  It significantly
    cleans up the interface for creating the report and provides
    translations in the current (modern) format.

--- client/src/js/services/ReportConfigService.js
+++ client/src/js/services/ReportConfigService.js
@@ -1,7 +1,7 @@
 angular.module('bhima.services')
 .service('reportConfigService', ReportConfigService);
 
-ReportConfigService.$inject = ['SessionService'];
+ReportConfigService.$inject = ['SessionService', '$translate'];
 
 /**
 * This service essentially used the pdf report exposes an object containing common data and common function
@@ -9,18 +9,31 @@ ReportConfigService.$inject = ['SessionService'];
 * @todo Make this service much more useful with getter/setter methods for lang,
 * layout, and more.  Not sure if this really needs SessionService.
 */
-function ReportConfigService(sessionService) {
+function ReportConfigService(sessionService, $translate) {
   var service = this;
 
+  // TODO/FIXME -- translations
   service.configuration = {
-      language : {
-        options : [
-          {value : 'en', label : 'English'},
-          {value : 'fr', label : 'French'}
-        ]
-      },
-      enterprise : sessionService.enterprise,
-      project : sessionService.project
+    currency : {
+      options : [
+        { value : 'dollars', label : 'Dollars', key : 'UTIL.DOLLARS' },
+        { value : 'francs', label : 'Francs', key : 'UTIL.FRANCS' }
+      ]
+    },
+    format : {
+      options : [ 
+        { value : 'compact', label : 'Compact' },
+        { value : 'standard', label : 'Standard' }
+      ]
+    },
+    language : {
+      options : [
+        { value : 'en', label : 'English', key : 'LANGUAGES.ENGLISH' },
+        { value : 'fr', label : 'French', key : 'LANGUAGES.FRENCH' }
+      ]
+    },
+    enterprise : sessionService.enterprise,
+    project : sessionService.project
   };
 
   return service;
