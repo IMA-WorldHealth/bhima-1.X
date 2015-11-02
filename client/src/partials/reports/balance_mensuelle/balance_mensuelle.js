cd1804ce7a9acf182ea7eeb921b76460892d186f
Jonathan Niles <jonathanwniles@gmail.com>
2015-11-02 14:49:48 +0100

    FIX(balance mensuelle): Fixes report summation bug
    This commit restructures the balance_mensuelle report to pull data from
    only the period totals table, rather than the combined posting_journal
    and general_ledger.  This change ensures that we are dealing with only
    general_ledger data.

--- client/src/partials/reports/balance_mensuelle/balance_mensuelle.js
+++ client/src/partials/reports/balance_mensuelle/balance_mensuelle.js
@@ -2,16 +2,18 @@ angular.module('bhima.controllers')
 .controller('ReportBalanceMensuelleController', ReportBalanceMensuelleController);
 
 ReportBalanceMensuelleController.$inject = [
-  '$translate', '$window', 'validate', 'util', 'messenger', 'exportFile', 'SessionService'
+  '$translate', '$window', '$http', 'messenger', 'exportFile', 'SessionService',
+  'DateService'
 ];
 
 /**
 * Report Balance Mensuelle Controller
 * This controller is responsible for managing balance menselle data
 */
-function ReportBalanceMensuelleController ($translate, $window, validate, util, messenger, exportFile, Session) {
+function ReportBalanceMensuelleController($translate, $window, $http, messenger, exportFile, Session, Dates) {
   var vm = this,
-      dependencies = {}, session = vm.session = {},
+      dependencies = {},
+      session = vm.session = {},
       state = vm.state;
 
   session.classes = [
@@ -26,10 +28,12 @@ function ReportBalanceMensuelleController ($translate, $window, validate, util, 
     { number : '*', name : $translate.instant('ACCOUNT.ALL_ACCOUNT')}
   ];
 
+
   // Expose to the view
+  vm.loading = false;
   vm.download = download;
   vm.reconfigure = reconfigure;
-  vm.getAccountBalance = getAccountBalance;
+  vm.submit = submit;
   vm.formatAccount = formatAccount;
   vm.print = printer;
 
@@ -46,8 +50,6 @@ function ReportBalanceMensuelleController ($translate, $window, validate, util, 
     angular.extend(vm, model);
     var balance = model.balance_mensuelle.data,
         newSoldeDebit, newSoldeCredit;
-
-    console.log('balance:', balance);
 
     balance.forEach(function (balances) {
       newSoldeDebit = balances.old_debit + balances.debit;
@@ -90,29 +92,98 @@ function ReportBalanceMensuelleController ($translate, $window, validate, util, 
     });
   }
 
-  function getAccountBalance() {
+  // actually submits the form
+  function submit(invalid) {
+    var url;
+
+    if (invalid) { return; }
+
     vm.state = 'generate';
-    if (session.classe && session.periode) {
+    vm.loading = true;
 
-      var params = {
-        project : session.project,
-        classe  : session.classe.number,
-        periode : util.sqlDate(session.periode)
-      };
+    // empty data caches
+    vm.accounts = {};
+    vm.totals = {};
 
-      var rootReport = 'balance_mensuelle';
+    url = '/reports/balance_mensuelle?' +
+        'enterpriseId=' + Session.enterprise.id +
+        '&classe=' + session.classe.number +
+        '&date=' + Dates.util.str(session.periode);
 
-      if (session.classe.number === '*') {
-        rootReport = 'balance_mensuelle_all';
-      }
+    // get the data
+    $http.get(url)
+    .then(function (response) {
+      var accounts,
+          totals,
+          data = response.data;
 
-      dependencies.balance_mensuelle = { query : 'reports/' + rootReport + '/?' + JSON.stringify(params) };
-      validate.process(dependencies, ['balance_mensuelle'])
-      .then(initialize)
-      .then(calculTotaux);
-    } else {
-      messenger.info($translate.instant('BALANCE_MENSUELLE.INFO_FILL_INPUT'), true);
-    }
+      // make the accounts object
+      accounts = data.beginning.reduce(function (accounts, row) {
+        var account,
+            id = row.account_number;
+
+        if (!accounts[id]) { accounts[id] = {}; }
+        account = accounts[id];
+        account.beginDebit = row.debit;
+        account.beginCredit = row.credit;
+        account.account_txt = row.account_txt;
+        return accounts;
+      }, {});
+
+      // add the middle references
+      data.middle.forEach(function (row) {
+        var account,
+            id = row.account_number;
+
+        if (!accounts[id]) { accounts[id] = {}; }
+        account = accounts[id];
+        account.middleDebit = row.debit;
+        account.middleCredit = row.credit;
+        account.account_txt = row.account_txt;
+      });
+
+      // add the final balances
+      data.end.forEach(function (row) {
+        var account,
+            id = row.account_number;
+
+        if (!accounts[id]) { accounts[id] = {}; }
+        account = accounts[id];
+        account.endDebit = row.debit;
+        account.endCredit = row.credit;
+        account.account_txt = row.account_txt;
+      });
+
+      // calculate totals
+      totals = Object.keys(accounts).reduce(function (totals, key) {
+        var account = accounts[key];
+        totals.beginDebit += (account.beginDebit || 0);
+        totals.beginCredit += (account.beginCredit || 0);
+        totals.middleDebit += (account.middleDebit || 0);
+        totals.middleCredit += (account.middleCredit || 0);
+        totals.endDebit += (account.endDebit || 0);
+        totals.endCredit += (account.endCredit || 0);
+        return totals;
+      }, {
+        beginDebit : 0,
+        beginCredit : 0,
+        middleDebit: 0,
+        middleCredit : 0,
+        endDebit : 0,
+        endCredit : 0
+      });
+
+      // expose to view
+      vm.accounts = accounts;
+      vm.totals = totals;
+      vm.empty = Object.keys(accounts).length === 0;
+    })
+    .catch(function (error) {
+      throw error;
+    })
+    .finally(function () {
+      vm.loading = false;
+    });
   }
 
   function formatAccount(classe) {
@@ -157,7 +228,7 @@ function ReportBalanceMensuelleController ($translate, $window, validate, util, 
 
   function download() {
     var fileData = {};
-    var metadata = util.htmlDate(session.periode) + '_' + session.classe.number + '(' + session.classe.name + ')';
+    var metadata = Dates.util.str(session.periode) + '_' + session.classe.number + '(' + session.classe.name + ')';
     var fileName = $translate.instant('BALANCE_MENSUELLE.TITLE') +
                   '_' + metadata;
 
@@ -171,6 +242,7 @@ function ReportBalanceMensuelleController ($translate, $window, validate, util, 
       $translate.instant('BALANCE_MENSUELLE.NEW_SOLD') + ' ' + $translate.instant('BALANCE_MENSUELLE.DEBITOR'),
       $translate.instant('BALANCE_MENSUELLE.NEW_SOLD') + ' ' + $translate.instant('BALANCE_MENSUELLE.CREDITOR')
     ];
+
     fileData.data = vm.balance_mensuelle.data.map(function (item) {
       return {
         'account_number' : item.account_number,
@@ -183,6 +255,7 @@ function ReportBalanceMensuelleController ($translate, $window, validate, util, 
         'solde_credit'   : item.solde_credit
       };
     });
+
     exportFile.csv(fileData, fileName, false);
   }
 
