d785f898dab0afe38a0054daa8ffb86016d35f41
Bruce Mbayo <mbayopanda@gmail.com>
2015-11-16 17:13:34 +0100

    Payroll at tshikaji

--- server/models/update/synt.sql
+++ server/models/update/synt.sql
@@ -4,3 +4,10 @@
 
 INSERT INTO `unit` (`id`, `name`, `key`, `parent`, `url`, `path`) VALUES
 (134, "Patient Visit Status", "TREE.PATIENT_VISIT_STATUS", 127, "/partials/reports/patient_visit_status/", "/reports/patient_visit_status/");
+
+-- IPR integer unsigned
+-- By: Bruce M.
+-- Date: 2015-11-15
+
+ALTER TABLE `taxe_ipr` CHANGE `tranche_annuelle_debut` `tranche_annuelle_debut` INT UNSIGNED NULL DEFAULT NULL;
+ALTER TABLE `taxe_ipr` CHANGE `tranche_annuelle_fin` `tranche_annuelle_fin` INT UNSIGNED NULL DEFAULT NULL;
