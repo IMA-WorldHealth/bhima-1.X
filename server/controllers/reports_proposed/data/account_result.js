328569e282f25cbf0551fbb485c2ec64bd1b6173
DedrickEnc <kitamuka@gmail.com>
2016-03-25 23:37:01 +0100

    finish the charge side , cleaning remains

--- server/controllers/reports_proposed/data/account_result.js
+++ server/controllers/reports_proposed/data/account_result.js
@@ -18,7 +18,7 @@ exports.compile = function (options) {
     '`src`.`position` AS `sectionResultPosition`, SUM(`gld`.`debit_equiv`) AS `generalLegderDebit`, SUM(`gld`.`credit_equiv`) AS `generalLegderCredit` ' +
     'FROM `section_resultat` `src` JOIN `reference` `ref` ON `ref`.`section_resultat_id` = `src`.`id` ' +
     'JOIN `account` `acc` ON `acc`.`reference_id` = `ref`.`id` JOIN `general_ledger` `gld` ON `gld`.`account_id` = `acc`.`id` WHERE `gld`.`period_id` IN (SELECT `id` ' +
-    'FROM `period` WHERE `period`.`fiscal_year_id`=?) AND `acc`.`is_ohada`=? AND `acc`.`account_type_id`=? GROUP BY `gld`.`account_id` ORDER BY `src`.`position`, `ref`.`position` ASC;';
+    'FROM `period` WHERE `period`.`fiscal_year_id`=?) AND `acc`.`is_ohada`=? AND `acc`.`account_type_id`=? GROUP BY `gld`.`account_id`, `src`.`id` ORDER BY `src`.`position`, `ref`.`position` ASC;';
 
   var doBalance = function (somDebit, somCredit, isCharge){
     return (isCharge == 1)? somDebit - somCredit : somCredit - somDebit;
@@ -38,12 +38,15 @@ exports.compile = function (options) {
     return db.exec(sql, [options.fy, 1, 1]);
   })  
   .then(function (currentAccountDetails) {
+    var t = currentAccountDetails.filter(function (it){return it.sectionResultId == 3;});
+    console.log(t);
+    
     infos.currentAccountDetails = currentAccountDetails;
     return q.all(options.parentIds.map(function (fid){
       return db.exec(sql, [fid, 1, 1]);
     }));
   })
-  .then(function (ans){
+  .then(function (ans){    
 
     ans = ans.map(function (items){
 
@@ -74,10 +77,76 @@ exports.compile = function (options) {
       return item.sectionResultIsCharge === 0;
     });
 
-    context.chargeSide = processComplement(processCharge(chargeData));
-    context.profitSide = processComplement(processProduit(produitData));
+    context.chargeSide = processCharge(chargeData);
+    context.profitSide = processProduit(produitData);
 
-    function processComplement (data){      
+    context.chargeSide = processComplement(context.chargeSide, context.profitSide, 1);    
+    context.profitSide = processComplement(context.chargeSide, context.profitSide, 0);
+
+
+
+    //add the result here
+    // *** your code ***
+
+    //this function receive a list ref section and send back there total, attr is the attr to sum
+    function getTotalMetadataList (sections, list, isCharge, attr){
+      var references = [];
+
+      sections.forEach(function (section){
+        var result = section.refs.filter(function (item){
+          return list.some(function (elm){
+            return elm.ref === item.referenceAbbr;
+          });          
+        });
+
+        references = references.concat(result);
+      });
+
+      var sum = references.reduce(function (x, y){
+        return x + y[attr];
+
+      }, 0);
+
+      return sum;
+    }
+
+    function forceTotalProcessing (dataCharge, dataProfit, isCharge){
+      var ref = isCharge === 1 ? 'ST' : 'UT';
+
+      var reference = infos.references.filter(function (item){
+        return item.ref === ref;
+      })[0];
+
+      var newItem = {
+        referenceId: reference.id,
+        referenceAbbr: reference.ref,
+        referencePosition : reference.position,
+        referenceLabel: reference.text,
+      };
+
+      var referenceMeta = null;
+
+      referenceMeta = getReferenceMeta(reference.ref); 
+
+      if(!referenceMeta) {return {};}
+
+      newItem.net = isCharge === 1 ? getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'net') : getTotalMetadataList(dataProfit, referenceMeta.produits, isCharge, 'net');
+      newItem.net_view = numeral(newItem.net).format(formatDollar);
+
+      infos.previous.forEach(function (previousYearData, index){
+        newItem['previous' + index] = isCharge === 1 ? 
+        getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'previous'+index) :
+        getTotalMetadataList(dataProfit, referenceMeta.produits, isCharge, 'previous'+index);               
+
+        newItem['previous_view' + index] = numeral(newItem['previous' + index]).format(formatDollar);
+      });
+
+      return newItem;
+    }
+
+    function processComplement (dataCharge, dataProfit, isCharge){   
+
+      var data = isCharge === 1 ? dataCharge : dataProfit;   
 
       data.forEach(function (section){
 
@@ -91,6 +160,8 @@ exports.compile = function (options) {
           return item.section_resultat_id === section.sectionResultId && refIds.indexOf(item.id) === -1;
         });
 
+        // var VAL = 0; //valeur ajouter
+
         //setting attribute to the property
         var formatedRefs = missedRefs.map(function (item){
 
@@ -99,8 +170,6 @@ exports.compile = function (options) {
             referenceAbbr: item.ref,
             referencePosition : item.position,
             referenceLabel: item.text,
-            net : 0,
-            net_view : numeral(item.net).format(formatDollar)
           };
 
           var referenceMeta = null;
@@ -110,21 +179,77 @@ exports.compile = function (options) {
 
           if(referenceMeta){
 
-            infos.previous.forEach(function (previousYearData, index){
-              newItem['previous' + index] = 0;
-              newItem['previous_view' + index] = numeral(newItem['previous' + index]).format(formatDollar);
-            });
+            if(referenceMeta.type === 'MAR' || referenceMeta.type === 'CHI' || referenceMeta.type === 'INF' || referenceMeta.type === 'VAL' || referenceMeta.type === 'EXC'){
 
-          }else{
+              newItem.net = getTotalMetadataList(dataProfit, referenceMeta.produits, isCharge, 'net') - 
+                            getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'net');
+
+              newItem.net_view = numeral(newItem.net).format(formatDollar);
+
+              infos.previous.forEach(function (previousYearData, index){
+                newItem['previous' + index] = 
+                getTotalMetadataList(dataProfit, referenceMeta.produits, isCharge, 'previous'+index) -                
+                getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'previous'+index);
+
+                newItem['previous_view' + index] = numeral(newItem['previous' + index]).format(formatDollar);
+              });
+            }
+            // else if(referenceMeta.type === 'VAL'){
+            //   //we will need to save it
+            //   newItem.net = getTotalMetadataList(dataProfit, referenceMeta.produits, 0, 'net') - 
+            //                 getTotalMetadataList(dataCharge, referenceMeta.charges, 1, 'net');
+
+            //   newItem.net_view = numeral(newItem.net).format(formatDollar);
+
+            //   infos.previous.forEach(function (previousYearData, index){
+            //     newItem['previous' + index] = 
+            //     getTotalMetadataList(dataProfit, referenceMeta.produits, isCharge, 'previous'+index) -                
+            //     getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'previous'+index);
+
+            //     newItem['previous_view' + index] = numeral(newItem['previous' + index]).format(formatDollar);
+            //   });
+
+            //   VAL = newItem;
+            // }
+            // else if (referenceMeta.type === 'EXC'){
+            //  newItem.net = VAL.net - getTotalMetadataList(dataCharge, referenceMeta.charges, 1, 'net');
+
+            //   newItem.net_view = numeral(newItem.net).format(formatDollar);
+
+            //   infos.previous.forEach(function (previousYearData, index){
+            //     newItem['previous' + index] = 
+            //     VAL['previous'+index] - getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'previous'+index);
+            //     newItem['previous_view' + index] = numeral(newItem['previous' + index]).format(formatDollar);
+            //   });
+            // }
+            else if (referenceMeta.type === 'TOT'){
+
+              newItem.net = isCharge === 1 ? getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'net') : getTotalMetadataList(dataProfit, referenceMeta.produits, isCharge, 'net');
+
+              newItem.net_view = numeral(newItem.net).format(formatDollar);
+
+              infos.previous.forEach(function (previousYearData, index){
+                newItem['previous' + index] = isCharge === 1 ? 
+                getTotalMetadataList(dataCharge, referenceMeta.charges, isCharge, 'previous'+index) :
+                getTotalMetadataList(dataProfit, referenceMeta.produits, isCharge, 'previous'+index);               
+
+                newItem['previous_view' + index] = numeral(newItem['previous' + index]).format(formatDollar);
+              });
+            }
+          }
+          else {
+            newItem.net = 0;
+            newItem.net_view = numeral(newItem.net).format(formatDollar);
+
             //previous net processing
             infos.previous.forEach(function (previousYearData, index){
               newItem['previous' + index] = 0;
               newItem['previous_view' + index] = numeral(newItem['previous' + index]).format(formatDollar);
             });
-          }
+          }            
 
           return newItem;
-        });
+        });        
 
         // init to a empty array if formatedRefs is undefined
         formatedRefs = formatedRefs || [];
@@ -138,6 +263,9 @@ exports.compile = function (options) {
         });
       });
 
+      var itemTotal = forceTotalProcessing(dataCharge, dataProfit, isCharge); 
+      data[data.length - 1].refs.push(itemTotal);
+      
       return data;
     }
 
---
+++ server/models/update/refScript.sql
@@ -0,0 +1,35 @@
+--adding additional refs
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "RW", "Total des charges d'exploitation", 13, NULL, 1);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "SF", "Total des charges financieres", 4, NULL, 2);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "SH", "Total des charges des activites ordinaires", 5, NULL, 2);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "SO", "Total des charges H.A.O", 4, NULL, 3);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "SS", "Total partcipation et impots", 3, NULL, 4);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "ST", "TOTAL GENARAL DES CHARGES", 6, NULL, NULL);
+
+
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "TW", "Total des produits d'exploitation", 17, NULL, 9);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "TX", "RESULTAT D'EXPLOTATION", 18, NULL, 9);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "UF", "Total des produits financiers", 5, NULL, 10);
+
+UPDATE reference SET position = 6 WHERE ref="UG";
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "UH", "Total des produits des activites ordinaires", 7, NULL, 10);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "UI", "RESULAT ACTIVITES ORDINAIRES", 8, NULL, 10);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "UO", "Total des produits H.A.O", 11, NULL, 11);
+
+UPDATE reference SET position = 12 WHERE ref="UP";
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "UT", "TOTAL GENERAL DES PRODUITS", 13, NULL, 11);
+
+INSERT INTO reference (is_report, ref, text, position, reference_group_id, section_resultat_id) VALUES (0, "UZ", "RESULAT NET", 14, NULL, NULL);
