a7e09c6ecd6c3a9303522ad0c33d4f858b01ca69
Bruce Mbayo <mbayopanda@gmail.com>
2016-03-27 12:40:45 +0100

    New synt-thikaji.sql for Tshikaji development

---
+++ server/models/update/synt-tshikaji.sql
@@ -0,0 +1,7 @@
+-- Cash flow reports 
+-- By: Bruce M.
+-- Date: 2016-03-16
+
+INSERT INTO `unit` (`id`, `name`, `key`, `parent`, `url`, `path`) VALUES
+(140, "Cashflow Report", "TREE.CASH_FLOW", 128, "/partials/reports/cash_flow/", "/reports/cash_flow/");
+
