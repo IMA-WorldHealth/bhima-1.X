3f1520c86278d889141db38b1947ef68b421b5d2
Bruce Mbayo <mbayopanda@gmail.com>
2016-03-29 19:44:38 +0100

    debtor group debts only

--- client/src/partials/reports/customer_debt/customer_debt.js
+++ client/src/partials/reports/customer_debt/customer_debt.js
@@ -11,31 +11,15 @@ function CustomerDebtController ($q, $http, $location, validate, util, exchange,
       session = vm.session = {},
       dependencies = {};
 
-  dependencies.debtorGroups = {
-    required: true,
-    query : {
-      tables : {
-        'debitor_group' : { columns : ['uuid', 'name', 'account_id', 'locked', 'is_convention'] }
-      }
-    }
-  };
-
   /** variables definitions */
-  vm.untilDate = new Date();
+  vm.untilDate   = new Date();
 
   /** expose to the view */
-
-  vm.setSelectedGroup = setSelectedGroup;
+  vm.generate    = generate;
   vm.reconfigure = reconfigure;
-  vm.print = imprimer;
-  vm.goto  = goto;
-
-  /** startup the module */
-
-  startup();
+  vm.print       = imprimer;
 
   /** functions definitions */
-
   function startup() {
     validate.process(dependencies)
     .then(initialize);
@@ -43,125 +27,31 @@ function CustomerDebtController ($q, $http, $location, validate, util, exchange,
 
   function initialize(models) {
     angular.extend(vm, models);
-    getPeriodicDebts();
   }
+
+  function generate()  {
+    vm.state = 'generate';
+    getPeriodicDebts()
+    .then(getDebtorGroupList)
+    .then(totals)
+    .catch(error);  
+  }
+  
 
   function getPeriodicDebts() {
     vm.loading = true;
 
     var params = {
-      uuid : vm.selectedGroup ? vm.selectedGroup.uuid : '*',
       date : vm.withUntilDate ? util.htmlDate(vm.untilDate) : ''
     };
 
-    $http.get('/customer_debt/', { 
+    return $http.get('/customer_debt/', { 
       params : params 
-    })
-    .then(switcher)
-    .then(totals)
-    .catch(error);
-  }
-
-  /** switcher cases */
-  function switcher(rows) {
-    return (vm.allDebtorGroup) ? 
-      getDebtorGroupList(rows) : 
-      getDebtorList(rows);
-  }
-
-  /** debtor data */
-  function getDebtorList(rows) {
-    vm.loading = false;
-    vm.customerDebts = {
-      first  : rows.data.first,
-      second : rows.data.second,
-      third  : rows.data.third,
-      fourth : rows.data.fourth
-    };
-
-    session.debtorList = [];
-    var temporary = [];
-
-    /** first : less than 3 months */
-    vm.customerDebts.first.forEach(function (item) {
-      var line = {
-        uuid           : item.debitor_uuid,
-        group_uuid     : item.group_uuid,
-        text           : item.text,
-        reference      : item.abbr + item.reference,
-        balance_first  : item.balance,
-        balance_second : 0,
-        balance_third  : 0,
-        balance_fourth : 0,
-      };
-      temporary.push(line);
     });
-
-    /** second: between 3 and 6 months */
-    vm.customerDebts.second.forEach(function (item) {
-      var line = {
-        uuid           : item.debitor_uuid,
-        group_uuid     : item.group_uuid,
-        text           : item.text,
-        reference      : item.abbr + item.reference,
-        balance_first  : 0,
-        balance_second : item.balance,
-        balance_third  : 0,
-        balance_fourth : 0
-      };
-      temporary.push(line);
-    });
-
-    /** third: between 6 and 12 months */
-    vm.customerDebts.third.forEach(function (item) {
-      var line = {
-        uuid           : item.debitor_uuid,
-        group_uuid     : item.group_uuid,
-        text           : item.text,
-        reference      : item.abbr + item.reference,
-        balance_first  : 0,
-        balance_second : 0,
-        balance_third  : item.balance,
-        balance_fourth : 0
-      };
-      temporary.push(line);
-    });
-
-    /** third: between 6 and 12 months */
-    vm.customerDebts.fourth.forEach(function (item) {
-      var line = {
-        uuid           : item.debitor_uuid,
-        group_uuid     : item.group_uuid,
-        text           : item.text,
-        reference      : item.abbr + item.reference,
-        balance_first  : 0,
-        balance_second : 0,
-        balance_third  : 0,
-        balance_fourth : item.balance
-      };
-      temporary.push(line);
-    });
-
-    var unique = {}, uniqueTmp = {};
-    temporary.forEach(function (item) {
-      if (!unique[item.uuid]) {
-        unique[item.uuid] = true;
-        uniqueTmp[item.uuid] = item;
-      } else {
-        uniqueTmp[item.uuid].balance_first  += item.balance_first;
-        uniqueTmp[item.uuid].balance_second += item.balance_second;
-        uniqueTmp[item.uuid].balance_third  += item.balance_third;
-        uniqueTmp[item.uuid].balance_first  += item.balance_first;
-      }
-    });
-
-    session.debtorList = temporary;
-
   }
 
   /** debtor group data */
   function getDebtorGroupList(rows) {
-    vm.loading = false;
     var innerCustomerDebts = {
       first  : rows.data.first,
       second : rows.data.second,
@@ -228,8 +118,8 @@ function CustomerDebtController ($q, $http, $location, validate, util, exchange,
       temporary.push(line);
     });
 
-    var unique = {}, uniqueTmp = {};
-    temporary.forEach(function (item) {
+    var unique = {}, uniqueTmp = {}, count = 0;
+    temporary.forEach(function (item, index) {
       if (!unique[item.uuid]) {
         unique[item.uuid] = true;
         uniqueTmp[item.uuid] = item;
@@ -241,10 +131,14 @@ function CustomerDebtController ($q, $http, $location, validate, util, exchange,
       }
     });
 
-    session.debtorList = temporary;
+    session.debtorList = [];
+    Object.keys(uniqueTmp).forEach(function (key) {
+      session.debtorList.push(uniqueTmp[key]);
+    });
+    
+    vm.loading = false;
 
   }
-
 
   function totals() {
     session.totalFirst  = 0;
@@ -258,19 +152,6 @@ function CustomerDebtController ($q, $http, $location, validate, util, exchange,
       session.totalFourth += item.balance_fourth;
     });
     session.totalTotal  = session.totalFirst + session.totalSecond + session.totalThird + session.totalFourth;
-  }
-
-  function sumAttribute(array, attribute) {
-    return array.length ? array.reduce(function (a, b) { return b[attribute] + a; }) : 0;
-  }
-
-  function setSelectedGroup() {
-    vm.state = 'generate';
-    getPeriodicDebts();
-  }
-
-  function goto(document_uuid) {
-    $location.path('/invoice/sale/' + document_uuid);
   }
 
   function reconfigure() {
--- server/controllers/customer_debt.js
+++ server/controllers/customer_debt.js
@@ -17,122 +17,12 @@ function periodicDebts(req, res, next) {
 
 	params = req.query;
 
-	if (params.uuid && params.uuid !== '*') {
-
-		debtorDebts(params.uuid, params.date)
-		.then(function (ans) {
-			res.status(200).send(ans);
-		})
-		.catch(next);
-
-	} else {
-
-		debtorGroupDebts(params.date)
-		.then(function (ans) {
-			res.status(200).send(ans);
-		})
-		.catch(next);
-
-	}
-
-}
-
-function debtorDebts(id, untilDate) {
-	var defer = q.defer();
-	var glb = {};
-
-	if (!id) { defer.reject(new Error('No debitor_group id selected!')); }
-
-  var query =
-    'SELECT `debitor_group`.`account_id` FROM `debitor_group` ' +
-    ' WHERE `debitor_group`.`uuid`=?';
-
-	db.exec(query, [id])
+	debtorGroupDebts(params.date)
 	.then(function (ans) {
-	  var accountId = ans[0].account_id;
-	  var query =
-	    'SELECT c.inv_po_id, c.trans_id, c.trans_date, c.account_id FROM (' +
-	      ' SELECT p.inv_po_id, p.trans_id, p.trans_date, p.account_id ' +
-	      ' FROM posting_journal AS p ' +
-	      ' WHERE p.account_id=? ' +
-	      ' UNION ' +
-	      ' SELECT g.inv_po_id, g.trans_date, g.trans_id, g.account_id ' +
-	      ' FROM general_ledger AS g ' +
-	      ' WHERE g.account_id=? ' +
-	    ') AS c ;';
-
-	  return db.exec(query, [accountId, accountId]);
+		res.status(200).send(ans);
 	})
-	.then(function (ans) {
-	  if (!ans.length) { defer.resolve([]); }
+	.catch(next);
 
-	  glb.invoices = ans.map(function (line) {
-	    return line.inv_po_id;
-	  });
-
-	  var dbQueries = [];
-	  var accountId = ans[0].account_id;
-
-	  glb.sqlTemplate =
-	    'SELECT s.reference, s.is_distributable, t.project_id, t.inv_po_id, t.trans_date, SUM(t.debit_equiv) AS debit,  ' +
-	      'SUM(t.credit_equiv) AS credit, SUM(t.debit_equiv - t.credit_equiv) as balance, ' +
-	      't.account_id, t.deb_cred_uuid, t.currency_id, t.doc_num, t.description, t.account_id, ' +
-	      't.comment, d.text, d.group_uuid, s.note, s.debitor_uuid, p.reference, pro.abbr ' +
-	    'FROM (' +
-	      '(' +
-	        'SELECT pj.project_id, pj.inv_po_id, pj.trans_date, pj.debit, ' +
-	          'pj.credit, pj.debit_equiv, pj.credit_equiv, ' +
-	          'pj.account_id, pj.deb_cred_uuid, pj.currency_id, ' +
-	          'pj.doc_num, pj.trans_id, pj.description, pj.comment ' +
-	        'FROM posting_journal pj ' +
-			    'WHERE (pj.inv_po_id IN ("' + glb.invoices.join('","') + '")) AND pj.account_id = ' + accountId + ' ' +
-	      ') UNION ALL (' +
-	        'SELECT gl.project_id, gl.inv_po_id, gl.trans_date, gl.debit, ' +
-	          'gl.credit, gl.debit_equiv, gl.credit_equiv, ' +
-	          'gl.account_id, gl.deb_cred_uuid, gl.currency_id, ' +
-	          'gl.doc_num, gl.trans_id, gl.description, gl.comment ' +
-	        'FROM general_ledger gl ' +
-	        'WHERE (gl.inv_po_id IN ("' + glb.invoices.join('","') + '")) AND gl.account_id = ' + accountId + ' ' +
-	      ')' +
-	    ') AS `t` JOIN sale s ON t.inv_po_id = s.uuid ' +
-	    'JOIN patient p ON p.debitor_uuid = s.debitor_uuid ' + 
-	    'JOIN debitor d ON d.uuid = p.debitor_uuid ' + 
-	    'JOIN project pro ON pro.id = t.project_id ' + 
-	    'WHERE (%PERIOD%) ' +
-	    'GROUP BY t.inv_po_id HAVING balance > 0 ;\n';
-
-	  var untilDate = util.toMysqlDate(untilDate);
-
-	  var requette1 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) <= 90');
-
-		var requette2 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) BETWEEN 91 AND 180');
-
-		var requette3 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) BETWEEN 181 AND 360');
-
-		var requette4 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) > 360');
-
-	  dbQueries.push(db.exec(requette1));
-	  dbQueries.push(db.exec(requette2));
-	  dbQueries.push(db.exec(requette3));
-	  dbQueries.push(db.exec(requette4));
-
-	  return q.all(dbQueries);
-	})
-	.spread(function (first, second, third, fourth) {
-		var ans = {
-			first  : first,
-			second : second, 
-			third  : third, 
-			fourth : fourth
-		};
-		console.log('More than a year : ', fourth);
-	  defer.resolve(ans);
-	})
-	.catch(function (error) {
-	  defer.reject(error);
-	});
-
-	return defer.promise;
 }
 
 
@@ -156,37 +46,41 @@ function debtorGroupDebts(untilDate) {
 	    'SELECT t.project_id, t.inv_po_id, t.trans_date, SUM(t.debit_equiv) AS debit,  ' +
 	      'SUM(t.credit_equiv) AS credit, SUM(t.debit_equiv - t.credit_equiv) as balance, ' +
 	      't.account_id, t.deb_cred_uuid, t.currency_id, t.doc_num, t.description, t.account_id, ' +
-	      't.comment, dg.name, dg.uuid ' +
+	      't.comment, t.name, t.uuid ' +
 	    'FROM (' +
 	      '(' +
-	        'SELECT pj.project_id, pj.inv_po_id, pj.trans_date, pj.debit, ' +
-	          'pj.credit, pj.debit_equiv, pj.credit_equiv, ' +
-	          'pj.account_id, pj.deb_cred_uuid, pj.currency_id, ' +
-	          'pj.doc_num, pj.trans_id, pj.description, pj.comment ' +
-	        'FROM posting_journal pj ' +
-			    'WHERE pj.account_id IN (' + accountIds + ') ' +
-	      ') UNION ALL (' +
-	        'SELECT gl.project_id, gl.inv_po_id, gl.trans_date, gl.debit, ' +
-	          'gl.credit, gl.debit_equiv, gl.credit_equiv, ' +
-	          'gl.account_id, gl.deb_cred_uuid, gl.currency_id, ' +
-	          'gl.doc_num, gl.trans_id, gl.description, gl.comment ' +
-	        'FROM general_ledger gl ' +
-	        'WHERE gl.account_id IN (' + accountIds + ')' +
+	        'SELECT t.project_id, t.inv_po_id, t.trans_date, t.debit, ' +
+	          't.credit, t.debit_equiv, t.credit_equiv, ' +
+	          't.account_id, t.deb_cred_uuid, t.currency_id, ' +
+	          't.doc_num, t.trans_id, t.description, t.comment, ' +
+	          'dg.name, dg.uuid ' + 
+	        'FROM posting_journal t ' +
+	        'JOIN debitor_group dg ON dg.account_id = t.account_id ' + 
+	        'WHERE (%PERIOD%) ' +
+	      ') UNION (' +
+	        'SELECT t.project_id, t.inv_po_id, t.trans_date, t.debit, ' +
+	          't.credit, t.debit_equiv, t.credit_equiv, ' +
+	          't.account_id, t.deb_cred_uuid, t.currency_id, ' +
+	          't.doc_num, t.trans_id, t.description, t.comment, ' +
+	          'dg.name, dg.uuid ' + 
+	        'FROM general_ledger t ' +
+	    		'JOIN debitor_group dg ON dg.account_id = t.account_id ' + 
+	    		'WHERE (%PERIOD%) ' +
 	      ')' +
 	    ') AS `t` ' +
-	    'JOIN debitor_group dg ON dg.account_id = t.account_id ' + 
-	    'WHERE (%PERIOD%) ' +
 	    'GROUP BY t.account_id HAVING balance > 0 ;\n';
 
-	  var untilDate = util.toMysqlDate(untilDate);
+	  if (!untilDate) {
+	  	untilDate = util.toMysqlDate(new Date());
+	  }
 
-	  var requette1 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) <= 90');
-
-		var requette2 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) BETWEEN 91 AND 180');
-
-		var requette3 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) BETWEEN 181 AND 360');
-
-		var requette4 = glb.sqlTemplate.replace('%PERIOD%', 'DATEDIFF("' + untilDate + '", t.trans_date) > 360');
+	  var requette1 = glb.sqlTemplate.replace(/%PERIOD%/g, '(DATEDIFF("' + untilDate + '", t.trans_date) BETWEEN 0 AND 90) AND ("'+untilDate+'" >= t.trans_date)');
+	  
+		var requette2 = glb.sqlTemplate.replace(/%PERIOD%/g, '(DATEDIFF("' + untilDate + '", t.trans_date) BETWEEN 91 AND 180) AND ("'+untilDate+'" >= t.trans_date)');
+		
+		var requette3 = glb.sqlTemplate.replace(/%PERIOD%/g, '(DATEDIFF("' + untilDate + '", t.trans_date) BETWEEN 181 AND 360) AND ("'+untilDate+'" >= t.trans_date)');
+		
+		var requette4 = glb.sqlTemplate.replace(/%PERIOD%/g, '(DATEDIFF("' + untilDate + '", t.trans_date) > 360) AND ("'+untilDate+'" >= t.trans_date)');
 
 	  dbQueries.push(db.exec(requette1));
 	  dbQueries.push(db.exec(requette2));
