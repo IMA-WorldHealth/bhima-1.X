2e40c1a09f8dfbce2750d9419341dabd4672568f
Bruce Mbayo <mbayopanda@gmail.com>
2016-04-07 07:54:42 +0100

    Removing commented queries

--- server/controllers/serviceStatus.js
+++ server/controllers/serviceStatus.js
@@ -17,23 +17,6 @@ function list(req, res, next) {
 	params = req.query;
 
 	requette = 
-		// 'SELECT v.name, v.id, s.uuid, s.service_id, s.note, s.invoice_date, SUM(s.cost) AS cost, s.currency_id, COUNT(s.uuid) AS nb_facture ' + 
-		// 'FROM sale s ' + 
-		// 'JOIN service v ON v.id = s.service_id ' +
-		// 'WHERE s.uuid NOT IN (SELECT sale_uuid FROM credit_note) AND (s.invoice_date BETWEEN ? AND ?) ' + 
-		// 'GROUP BY s.service_id';
-
-		// 'select distinct SUM(credit_equiv - debit_equiv) as cost, t.service_id as id, t.uuid, t.name ' +
-		// 'from  (' +
-		// 	'select general_ledger.debit_equiv, general_ledger.credit_equiv, general_ledger.pc_id, v.name, s.uuid, s.service_id ' +
-		// 	'from general_ledger ' +
-		// 	'join account on account.id = general_ledger.account_id join account_type on account.account_type_id = account_type.id ' +
-		// 	'join profit_center on profit_center.id = general_ledger.pc_id ' +
-		// 	'join sale s on s.uuid = general_ledger.inv_po_id ' +
-		// 	'join service v on v.id = s.service_id ' +
-		// 	'where account_type_id = 1 AND s.uuid NOT IN (SELECT sale_uuid FROM credit_note) AND (general_ledger.trans_date BETWEEN ? AND ?) ' +
-		// ') as t group by t.service_id;';
-
 		'SELECT v.name, v.id, s.uuid, s.service_id, s.note, s.invoice_date, SUM(s.cost) AS cost, s.currency_id, COUNT(s.uuid) AS nb_facture ' + 
 		'FROM sale s ' + 
 		'JOIN service v ON v.id = s.service_id ' +
