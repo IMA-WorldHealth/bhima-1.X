e8eac0ce2b79755d4f77282b63a4a8fa7a06b843
Bruce Mbayo <mbayopanda@gmail.com>
2016-10-20 12:34:24 +0100

    Fixing Fiscal year creation and locking
    Former-commit-id: c20e44f1c1dd6990990dd6ca7dbc3fe1e6f20369 [formerly fcd8f16ff8229c046264c39b98d7b515db38df1f]
    Former-commit-id: 8aed369c1431a347a6bfbb1c549bb7618af2eaf8

--- client/src/partials/fiscal/fiscal.lock.js
+++ client/src/partials/fiscal/fiscal.lock.js
@@ -1,212 +1,175 @@
 angular.module('bhima.controllers')
-.controller('fiscal.lock', [
-  '$q',
-  '$scope',
-  '$http',
-  '$translate',
-  'validate',
-  'connect',
-  'messenger',
-  '$route',
-  function ($q, $scope, $http, $translate, validate, connect, messenger, $route) {
-    var imports,
-        editCache,
-        session = $scope.session = {},
-        dependencies = {};
+.controller('FiscalLockController', FiscalLockController);
 
-    // pull in data from the parent controller to use
-    // in child requests.
-    imports = $scope.$parent;
+FiscalLockController.$inject = [
+  '$q', '$scope', '$http', '$route', '$translate', 'validate', 'connect', 'messenger', 'util'
+];
 
-    // dependencies
-    dependencies.fiscal = {
-      query : {
-        tables : {
-          fiscal_year : {
-            columns : ['id', 'number_of_months', 'fiscal_year_txt', 'transaction_start_number', 'transaction_stop_number', 'start_month', 'start_year', 'previous_fiscal_year', 'locked']
-          }
-        }
-      }
-    };
+/**
+  * Fiscal Lock Controller
+  * This controller is responsible for locking a fiscal year
+  */
+function FiscalLockController ($q, $scope, $http, $route, $translate, validate, connect, messenger, util) {
+  var imports = $scope.$parent,
+      session = $scope.session = {},
+      editCache,
+      dependencies = {};
 
-    dependencies.resultatAccount = {
-      query : {
-        tables : {
-          account : { columns : ['id', 'account_number', 'account_txt'] }
-        },
-        where : ['account.classe=1', 'AND', 'is_ohada=1']
-      }
-    };
+  // imports shared functions
+  var getSolde     = imports.getSolde,
+      getTypeSolde = imports.getTypeSolde,
+      error        = imports.error;
 
-    dependencies.user = {
-      query : '/user_session'
-    };
-
-    // Fire off the onload function for this controller
-    onLoad();
-
-    // Reload the controller on selection change
-    $scope.$on('fiscal-year-selection-lock-change', onLoad);
-
-    // Exposition to the view
-    $scope.closeFiscalYear = closeFiscalYear;
-    $scope.formatAccount = formatAccount;
-
-    function observation () {
-      if ((session.produit - session.charge) > 0) {
-        session.observation = 1;
-      } else if ((session.produit - session.charge) < 0)  {
-        session.observation = -1;
-      } else {
-        session.observation = 0;
+  // dependencies
+  dependencies.fiscal = {
+    query : {
+      tables : {
+        fiscal_year : { columns : ['id', 'number_of_months', 'fiscal_year_txt', 'transaction_start_number', 'transaction_stop_number', 'start_month', 'start_year', 'previous_fiscal_year', 'locked'] }
       }
     }
+  };
 
-    function sumCredMinusDeb (a, b) {
-      return (b.credit_equiv - b.debit_equiv) + a;
+  dependencies.resultatAccount = {
+    query : {
+      tables : {
+        account : { columns : ['id', 'account_number', 'account_txt'] }
+      },
+      where : ['account.classe=1', 'AND', 'is_ohada=1']
     }
+  };
 
-    function sumDebMinusCred (a, b) {
-      return (b.debit_equiv - b.credit_equiv) + a;
+  dependencies.user = {
+    query : '/user_session'
+  };
+
+  // Fire off the onload function for this controller
+  onLoad();
+
+  // Reload the controller on selection change
+  $scope.$on('fiscal-year-selection-lock-change', onLoad);
+
+  // Exposition to the view
+  $scope.closeFiscalYear = closeFiscalYear;
+  $scope.formatAccount = formatAccount;
+
+  /**
+    * This function is responsible for giving the result of the enterprise
+    * loss or profit, and we use this result in the ui
+    */
+  function observation () {
+    if ((session.produit - session.charge) > 0) {
+      session.observation = 1;
+    } else if ((session.produit - session.charge) < 0)  {
+      session.observation = -1;
+    } else {
+      session.observation = 0;
     }
+  }
 
-    function getSolde (classe, fy) {
-      return $http.get('/getClassSolde/'+classe+'/'+fy)
-      .success(function (data) {
-        return data;
-      });
-    }
+  function sumCredMinusDeb (a, b) {
+    return (b.credit_equiv - b.debit_equiv) + a;
+  }
 
-    function loadSolde (fy_id) {
-      getSolde(6, fy_id)
-      .then(function (data6) {
-        session.array6 = data6.data;
-        return getSolde(7, fy_id);
-      })
-      .then(function (data7) {
-        session.array7 = data7.data;
-        return getSolde(8, fy_id);
-      })
-      .then(function (data8) {
-        session.array8 = data8.data;
+  function sumDebMinusCred (a, b) {
+    return (b.debit_equiv - b.credit_equiv) + a;
+  }
+
+  function loadSolde (fy_id) {
+    getTypeSolde(fy_id, 'INCOME_EXPENSE', 'CHARGE')
+    .then(function (charge) {
+      session.chargeData = charge.data;
+      return getTypeSolde(fy_id, 'INCOME_EXPENSE', 'PRODUIT');
+    })
+    .then(function (produit) {
+      session.produitData = produit.data;
+    })
+    .then(function () {
+      session.charge = session.chargeData.reduce(sumDebMinusCred, 0);
+      session.produit = session.produitData.reduce(sumCredMinusDeb, 0);
+      observation();
+    })
+    .catch(error);
+  }
+
+  function closeFiscalYear (fy_id) {
+    var res = confirm($translate.instant('FISCAL_YEAR.CONFIRM_CLOSING'));
+    if (res) {
+      var updateFY = { id : fy_id, locked : 1 };
+      postingLockedFiscalYear(fy_id)
+      .then(function () {
+        connect.put('fiscal_year', [updateFY], ['id']);
       })
       .then(function () {
-        var otherCharge = session.array8.filter(function (item) {
-          var accountNumber = String(item.account_number);
-          var accountIndex = accountNumber.substring(0,2);
-          accountIndex = parseInt(accountIndex);
-          return ((item.is_charge) === 1);
-        });
-
-        var otherProfit = session.array8.filter(function (item) {
-          var accountNumber = String(item.account_number);
-          var accountIndex = accountNumber.substring(0,2);
-          accountIndex = parseInt(accountIndex);
-          return ((item.is_charge) === 0);
-        });
-
-        session.otherCharge = otherCharge;
-        session.otherProfit = otherProfit;           
-
-        session.solde6 = session.array6.reduce(sumDebMinusCred, 0);
-        session.solde7 = session.array7.reduce(sumCredMinusDeb, 0);
-
-        session.solde8Charge = otherCharge.reduce(sumDebMinusCred, 0);
-        session.solde8Profit = otherProfit.reduce(sumCredMinusDeb, 0);
-
-        session.charge = session.solde6 + session.solde8Charge;
-        session.produit = session.solde7 + session.solde8Profit;
-
-        observation();
-      });
-    }
-
-    function closeFiscalYear (fy_id) {
-      var res = confirm($translate.instant('FISCAL_YEAR.CONFIRM_CLOSING'));
-      if (res) {
-        var updateFY = {
-          id : fy_id,
-          locked : 1
-        };
-
-        postingLockedFiscalYear(fy_id)
-        .then(function () {
-          connect.put('fiscal_year', [updateFY], ['id']);
-        })
-        .then(function () {
-          messenger.success($translate.instant('FISCAL_YEAR.LOCKED_SUCCESS'), true);
-        })
-        .then(refresh)
-        .catch(function (err) {
-          console.error(err);
-        });
-      }
-    }
-
-    function postingLockedFiscalYear (fy_id) {
-      var data = {
-        new_fy_id : fy_id,
-        user_id   : session.user_id,
-        resultat  : {
-          resultat_account : session.resultat_account,
-          class6           : session.array6,
-          class7           : session.array7,
-          solde8Charge     : session.otherCharge,
-          solde8Profit     : session.otherProfit,          
-          forcingDate      : session.fiscalYearLastDate
-        }
-      };
-
-      return $http.post('/posting_fiscal_resultat/', { params: 
-        {
-          new_fy_id : data.new_fy_id,
-          user_id   : data.user_id,
-          resultat  : data.resultat,
-          date      : data.forcingDate
-        }
-      });
-    }
-
-    function getFiscalYearLastDate (fy_id) {
-      dependencies.period = {
-        query : {
-          tables : {
-            period : { columns : ['period_start', 'period_stop'] }
-          },
-          where : ['period.fiscal_year_id='+fy_id]
-        }
-      };
-
-      validate.refresh(dependencies, ['period'])
-      .then(function (model) {
-        session.fiscalYearLastDate = model.period.data[model.period.data.length-1].period_stop;
-      });
-    }
-
-    function onLoad () {
-      session.selectedToLock = imports.selectedToLock;
-      dependencies.fiscal.query.where = ['fiscal_year.id=' + session.selectedToLock];
-      validate.refresh(dependencies, ['fiscal', 'resultatAccount', 'user'])
-      .then(function (models) {
-        $scope.resultatAccount = models.resultatAccount;
-        $scope.fiscal = models.fiscal.data[0];
-        // get user id
-        session.user_id = models.user.data.id;
-        // cache the fiscal year data for expected edits
-        editCache = angular.copy($scope.fiscal);
+        messenger.success($translate.instant('FISCAL_YEAR.LOCKED_SUCCESS'), true);
       })
-      .then(loadSolde(session.selectedToLock))
-      .then(getFiscalYearLastDate(session.selectedToLock));
+      .then(refresh)
+      .catch(error);
     }
-
-    function formatAccount (ac) {
-      return '['+ac.account_number+'] => '+ac.account_txt;
-    }
-
-    function refresh () {
-      // TODO implement a fiscal year list refresh mechanism
-      $route.reload();
-    }
-
   }
-]);
+
+  function postingLockedFiscalYear (fy_id) {
+    var data = {
+      new_fy_id : fy_id,
+      user_id   : session.user_id,
+      bundle  : {
+        flag               : 'SIMPLE_LOCKING',
+        resultat_account   : session.resultat_account,
+        charge             : session.chargeData,
+        produit            : session.produitData,
+        fiscalYearLastDate : util.sqlDate(session.fiscalYearLastDate)
+      }
+    };
+
+    return $http.post('/posting_fiscal_resultat/', { params:
+      {
+        new_fy_id : data.new_fy_id,
+        user_id   : data.user_id,
+        bundle    : data.bundle
+      }
+    });
+  }
+
+  function getFiscalYearLastDate (fy_id) {
+    dependencies.period = {
+      query : {
+        tables : {
+          period : { columns : ['period_start', 'period_stop'] }
+        },
+        where : ['period.fiscal_year_id='+fy_id]
+      }
+    };
+
+    validate.refresh(dependencies, ['period'])
+    .then(function (model) {
+      session.fiscalYearLastDate = model.period.data[model.period.data.length-1].period_stop;
+    })
+    .catch(error);
+  }
+
+  function onLoad () {
+    session.selectedToLock = imports.selectedToLock;
+    dependencies.fiscal.query.where = ['fiscal_year.id=' + session.selectedToLock];
+    validate.refresh(dependencies, ['fiscal', 'resultatAccount', 'user'])
+    .then(function (models) {
+      $scope.resultatAccount = models.resultatAccount;
+      $scope.fiscal = models.fiscal.data[0];
+      // get user id
+      session.user_id = models.user.data.id;
+      // cache the fiscal year data for expected edits
+      editCache = angular.copy($scope.fiscal);
+    })
+    .then(loadSolde(session.selectedToLock))
+    .then(getFiscalYearLastDate(session.selectedToLock))
+    .catch(error);
+  }
+
+  function formatAccount (ac) {
+    return '['+ac.account_number+'] => '+ac.account_txt;
+  }
+
+  function refresh () {
+    // TODO implement a fiscal year list refresh mechanism
+    $route.reload();
+  }
+
+}
